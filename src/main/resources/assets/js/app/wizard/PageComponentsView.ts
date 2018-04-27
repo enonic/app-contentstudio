e5605f0d5398ba6db6074e3db2846aebff5a22ed
Alan Semenov <ase@enonic.com>
2018-04-27 12:48:05 +0200

    Fixed console error on a click inside Page Components view (#55)

--- src/main/resources/assets/js/app/wizard/PageComponentsView.ts
+++ src/main/resources/assets/js/app/wizard/PageComponentsView.ts
@@ -638,6 +638,10 @@ export class PageComponentsView
         event.stopPropagation();
         event.preventDefault();
 
+        if (!this.pageView.isLocked()) {
+            return;
+        }
+
         if (this.contextMenu && this.contextMenu.isVisible()) {
             this.hideContextMenu();
         } else {
