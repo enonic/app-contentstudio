68b87fe2f9b21e500f8c9e45570080121bc12cef
Pavel Milkevich <pavel.milkevich@gmail.com>
2018-05-11 18:49:57 +0300

    Show content status and last modified in the Content preview toolbar #78

--- src/main/resources/assets/js/app/view/ContentItemPreviewToolbar.ts
+++ src/main/resources/assets/js/app/view/ContentItemPreviewToolbar.ts
@@ -1,16 +1,39 @@
 import '../../api.ts';
 import ContentSummaryAndCompareStatus = api.content.ContentSummaryAndCompareStatus;
+import ViewItem = api.app.view.ViewItem;
+import i18n = api.util.i18n;
 
 export class ContentItemPreviewToolbar
     extends api.app.view.ItemPreviewToolbar<ContentSummaryAndCompareStatus> {
+    private status: api.dom.SpanEl;
 
     constructor() {
         super('content-item-preview-toolbar');
+        this.status = new api.dom.SpanEl('content-status');
+        this.addElement(this.status);
     }
 
-    setItem(item: api.app.view.ViewItem<ContentSummaryAndCompareStatus>) {
+    setItem(item: ViewItem<ContentSummaryAndCompareStatus>) {
         if (this.getItem() !== item) {
             super.setItem(item);
+            this.status.setHtml(this.getStatusString(item.getModel()), false);
         }
+    }
+
+    private isOnline(content: ContentSummaryAndCompareStatus): boolean {
+        return !!content && content.isOnline();
+    }
+
+    private getStatusString(content: ContentSummaryAndCompareStatus): string {
+
+        let status = new api.dom.SpanEl();
+        if (this.isOnline(content)) {
+            status.addClass('online');
+        }
+
+        status.addClass(content.getStatusClass());
+        status.setHtml(content.getStatusText());
+
+        return i18n('field.preview.toolbar.status', status.toString(), content.getContentSummary().getModifier());
     }
 }
