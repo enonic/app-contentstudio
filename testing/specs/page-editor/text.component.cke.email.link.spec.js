7831c026cb4d5781e3b41b028ca18d96f52878c9
Siarhei Hauruseu <sig@enonic.com>
2018-06-18 15:48:46 +0300

    ui-tests - bail() temporarily commented

--- testing/specs/page-editor/text.component.cke.email.link.spec.js
+++ testing/specs/page-editor/text.component.cke.email.link.spec.js
@@ -25,7 +25,7 @@ describe('Text Component with CKE - insert email link  specification', function 
 
     it(`Precondition: WHEN new site has been added THEN the site should be listed in the grid`,
         () => {
-            this.bail(1);
+            //this.bail(1);
             let displayName = contentBuilder.generateRandomName('site');
             SITE = contentBuilder.buildSite(displayName, 'description', ['All Content Types App'], CONTROLLER_NAME);
             return studioUtils.doAddSite(SITE).then(() => {
