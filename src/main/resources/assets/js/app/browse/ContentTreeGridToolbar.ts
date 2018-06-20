40d65a8a05115e5efcd3d7b3079676a03545e467
Alan Semenov <ase@enonic.com>
2018-06-20 23:00:19 +0200

    Refactored the show issues button placement (#219)

--- src/main/resources/assets/js/app/browse/ContentTreeGridToolbar.ts
+++ src/main/resources/assets/js/app/browse/ContentTreeGridToolbar.ts
@@ -4,17 +4,9 @@ import {ContentTreeGrid} from './ContentTreeGrid';
 export class ContentTreeGridToolbar
     extends api.ui.treegrid.TreeGridToolbar {
 
-    private issuesDialogButton: ShowIssuesDialogButton;
-
     constructor(treeGrid: ContentTreeGrid) {
         super(treeGrid);
         this.addClass('content-tree-grid-toolbar');
-        this.issuesDialogButton = new ShowIssuesDialogButton();
-        this.appendToCenter(this.issuesDialogButton);
     }
 
-
-    public getIssuesDialogButton(): ShowIssuesDialogButton {
-        return this.issuesDialogButton;
-    }
 }
