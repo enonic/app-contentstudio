7faa10e62c444ee2231cfaad5ab77b9dae08f737
Viachaslau Bradnitski <vbr@enonic.com>
2018-08-03 16:54:53 +0300

    Drop support for old implementation of mixins #347 files from lib-admin-ui

---
+++ src/main/resources/assets/js/app/resource/XDataResourceRequest.ts
@@ -0,0 +1,19 @@
+import Mixin = api.schema.mixin.Mixin;
+
+export class XDataResourceRequest<JSON_TYPE, PARSED_TYPE>
+    extends api.rest.ResourceRequest<JSON_TYPE, PARSED_TYPE> {
+    private resourceUrl: api.rest.Path;
+
+    constructor() {
+        super();
+        this.resourceUrl = api.rest.Path.fromParent(super.getRestPath(), 'schema/xdata');
+    }
+
+    getResourcePath(): api.rest.Path {
+        return this.resourceUrl;
+    }
+
+    fromJsonToMixin(json: api.schema.mixin.MixinJson) {
+        return Mixin.fromJson(json);
+    }
+}
