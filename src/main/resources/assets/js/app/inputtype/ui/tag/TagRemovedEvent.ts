677f5b137c2f42ca9127f56ac9e1759a92a5209d
Taukachou Mikita <edloidas@gmail.com>
2018-09-03 14:24:12 +0300

    Move input types to the app-content #400
    Moved `tag` input type.

---
+++ src/main/resources/assets/js/app/inputtype/ui/tag/TagRemovedEvent.ts
@@ -0,0 +1,20 @@
+export class TagRemovedEvent {
+
+    private value: string;
+
+    private index: number;
+
+    constructor(value: string, index: number) {
+        this.value = value;
+        this.index = index;
+    }
+
+    getValue(): string {
+        return this.value;
+    }
+
+    getIndex(): number {
+        return this.index;
+    }
+
+}
