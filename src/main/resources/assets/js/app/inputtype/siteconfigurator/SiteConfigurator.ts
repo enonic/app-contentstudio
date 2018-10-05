115a27c36933b5320260555c0ee0952b1bce238c
Viachaslau Bradnitski <vbr@enonic.com>
2018-10-06 01:56:59 +0300

    X-data is not refreshed after version rollback #453

--- src/main/resources/assets/js/app/inputtype/siteconfigurator/SiteConfigurator.ts
+++ src/main/resources/assets/js/app/inputtype/siteconfigurator/SiteConfigurator.ts
@@ -85,11 +85,10 @@ export class SiteConfigurator
 
             this.siteConfigProvider.setPropertyArray(propertyArray);
 
+            this.deselectOldViews(propertyArray);
+
             const selectedOptionViews = propertyArray.map(property =>
                 <SiteConfiguratorSelectedOptionView>this.selectOptionFromProperty(property).getOptionView());
-
-            const optionsToDeselect = this.getOptionsToDeselect(selectedOptionViews);
-            optionsToDeselect.forEach(option => this.comboBox.deselect(option.getApplication(), true));
 
             const updatePromises = selectedOptionViews.map((view, index) => {
                 const configSet = propertyArray.get(index).getPropertySet().getProperty('config').getPropertySet();
@@ -113,10 +112,19 @@ export class SiteConfigurator
         return option.getApplication().getApplicationKey().toString();
     }
 
-    private getOptionsToDeselect(selectedOptionViews: SiteConfiguratorSelectedOptionView[]) {
-        return this.comboBox.getSelectedOptionViews().filter(oldView => !selectedOptionViews.some(newView =>
-            SiteConfigurator.optionViewToKey(newView) === SiteConfigurator.optionViewToKey(oldView))
-        );
+    private deselectOldViews(newPropertyArray: PropertyArray) {
+
+        this.comboBox.getSelectedOptionViews().forEach(oldView => {
+            const haveToDeselect = !newPropertyArray.some(property => {
+                const key = property.getPropertySet().getProperty('applicationKey').getValue().getString();
+                return SiteConfigurator.optionViewToKey(oldView) === key;
+
+            });
+
+            if (haveToDeselect) {
+                this.comboBox.deselect(oldView.getApplication(), true);
+            }
+        });
     }
 
     private selectOptionFromProperty(property: Property): SelectedOption<Application> {
