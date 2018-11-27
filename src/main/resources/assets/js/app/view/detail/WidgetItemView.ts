c5d902f819c8eec98bc22c2aadd56adba43ac259
Alan Semenov <ase@enonic.com>
2018-11-27 10:58:04 +0100

    Got rid of odd parameters in widget URL (#36)

--- src/main/resources/assets/js/app/view/detail/WidgetItemView.ts
+++ src/main/resources/assets/js/app/view/detail/WidgetItemView.ts
@@ -4,7 +4,6 @@ import {ContentSummaryAndCompareStatus} from '../../content/ContentSummaryAndCom
 export class WidgetItemView extends api.dom.DivEl {
 
     public static debug: boolean = false;
-    private uid: string = '';
 
     constructor(className?: string) {
         super('widget-item-view' + (className ? ' ' + className : ''));
@@ -21,14 +20,13 @@ export class WidgetItemView extends api.dom.DivEl {
         return wemQ<any>(null);
     }
 
-    private getFullWidgetUrl(url: string, uid: string, contentId: string) {
-        return url + '?uid=' + uid + '&contentId=' + contentId;
+    private getFullWidgetUrl(url: string, contentId: string) {
+        return `${url}?contentId=${contentId}&t=${new Date().getTime()}`;
     }
 
-    public setUrl(url: string, contentId: string, keepId: boolean = false): wemQ.Promise<void> {
+    public setUrl(url: string, contentId: string): wemQ.Promise<void> {
         let deferred = wemQ.defer<void>();
-        let uid = (!keepId || !this.uid) ? Date.now().toString() : this.uid;
-        let linkEl = new LinkEl(this.getFullWidgetUrl(url, uid, contentId)).setAsync();
+        let linkEl = new LinkEl(this.getFullWidgetUrl(url, contentId)).setAsync();
         let el = this.getEl();
         let onLinkLoaded = ((event: UIEvent) => {
                 const mainContainer = event.target['import'].querySelector('widget');
@@ -38,9 +36,6 @@ export class WidgetItemView extends api.dom.DivEl {
                 linkEl.remove();
                 deferred.resolve(null);
             });
-
-        this.uid = uid;
-        this.removeChildren();
 
         linkEl.onLoaded(onLinkLoaded);
         document.head.appendChild(linkEl.getHTMLElement());
