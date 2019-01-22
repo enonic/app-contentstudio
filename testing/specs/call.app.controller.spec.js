fd3833954d4a1489d13a47c48c22c93001ee648f
Siarhei Hauruseu <sig@enonic.com>
2019-01-22 17:22:37 +0300

    ui-test - call application controller

---
+++ testing/specs/call.app.controller.spec.js
@@ -0,0 +1,31 @@
+/**
+ * Created on 22.01.2019.
+ *
+ */
+const chai = require('chai');
+chai.use(require('chai-as-promised'));
+const webDriverHelper = require('../libs/WebDriverHelper');
+const appConstant = require('../libs/app_const');
+
+describe('Call the `Application controller` specification', function () {
+    this.timeout(appConstant.SUITE_TIMEOUT);
+    webDriverHelper.setupBrowser();
+
+    it(`GIVEN application with page controller is installed WHEN getting the URL that ending without slash THEN expected header should be loaded`,
+        () => {
+            return webDriverHelper.browser.url("http://127.0.0.1:8080/app/com.enonic.app.appControllerTest").then(() => {
+                return webDriverHelper.browser.waitForVisible("//h1[text()='My controller test page']", appConstant.TIMEOUT_3);
+            })
+        });
+    //verifies bug: `Application controller not called for URL ending in /`
+    it(`GIVEN application with page controller is installed WHEN getting the URL that ending with slash THEN expected header should be loaded`,
+        () => {
+            return webDriverHelper.browser.url("http://127.0.0.1:8080/app/com.enonic.app.appControllerTest/").then(() => {
+                return webDriverHelper.browser.waitForVisible("//h1[text()='My controller test page']", appConstant.TIMEOUT_3);
+            })
+        });
+
+    before(() => {
+        return console.log('specification is starting: ' + this.title);
+    });
+});
