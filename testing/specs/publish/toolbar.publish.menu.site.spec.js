d90a8096f61ae3a1f00905ab1ad56230f22a826c
Siarhei Hauruseu <sig@enonic.com>
2019-06-04 12:41:02 +0300

    tests for Drag'n'Drop of selected options issue-520

--- testing/specs/publish/toolbar.publish.menu.site.spec.js
+++ testing/specs/publish/toolbar.publish.menu.site.spec.js
@@ -9,7 +9,7 @@ const ContentBrowsePanel = require('../../page_objects/browsepanel/content.brows
 const studioUtils = require('../../libs/studio.utils.js');
 const contentBuilder = require("../../libs/content.builder");
 
-describe('publish.dialog.site.with.children.spec - Select a site with not valid child and try to publish it`', function () {
+describe('toolbar.publish.menu.site.spec - publishes a site and checks publish menu items`', function () {
     this.timeout(appConstant.SUITE_TIMEOUT);
     webDriverHelper.setupBrowser();
 
@@ -20,14 +20,17 @@ describe('publish.dialog.site.with.children.spec - Select a site with not valid 
             let contentBrowsePanel = new ContentBrowsePanel();
             let displayName = contentBuilder.generateRandomName('site');
             SITE = contentBuilder.buildSite(displayName, 'description', [appConstant.SIMPLE_SITE_APP]);
+            //1. add new site
             await studioUtils.doAddSite(SITE);
             await studioUtils.findAndSelectItem(SITE.displayName);
-            //let isDisplayed = await contentBrowsePanel.clickOnPublishButton();
+
+            //2.  the site has been published:
             await studioUtils.doPublish();
-            //'Publish Tree button should appear on the toolbar' ( throw exception after the timeout)
+            //'Publish Tree button should appear on the toolbar' ( exception will be thrown after the timeout)
             await contentBrowsePanel.waitForPublishTreeButtonVisible();
+            //3. 'Publish Tree' menu item has been clicked and the action confirmed
             await studioUtils.doPublishTree();
-            //'Unpublish button should appear on the toolbar' ( throw exception after the timeout)
+            //'Unpublish button should appear on the toolbar' ( exception will be thrown after the timeout)
             await contentBrowsePanel.waitForUnPublishButtonVisible();
         });
 
