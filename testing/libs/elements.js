5e033529e588f6f268cf2fe80143691f767d264e
Siarhei Hauruseu <sig@enonic.com>
2019-06-26 15:10:32 +0300

    updating for ui-tests

--- testing/libs/elements.js
+++ testing/libs/elements.js
@@ -8,16 +8,16 @@ module.exports = Object.freeze({
     CONTENT_SELECTOR: "//div[contains(@id,'ContentSelector')]",
     NAMES_VIEW_BY_NAME: "//div[contains(@id,'NamesView') and child::p[contains(@class,'sub-name') and contains(.,'%s')]]",
     NAMES_VIEW_BY_DISPLAY_NAME: "//div[contains(@id,'NamesView') and child::h6[contains(@class,'main-name') and contains(.,'%s')]]",
-    SLICK_VIEW_PORT: `//div[@class='slick-viewport']`,
-    SLICK_ROW: "//div[@class='slick-viewport']//div[contains(@class,'slick-row')]",
-    SLICK_ROW_BY_NAME: "//div[@class='slick-viewport']//div[contains(@class,'slick-row') and descendant::p[contains(@class,'sub-name') and contains(.,'%s')]]",
+    SLICK_VIEW_PORT: `//div[contains(@class,'slick-viewport')]`,
+    SLICK_ROW: "//div[contains(@class,'slick-viewport')]//div[contains(@class,'slick-row')]",
+    SLICK_ROW_BY_NAME: "//div[contains(@class,'slick-viewport')]//div[contains(@class,'slick-row') and descendant::p[contains(@class,'sub-name') and contains(.,'%s')]]",
     H6_DISPLAY_NAME: "//div[contains(@id,'NamesView')]//h6[contains(@class,'main-name')]",
     P_SUB_NAME: "//p[contains(@class,'sub-name')]",
     TEXT_INPUT: "//input[contains(@id,'TextInput')]",
     RICH_TEXT_EDITOR: `//div[contains(@id,'TextComponentView') and contains(@class,'editor-focused')]//div[contains(@id,'TextComponentView')]`,
     TEXT_AREA: "//textarea[contains(@id,'TextArea')]",
     DROP_DOWN_HANDLE: "//button[contains(@id,'DropdownHandle')]",
-    GRID_CANVAS: `//div[@class='grid-canvas']`,
+    GRID_CANVAS: `//div[contains(@class,'grid-canvas')]`,
     TEXT_INPUT: `//input[@type='text']`,
     DROPDOWN_OPTION_FILTER_INPUT: "//input[contains(@id,'DropdownOptionFilterInput')]",
     VALIDATION_RECORDING_VIEWER: "//div[contains(@id,'ValidationRecordingViewer')]//li",
@@ -26,7 +26,7 @@ module.exports = Object.freeze({
     },
     slickRowByDisplayName: function (container, displayName) {
         return container +
-               `//div[@class='slick-viewport']//div[contains(@class,'slick-row') and descendant::h6[contains(@class,'main-name') and contains(.,'${displayName}')]]`;
+               `//div[contains(@class,'slick-viewport')]//div[contains(@class,'slick-row') and descendant::h6[contains(@class,'main-name') and contains(.,'${displayName}')]]`;
     },
     itemByDisplayName: function (displayName) {
         return `//div[contains(@id,'NamesView') and child::h6[contains(@class,'main-name') and contains(.,'${displayName}')]]`
