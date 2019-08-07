ebaca4f007001b51f3d0af81fe2dcac4c2ad4752
Alex Shklianko <asl@enonic.com>
2019-08-07 10:46:00 +0300

    Apply styling to content items in the Grid based on inherited property #672

--- src/main/resources/assets/js/app/browse/action/ContentTreeGridActions.ts
+++ src/main/resources/assets/js/app/browse/action/ContentTreeGridActions.ts
@@ -249,10 +249,11 @@ export class ContentTreeGridActions implements TreeGridActions<ContentSummaryAnd
 
         let treePublishEnabled: boolean = true;
         let unpublishEnabled: boolean = true;
+        const anyIsInherited: boolean = this.anyInherited(contentSummaries);
 
-        const deleteEnabled: boolean = this.anyDeletable(contentSummaries) && noManagedActionExecuting;
-        const duplicateEnabled: boolean = contentSummaries.length >= 1 && noManagedActionExecuting;
-        const moveEnabled: boolean = !this.isAllItemsSelected(contentBrowseItems.length) && noManagedActionExecuting;
+        const deleteEnabled: boolean = !anyIsInherited && this.anyDeletable(contentSummaries) && noManagedActionExecuting;
+        const duplicateEnabled: boolean = !anyIsInherited && contentSummaries.length >= 1 && noManagedActionExecuting;
+        const moveEnabled: boolean = !anyIsInherited && !this.isAllItemsSelected(contentBrowseItems.length) && noManagedActionExecuting;
 
         let allAreOnline: boolean = contentBrowseItems.length > 0;
         let allArePendingDelete: boolean = contentBrowseItems.length > 0;
@@ -300,7 +301,7 @@ export class ContentTreeGridActions implements TreeGridActions<ContentSummaryAnd
             DELETE: deleteEnabled,
             DUPLICATE: duplicateEnabled,
             MOVE: moveEnabled,
-            SORT: contentSummaries.length === 1 && contentSummaries[0].hasChildren(),
+            SORT: contentSummaries.length === 1 && contentSummaries[0].hasChildren() && !contentSummaries[0].isInherited(),
             PUBLISH: publishEnabled,
             PUBLISH_TREE: treePublishEnabled,
             UNPUBLISH: unpublishEnabled,
@@ -491,7 +492,17 @@ export class ContentTreeGridActions implements TreeGridActions<ContentSummaryAnd
         });
     }
 
+    private anyInherited(contentSummaries: api.content.ContentSummary[]): boolean {
+        return contentSummaries.some((content) => {
+            return !!content && content.isInherited();
+        });
+    }
+
     private updateCanDuplicateActionSingleItemSelected(selectedItem: ContentSummary): wemQ.Promise<void> {
+        if (selectedItem.isInherited()) {
+            return wemQ(null);
+        }
+
         // Need to check if parent allows content creation
         return new GetContentByPathRequest(selectedItem.getPath().getParentPath()).sendAndParse().then((content: Content) =>
             new GetPermittedActionsRequest()
