e0b02e60b0a9600eebe8b94b6b646de3c9049de5
Siarhei Hauruseu <sig@enonic.com>
2019-09-04 11:22:11 +0300

    Update ui-tests in order to match new changes in Issues List modal dialog issue#901

---
+++ testing/specs/content-types/htmlarea.insert.image.dialog.spec.js
@@ -0,0 +1,46 @@
+/**
+ * Created on 11.01.2019.
+ */
+const chai = require('chai');
+chai.use(require('chai-as-promised'));
+const expect = chai.expect;
+const assert = chai.assert;
+const webDriverHelper = require('../../libs/WebDriverHelper');
+const appConstant = require('../../libs/app_const');
+const studioUtils = require('../../libs/studio.utils.js');
+const contentBuilder = require("../../libs/content.builder");
+const HtmlAreaForm = require('../../page_objects/wizardpanel/htmlarea.form.panel');
+const InsertImageDialog = require('../../page_objects/wizardpanel/insert.image.dialog.cke');
+const ContentWizard = require('../../page_objects/wizardpanel/content.wizard.panel');
+
+describe('htmlarea.insert.image.dialog.spec: open insert image dialog.',
+    function () {
+        this.timeout(appConstant.SUITE_TIMEOUT);
+        webDriverHelper.setupBrowser();
+        let SITE;
+
+        it(`Preconditions: new site should be created`,
+            async () => {
+                let displayName = contentBuilder.generateRandomName('site');
+                SITE = contentBuilder.buildSite(displayName, 'description', [appConstant.APP_CONTENT_TYPES]);
+                await studioUtils.doAddSite(SITE);
+            });
+
+        it(`GIVEN 'Insert Image' dialog is opened WHEN ESC key has been pressed THEN dialog should be closed`,
+            async () => {
+                let contentWizard = new ContentWizard();
+                let htmlAreaForm = new HtmlAreaForm();
+                let insertImageDialog = new InsertImageDialog();
+                await studioUtils.selectSiteAndOpenNewWizard(SITE.displayName, 'htmlarea0_1');
+                await contentWizard.pause(700);
+                await htmlAreaForm.showToolbarAndClickOnInsertImageButton();
+                studioUtils.saveScreenshot("insert_image_esc_test1");
+                await insertImageDialog.waitForDialogVisible();
+                await contentWizard.pressEscKey();
+                studioUtils.saveScreenshot("insert_image_esc_test2");
+                await insertImageDialog.waitForDialogClosed();
+            });
+
+        beforeEach(() => studioUtils.navigateToContentStudioApp());
+        afterEach(() => studioUtils.doCloseAllWindowTabsAndSwitchToHome());
+    });
