df3e41e3fd2443873002b3403890b6bb928b9208
Pavel Milkevich <pavel.milkevich@gmail.com>
2019-09-24 18:36:50 +0300

    Change default behaviour of Delete Content action #987

--- src/main/resources/assets/js/app/remove/ContentDeleteDialogAction.ts
+++ src/main/resources/assets/js/app/remove/ContentDeleteDialogAction.ts
@@ -1,9 +1,10 @@
 import '../../api.ts';
 import i18n = api.util.i18n;
 
-export class ContentDeleteDialogAction extends api.ui.Action {
+export class ContentDeleteDialogAction
+    extends api.ui.Action {
     constructor() {
-        super(i18n('action.delete'));
+        super(i18n('dialog.delete.now'));
         this.setIconClass('delete-action');
     }
 }
