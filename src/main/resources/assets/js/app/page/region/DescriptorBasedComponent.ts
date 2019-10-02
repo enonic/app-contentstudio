253dd22ee3848c0160cada1a57a122358f60d052
Pavel Milkevich <pavel.milkevich@gmail.com>
2019-10-02 18:01:24 +0300

    Show part icon in Page Components View #1027

--- src/main/resources/assets/js/app/page/region/DescriptorBasedComponent.ts
+++ src/main/resources/assets/js/app/page/region/DescriptorBasedComponent.ts
@@ -14,6 +14,8 @@ export abstract class DescriptorBasedComponent
 
     private description: string;
 
+    private icon: string;
+
     constructor(builder: DescriptorBasedComponentBuilder<DescriptorBasedComponent>) {
         super(builder);
 
@@ -31,6 +33,8 @@ export abstract class DescriptorBasedComponent
     setDescriptor(descriptor: Descriptor) {
         const oldDescriptorKeyValue = this.descriptorKey;
         this.descriptorKey = descriptor ? descriptor.getKey() : null;
+
+        this.icon = descriptor ? descriptor.getIcon() : null;
 
         this.setName(descriptor ? new ComponentName(descriptor.getDisplayName()) : this.getType().getDefaultName());
         this.description = descriptor ? descriptor.getDescription() : null;
@@ -61,6 +65,10 @@ export abstract class DescriptorBasedComponent
 
     setDescription(value: string) {
         this.description = value;
+    }
+
+    getIcon(): string {
+        return this.icon;
     }
 
     doReset() {
