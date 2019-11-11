c3df60c7c0b1f52322b21192e8eacd1680d9c131
Siarhei Hauruseu <sig@enonic.com>
2019-11-11 16:50:12 +0300

    ui-tests - issue#1034

--- testing/specs/publish/content.publish.dialog.spec.js
+++ testing/specs/publish/content.publish.dialog.spec.js
@@ -2,8 +2,6 @@
  * Created on 22.07.2019.
  */
 const chai = require('chai');
-chai.use(require('chai-as-promised'));
-const expect = chai.expect;
 const assert = chai.assert;
 const webDriverHelper = require('../../libs/WebDriverHelper');
 const appConst = require('../../libs/app_const');
@@ -20,34 +18,32 @@ describe('content.publish.dialog.spec - opens publish modal dialog and checks co
     let PARENT_FOLDER;
     let CHILD_FOLDER;
 
-    let SITE;
-    it(`GIVEN folder is opened AND 'Marked as ready' done WHEN publish dialog has been opened AND 'Publish' button pressed THEN 'New' status should be displayed on the dialog`,
+    it(`GIVEN folder is opened AND 'Marked as ready' is done WHEN publish dialog has been opened THEN 'New' status should be displayed in the dialog`,
         async () => {
             let contentWizard = new ContentWizard();
             let contentPublishDialog = new ContentPublishDialog();
             FOLDER1_NAME = contentBuilder.generateRandomName('folder');
             await studioUtils.openContentWizard(appConst.contentTypes.FOLDER);
             await contentWizard.typeDisplayName(FOLDER1_NAME);
-            //the folder should be marked as ready.
+            //1. Click on 'MARK AS READY' default action:
             await contentWizard.clickOnMarkAsReadyButton();
+            //2. Open Publish Wizard:
             await contentWizard.clickOnPublishButton();
             studioUtils.saveScreenshot("wizard_publish_dialog_single_folder");
             let status = await contentPublishDialog.getContentStatus(FOLDER1_NAME);
-
+            //3. New status should be displayed in the modal dialog:
             assert.equal(status, "New", "'New' status should be displayed in the dialog");
             let isPresent = await contentPublishDialog.isAddScheduleButtonDisplayed();
             assert.isTrue(isPresent, "Add schedule button should be displayed");
-
+            //This item should not be removable:
             let isRemovable = await contentPublishDialog.isPublishItemRemovable(FOLDER1_NAME);
             assert.isFalse(isRemovable, "One publish item should be displayed and it should not be removable");
         });
 
-    it(`GIVEN existing folder(no children) is selected in grid WHEN 'Publish' button has been pressed THEN 'New' status should be displayed on the dialog`,
+    it(`GIVEN existing folder(no children) is selected WHEN 'Publish' button has been pressed THEN 'New' status should be displayed in the modal dialog`,
         async () => {
-
             let contentPublishDialog = new ContentPublishDialog();
             let contentBrowsePanel = new ContentBrowsePanel();
-
             await studioUtils.findAndSelectItem(FOLDER1_NAME);
             //Click on 'Publish...' button
             await contentBrowsePanel.clickOnPublishButton();
@@ -59,16 +55,15 @@ describe('content.publish.dialog.spec - opens publish modal dialog and checks co
             let isRemovable = await contentPublishDialog.isPublishItemRemovable(FOLDER1_NAME);
             assert.isFalse(isRemovable, "One publish item should be displayed and it should not be removable");
 
-            //the folder has no children!
+            //Include Children toggler should not be displayed, the folder has no children!
             let isDisplayed = await contentPublishDialog.isIncludeChildTogglerDisplayed();
             assert.isFalse(isDisplayed, "Include child icon should not be visible");
         });
 
-    it(`GIVEN existing folder with children is selected in grid WHEN 'Publish' button has been pressed THEN expected control elements should be displayed on the dialog`,
+    it(`GIVEN existing folder with children is selected WHEN 'Publish' button has been pressed THEN expected control elements should be displayed in the dialog`,
         async () => {
             let contentPublishDialog = new ContentPublishDialog();
             let contentBrowsePanel = new ContentBrowsePanel();
-
             await studioUtils.findAndSelectItem(appConst.TEST_FOLDER_NAME);
             //Click on 'Publish...' button
             await contentBrowsePanel.clickOnPublishButton();
@@ -89,8 +84,7 @@ describe('content.publish.dialog.spec - opens publish modal dialog and checks co
 
             //'Publish Now' button should be enabled!
             let result = await contentPublishDialog.waitForPublishNowButtonEnabled();
-
-            //Log message link should be displayed
+            //Log message link should be displayed:
             let isLinkDisplayed = await contentPublishDialog.isLogMessageLinkDisplayed();
             assert.isTrue(isLinkDisplayed, "Log message link should be displayed");
         });
@@ -99,43 +93,34 @@ describe('content.publish.dialog.spec - opens publish modal dialog and checks co
         async () => {
             let contentPublishDialog = new ContentPublishDialog();
             let contentBrowsePanel = new ContentBrowsePanel();
-
             await studioUtils.findAndSelectItem(appConst.TEST_FOLDER_NAME);
             //Click on 'Publish...' button
             await contentBrowsePanel.clickOnPublishButton();
-
             //'Include children' has been clicked
             await contentPublishDialog.clickOnIncludeChildrenToogler();
-
             //'Show Dependent items' gets visible!
             await contentPublishDialog.waitForShowDependentButtonDisplayed();
 
             let items = await contentPublishDialog.getNumberItemsToPublish();
             assert.equal(items, 13, "13 items to publish should be in the dialog");
-
         });
 
     it(`GIVEN folder is selected AND Publish dialog is opened WHEN 'Include children' and Show dependent items button have been pressed THEN 'Hide dependent items' gets visible`,
         async () => {
             let contentPublishDialog = new ContentPublishDialog();
             let contentBrowsePanel = new ContentBrowsePanel();
-
             await studioUtils.findAndSelectItem(appConst.TEST_FOLDER_NAME);
-            //Click on 'Publish...' button
+            //Click on 'Publish...' button (open Publish Wizard)
             await contentBrowsePanel.clickOnPublishButton();
-
             //'Include children' has been clicked
             await contentPublishDialog.clickOnIncludeChildrenToogler();
-
             //'Show Dependent items' has been clicked
             await contentPublishDialog.clickOnShowDependentItems();
             //Hide dependant items gets visible.
             await contentPublishDialog.waitForHideDependentItemsDisplayed();
-
             //child item should be removable.
             let isRemovable = await contentPublishDialog.isPublishItemRemovable(appConst.TEST_IMAGES.BOOK);
             assert.isTrue(isRemovable, "Child item should be removable");
-
         });
 
     it(`GIVEN 'Content Publish' dialog is opened WHEN cancel button on the bottom has been clicked THEN dialog is closing`,
@@ -146,7 +131,6 @@ describe('content.publish.dialog.spec - opens publish modal dialog and checks co
             await studioUtils.findAndSelectItem(appConst.TEST_FOLDER_NAME);
             await contentBrowsePanel.clickOnPublishButton();
             await contentPublishDialog.waitForDialogOpened();
-
             // "button 'Cancel' on the bottom of dialog has been pressed"
             await contentPublishDialog.clickOnCancelTopButton();
             // "dialog is closing. Otherwise, exception will be thrown after the timeout."
@@ -165,7 +149,7 @@ describe('content.publish.dialog.spec - opens publish modal dialog and checks co
             await studioUtils.findAndSelectItem(PARENT_FOLDER.displayName);
             await studioUtils.doAddReadyFolder(CHILD_FOLDER);
 
-            //opens Publish Content dialog
+            //Opens Publish Content dialog
             await contentBrowsePanel.clickOnPublishButton();
             await contentPublishDialog.waitForDialogOpened();
 
@@ -176,7 +160,6 @@ describe('content.publish.dialog.spec - opens publish modal dialog and checks co
             let result = await contentPublishDialog.getItemsToPublish();
             assert.isTrue(result.length === 1, "Only one item should be present in the dialog");
             assert.equal(result[0], PARENT_FOLDER.displayName, "Parent folder should be in items to publish");
-
         });
 
     it(`GIVEN existing parent folder(ready to publish) is selected and 'PublishDialog' is opened WHEN child has been removed AND Publish Now has been pressed THEN only parent folder should be published`,
@@ -184,19 +167,16 @@ describe('content.publish.dialog.spec - opens publish modal dialog and checks co
             let contentBrowsePanel = new ContentBrowsePanel();
             let contentPublishDialog = new ContentPublishDialog();
             let contentWizard = new ContentWizard();
-
             await studioUtils.selectContentAndOpenWizard(PARENT_FOLDER.displayName);
             await contentWizard.clickOnPublishButton();
             await contentPublishDialog.clickOnIncludeChildrenToogler();
             await contentPublishDialog.clickOnShowDependentItems();
             //Dependent item has been removed on the modal dialog
             await contentPublishDialog.removeDependentItem(CHILD_FOLDER.displayName);
-
             //click on 'Publish Now'
             await contentPublishDialog.clickOnPublishNowButton();
             await contentPublishDialog.waitForDialogClosed();
             await studioUtils.doCloseWizardAndSwitchToGrid();
-
 
             let parentStatus = await contentBrowsePanel.getContentStatus(PARENT_FOLDER.displayName);
             assert.equal(parentStatus, appConst.CONTENT_STATUS.PUBLISHED, "Parent folder should be 'PUBLISHED'");
@@ -206,18 +186,9 @@ describe('content.publish.dialog.spec - opens publish modal dialog and checks co
             assert.equal(childStatus, appConst.CONTENT_STATUS.NEW, "child folder should be 'New'");
         });
 
-
     beforeEach(() => studioUtils.navigateToContentStudioApp());
     afterEach(() => studioUtils.doCloseAllWindowTabsAndSwitchToHome());
     before(() => {
         return console.log('specification is starting: ' + this.title);
     });
-
-    function addNotValidFolder() {
-        return studioUtils.findAndSelectItem(SITE.displayName).then(() => {
-            return studioUtils.openContentWizard(appConstant.contentTypes.FOLDER);
-        }).then(() => {
-            return studioUtils.doCloseWizardAndSwitchToGrid();
-        });
-    }
 });
