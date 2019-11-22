003910d3347b75a5e706f447817e3ca388fac129
Viachaslau Bradnitski <vbr@enonic.com>
2019-11-22 16:50:10 +0300

    Image Wizard is not refreshed on version revert after binary is updated #1180

--- src/main/resources/assets/js/app/inputtype/upload/ImageUploader.ts
+++ src/main/resources/assets/js/app/inputtype/upload/ImageUploader.ts
@@ -79,6 +79,10 @@ export class ImageUploader
             api.notify.showFeedback(content.getDisplayName() + ' saved');
         });
 
+        this.imageUploader.onUploadCompleted(() => {
+            this.imageUploader.resetBaseValues();
+        });
+
         this.imageUploader.onUploadReset(() => {
             this.saveToProperty(this.newInitialValue());
             this.imageUploader.getUploadButton().show();
