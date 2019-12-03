dedd987fffe5ea81089fc5c8a2affc53e6481fac
Siarhei Hauruseu <sig@enonic.com>
2019-12-03 13:31:06 +0300

    ui-tests for Closed/Open buttons in Issue List dialog issue#1268

--- testing/specs/issue/issue.list.typefilter.spec.js
+++ testing/specs/issue/issue.list.typefilter.spec.js
@@ -11,12 +11,14 @@ const TaskDetailsDialog = require('../../page_objects/issue/task.details.dialog'
 const contentBuilder = require("../../libs/content.builder");
 const ContentBrowsePanel = require('../../page_objects/browsepanel/content.browse.panel');
 const IssueListDialog = require('../../page_objects/issue/issue.list.dialog');
+const CreateRequestPublishDialog = require('../../page_objects/issue/create.request.publish.dialog');
 
 describe(`issue.list.type.filter.spec: tests 'Type Filter' in Issues List modal dialog`, function () {
     this.timeout(csConst.SUITE_TIMEOUT);
     webDriverHelper.setupBrowser();
-    let issueTitle = csConst.generateRandomName('task');
+    let TASK_TITLE = csConst.generateRandomName('task');
     let TEST_FOLDER;
+    let PUBLISH_REQUEST_TITLE = "my first request";
 
     it(`Precondition: new folder and new task should be created`,
         async () => {
@@ -30,10 +32,22 @@ describe(`issue.list.type.filter.spec: tests 'Type Filter' in Issues List modal 
             await studioUtils.findAndSelectItem(TEST_FOLDER.displayName);
             await contentBrowsePanel.waitForPublishButtonVisible();
             await contentBrowsePanel.openPublishMenuAndClickOnCreateTask();
-            await createTaskDialog.typeTitle(issueTitle);
+            await createTaskDialog.typeTitle(TASK_TITLE);
             //2. Create new task:
             await createTaskDialog.clickOnCreateTaskButton();
             await taskDetailsDialog.waitForDialogOpened();
+        });
+
+    it(`Precondition: new request publish should be added`,
+        async () => {
+            let contentBrowsePanel = new ContentBrowsePanel();
+            let createRequestPublishDialog = new  CreateRequestPublishDialog();
+            await studioUtils.findAndSelectItem(TEST_FOLDER.displayName);
+            await contentBrowsePanel.openPublishMenuSelectItem(csConst.PUBLISH_MENU.REQUEST_PUBLISH);
+            await createRequestPublishDialog.waitForDialogLoaded();
+            await createRequestPublishDialog.clickOnNextButton();
+            await createRequestPublishDialog.typeInChangesInput(PUBLISH_REQUEST_TITLE);
+            await createRequestPublishDialog.clickOnCreateRequestButton();
         });
 
     it(`GIVEN Issue List dialog is opened WHEN dropdown handle in Type Filter selector has been clicked THEN expected options should be present in the selector`,
@@ -79,6 +93,98 @@ describe(`issue.list.type.filter.spec: tests 'Type Filter' in Issues List modal 
             //4. All option should not be disabled:
             let result = await issueListDialog.isFilterOptionDisabled("All");
             assert.isFalse(result, "All' option should not be disabled in 'Type Filter' selector");
+        });
+
+    it(`GIVEN Task Details dialog is opened WHEN task has been closed THEN number in Open/Closed buttons should be updated`,
+        async () => {
+            let issueListDialog = new IssueListDialog();
+            let taskDetailsDialog = new TaskDetailsDialog();
+            //1.Open Issue List dialog:
+            await studioUtils.openIssuesListDialog();
+            let closedNumberBeforeClose = await issueListDialog.getNumberInClosedButton();
+            let openNumberBeforeClose = await issueListDialog.getNumberInOpenButton();
+            let filterInputNumberBeforeClose = await issueListDialog.getNumberInSelectedOption();
+            //2. Click on the task, open Task Details dialog:
+            await issueListDialog.clickOnIssue(TASK_TITLE);
+            await taskDetailsDialog.waitForDialogOpened();
+            //3. Close the task:
+            await taskDetailsDialog.clickOnCloseIssueButton();
+            await taskDetailsDialog.clickOnBackButton();
+            await issueListDialog.pause(500);
+            let closedNumber = await issueListDialog.getNumberInClosedButton();
+
+            //4. Number of closed and Open  issues should be updated:
+            await issueListDialog.isOpenButtonActive();
+            let openNumber = await issueListDialog.getNumberInOpenButton();
+            let filterInputNumber = await issueListDialog.getNumberInSelectedOption();
+            //Number in 'All()' should be reduced, because 'Open' button is active:
+            assert.isTrue(filterInputNumberBeforeClose - filterInputNumber === 1, "");
+            //Number of Open issues should be reduced:
+            assert.isTrue(openNumberBeforeClose - openNumber === 1, "");
+            //Number of closed issues should be increased:
+            assert.isTrue(closedNumber - closedNumberBeforeClose === 1, "");
+        });
+
+    //verifies https://github.com/enonic/app-contentstudio/issues/1233
+    it(`GIVEN Task Details dialog is opened WHEN task has been reopened THEN number in Open/Closed button should be updated`,
+        async () => {
+            let issueListDialog = new IssueListDialog();
+            let taskDetailsDialog = new TaskDetailsDialog();
+            //1.Open 'Issue List' dialog:
+            await studioUtils.openIssuesListDialog();
+            await issueListDialog.clickOnClosedButton();
+            let closedNumberBeforeReopen = await issueListDialog.getNumberInClosedButton();
+            let openNumberBeforeReopen = await issueListDialog.getNumberInOpenButton();
+            let filterInputNumberBeforeReopen = await issueListDialog.getNumberInSelectedOption();
+            //2. Click on 'Closed' button, load Closed-issues then click on the closed task( open 'Task Details' dialog):
+            await issueListDialog.clickOnIssue(TASK_TITLE);
+            await taskDetailsDialog.waitForDialogOpened();
+            //3. Click on 'Reopen Issue' button and reopen the task:
+            await taskDetailsDialog.clickOnReopenIssueButton();
+            //4. Go to Issue List  dialog:
+            await taskDetailsDialog.clickOnBackButton();
+            await issueListDialog.pause(500);
+            let closedNumber = await issueListDialog.getNumberInClosedButton();
+            //4. 'Closed()' button should be active:
+            await issueListDialog.isClosedButtonActive();
+            let openNumber = await issueListDialog.getNumberInOpenButton();
+            let filterInputNumber = await issueListDialog.getNumberInSelectedOption();
+            //Number in 'All()' should be reduced, because 'Closed' button is active:
+            assert.isTrue(filterInputNumberBeforeReopen - filterInputNumber === 1, "number in 'All' should be reduced");
+            //Number of Open issues should be increased:
+            assert.isTrue(openNumber - openNumberBeforeReopen === 1, "Number of open-issues should be increased - 'Open()' button)");
+            //Number of closed issues should be reduced:
+            assert.isTrue(closedNumberBeforeReopen - closedNumber === 1, "Number of closed issues should be reduced- 'Closed()' button");
+        });
+
+    it(`GIVEN Issue List dialog is opened WHEN 'Tasks' option item has been selected THEN publish requests should not be displayed in the dialog`,
+        async () => {
+            let issueListDialog = new IssueListDialog();
+            let taskDetailsDialog = new TaskDetailsDialog();
+            //1.Open 'Issue List' dialog:
+            await studioUtils.openIssuesListDialog();
+            //2. Select 'Tasks' in the filter:
+            await issueListDialog.selectTypeFilterOption(csConst.ISSUE_LIST_TYPE_FILTER.TASKS);
+            studioUtils.saveScreenshot("typefilter_tasks");
+            //Publish request should not be present:
+            await issueListDialog.waitForIssueNotPresent(PUBLISH_REQUEST_TITLE);
+            // but the task should be present:
+            await issueListDialog.waitForIssuePresent(TASK_TITLE);
+        });
+
+    it(`GIVEN Issue List dialog is opened WHEN 'Publish Requests' option item has been selected THEN existing task should not be displayed in the dialog`,
+        async () => {
+            let issueListDialog = new IssueListDialog();
+            let taskDetailsDialog = new TaskDetailsDialog();
+            //1.Open 'Issue List' dialog:
+            await studioUtils.openIssuesListDialog();
+            //2. Select 'Publish Requests' in the filter:
+            await issueListDialog.selectTypeFilterOption(csConst.ISSUE_LIST_TYPE_FILTER.PUBLISH_REQUESTS);
+            studioUtils.saveScreenshot("typefilter_requests");
+            //Publish request should be present:
+            await issueListDialog.waitForIssuePresent(PUBLISH_REQUEST_TITLE);
+            // But the task should not be present:
+            await issueListDialog.waitForIssueNotPresent(TASK_TITLE);
         });
 
     beforeEach(() => studioUtils.navigateToContentStudioApp());
