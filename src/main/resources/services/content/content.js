04588e96d4762d78d6c825891a0fd69a5cfaf499
Alan Semenov <ase@enonic.com>
2019-12-13 12:06:46 +0100

    Implemented service for fetching specific version of a content (#1332)

---
+++ src/main/resources/services/content/content.js
@@ -0,0 +1,26 @@
+var contentLib = require('/lib/xp/content');
+
+exports.get = function (req) {
+    var contentId = req.params.contentId;
+    if (!contentId) {
+        return {
+            status: 400,
+            contentType: 'text/plain',
+            body: 'Missing required parameter: contentId'
+        }
+    }
+
+    return {
+        status: 200,
+        contentType: 'application/json',
+        body: getContentByIdAndVersion(contentId, req.params.versionId)
+    }
+};
+
+var getContentByIdAndVersion = function(contentId, versionId) {
+
+    return contentLib.get({
+        key: contentId,
+        versionId: versionId
+    });
+};
