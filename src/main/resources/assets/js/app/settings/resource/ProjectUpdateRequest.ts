816826c0aff915067335a67285b91ada4193f903
Alex Shklianko <asl@enonic.com>
2019-12-23 17:31:48 +0300

    Implement ProjectUpdateRequest #1350

---
+++ src/main/resources/assets/js/app/settings/resource/ProjectUpdateRequest.ts
@@ -0,0 +1,53 @@
+import {ProjectResourceRequest} from './ProjectResourceRequest';
+import {ProjectItemJson} from './json/ProjectItemJson';
+import {ProjectItem} from '../ProjectItem';
+import {Path} from 'lib-admin-ui/rest/Path';
+import {JsonResponse} from 'lib-admin-ui/rest/JsonResponse';
+
+export class ProjectUpdateRequest
+    extends ProjectResourceRequest<ProjectItemJson, ProjectItem> {
+
+    private name: string;
+
+    private displayName: string;
+
+    private description: string;
+
+    constructor() {
+        super();
+        this.setMethod('POST');
+    }
+
+    setName(value: string): ProjectUpdateRequest {
+        this.name = value;
+        return this;
+    }
+
+    setDisplayName(value: string): ProjectUpdateRequest {
+        this.displayName = value;
+        return this;
+    }
+
+    setDescription(value: string): ProjectUpdateRequest {
+        this.description = value;
+        return this;
+    }
+
+    getRequestPath(): Path {
+        return Path.fromParent(super.getResourcePath(), 'modify');
+    }
+
+    getParams(): Object {
+        return {
+            name: this.name,
+            displayName: this.displayName,
+            description: this.description
+        };
+    }
+
+    sendAndParse(): Q.Promise<ProjectItem> {
+        return this.send().then((response: JsonResponse<ProjectItemJson>) => {
+            return ProjectItem.fromJson(response.getResult());
+        });
+    }
+}
