ac1f9d2eef270f2689d0397c21abd1251c92aedc
Alex Shklianko <asl@enonic.com>
2019-12-23 16:24:43 +0300

    Implement ProjectGetRequest #1345

---
+++ src/main/resources/assets/js/app/settings/resource/ProjectGetRequest.ts
@@ -0,0 +1,33 @@
+import {ProjectResourceRequest} from './ProjectResourceRequest';
+import {ProjectItemJson} from './json/ProjectItemJson';
+import {ProjectItem} from '../ProjectItem';
+import {Path} from 'lib-admin-ui/rest/Path';
+import {JsonResponse} from 'lib-admin-ui/rest/JsonResponse';
+
+export class ProjectGetRequest
+    extends ProjectResourceRequest<ProjectItemJson, ProjectItem> {
+
+    private name: string;
+
+    constructor(name: string) {
+        super();
+
+        this.name = name;
+    }
+
+    getRequestPath(): Path {
+        return Path.fromParent(super.getResourcePath(), 'get');
+    }
+
+    getParams(): Object {
+        return {
+            name: this.name
+        };
+    }
+
+    sendAndParse(): Q.Promise<ProjectItem> {
+        return this.send().then((response: JsonResponse<ProjectItemJson>) => {
+            return ProjectItem.fromJson(response.getResult());
+        });
+    }
+}
