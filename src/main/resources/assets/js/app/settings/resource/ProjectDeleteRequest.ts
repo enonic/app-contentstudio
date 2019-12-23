1760691ee807b2c3e5ac2afee1dde16bfe0a86e5
Alex Shklianko <asl@enonic.com>
2019-12-23 16:47:00 +0300

    Implement ProjectDeleteRequest #1351

---
+++ src/main/resources/assets/js/app/settings/resource/ProjectDeleteRequest.ts
@@ -0,0 +1,32 @@
+import {ProjectResourceRequest} from './ProjectResourceRequest';
+import {Path} from 'lib-admin-ui/rest/Path';
+import {JsonResponse} from 'lib-admin-ui/rest/JsonResponse';
+
+export class ProjectDeleteRequest
+    extends ProjectResourceRequest<boolean, boolean> {
+
+    private name: string;
+
+    constructor(name: string) {
+        super();
+        super.setMethod('POST');
+        this.name = name;
+    }
+
+    getRequestPath(): Path {
+        return Path.fromParent(super.getResourcePath(), 'delete');
+    }
+
+    getParams(): Object {
+        return {
+            name: this.name
+        };
+    }
+
+    sendAndParse(): Q.Promise<boolean> {
+        return this.send().then((response: JsonResponse<boolean>) => {
+            return !!response.getResult();
+        });
+    }
+
+}
