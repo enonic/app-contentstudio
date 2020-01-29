bb85d0514b12e40e69302ab93b73cbf55c881a25
Alex Shklianko <asl@enonic.com>
2020-01-29 11:04:37 +0100

    "Source" icon is not available for HTML Area inside item-set in part config #1446
    -Adding editor to a list of managed editors only after it is fully initialized, otherwise it might be triggered to reinitialize (while initializing itself) during parent element rendering

--- src/main/resources/assets/js/app/inputtype/text/HtmlArea.ts
+++ src/main/resources/assets/js/app/inputtype/text/HtmlArea.ts
@@ -90,14 +90,16 @@ export class HtmlArea
 
         const textAreaWrapper = new api.dom.DivEl();
 
-        this.editors.push({id: editorId, textAreaWrapper, textAreaEl, property, hasStickyToolbar: false});
-
         textAreaEl.onRendered(() => {
             if (this.authRequest.isFulfilled()) {
-                this.initEditor(editorId, property, textAreaWrapper);
+                this.initEditor(editorId, property, textAreaWrapper).then(() => {
+                    this.editors.push({id: editorId, textAreaWrapper, textAreaEl, property, hasStickyToolbar: false});
+                });
             } else {
                 this.authRequest.then(() => {
-                    this.initEditor(editorId, property, textAreaWrapper);
+                    this.initEditor(editorId, property, textAreaWrapper).then(() => {
+                        this.editors.push({id: editorId, textAreaWrapper, textAreaEl, property, hasStickyToolbar: false});
+                    });
                 });
             }
         });
