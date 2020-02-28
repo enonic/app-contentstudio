17bb24b67d7f25f4e83fcae901c64bda3da24f3c
Alex Shklianko <asl@enonic.com>
2020-02-28 14:27:05 +0300

    Revert "Move Permissions to the first step of the Project Wizard #1546"
    This reverts commit b710247

--- src/main/resources/assets/js/app/settings/wizard/SettingDataItemWizardStepForm.ts
+++ src/main/resources/assets/js/app/settings/wizard/SettingDataItemWizardStepForm.ts
@@ -54,9 +54,7 @@ export abstract class SettingDataItemWizardStepForm<ITEM extends SettingsDataVie
         });
     }
 
-    protected getFormItems(): FormItem[] {
-        return [this.createDescriptionFormItem()];
-    }
+    protected abstract getFormItems(): FormItem[];
 
     protected initListeners() {
         this.descriptionInput.onValueChanged(() => {
@@ -71,16 +69,13 @@ export abstract class SettingDataItemWizardStepForm<ITEM extends SettingsDataVie
     }
 
     private addFormItems() {
+        const descriptionFormItem: FormItem = new FormItemBuilder(this.descriptionInput).setLabel(i18n('field.description')).build();
         const fieldSet: Fieldset = new Fieldset();
-
         this.getFormItems().forEach((formItem: FormItem) => {
             fieldSet.add(formItem);
         });
+        fieldSet.add(descriptionFormItem);
 
         this.form.add(fieldSet);
-    }
-
-    private createDescriptionFormItem(): FormItem {
-        return new FormItemBuilder(this.descriptionInput).setLabel(i18n('field.description')).build();
     }
 }
