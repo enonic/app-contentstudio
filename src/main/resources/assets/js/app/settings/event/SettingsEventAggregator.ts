614894c0cc16cadfae500876825a6bd5f3d472e8
Alex Shklianko <asl@enonic.com>
2020-04-14 15:44:11 +0300

    Updated project server event handling since project save/update can spawn up to 3 requests and multiple server update events.
    1. Handling indirect project events that occur on root /content node and on project roles
    2. Same node.update event is received 2 times, thus using event aggregator
    3. Refactored project event usage

---
+++ src/main/resources/assets/js/app/settings/event/SettingsEventAggregator.ts
@@ -0,0 +1,61 @@
+import {AppHelper} from 'lib-admin-ui/util/AppHelper';
+import {ProjectUpdatedEvent} from './ProjectUpdatedEvent';
+import {ProjectCreatedEvent} from './ProjectCreatedEvent';
+import {ProjectDeletedEvent} from './ProjectDeletedEvent';
+
+export class SettingsEventAggregator {
+
+    private static TIMEOUT: number = 1000;
+
+    private updateEvents: Map<string, Function> = new Map<string, Function>();
+
+    private createEvents: Map<string, Function> = new Map<string, Function>();
+
+    private deleteEvents: Map<string, Function> = new Map<string, Function>();
+
+    public appendUpdateEvent(projectName: string) {
+        if (this.updateEvents.get(projectName)) {
+            this.updateEvents.get(projectName)();
+        } else {
+            const debouncedFunc: Function = AppHelper.debounce(() => {
+                this.updateEvents.delete(projectName);
+                new ProjectUpdatedEvent(projectName).fire();
+            }, SettingsEventAggregator.TIMEOUT);
+
+            this.updateEvents.set(projectName, debouncedFunc);
+
+            debouncedFunc();
+        }
+    }
+
+    public appendCreateEvent(projectName: string) {
+        if (this.createEvents.get(projectName)) {
+            this.createEvents.get(projectName)();
+        } else {
+            const debouncedFunc: Function = AppHelper.debounce(() => {
+                this.createEvents.delete(projectName);
+                new ProjectCreatedEvent(projectName).fire();
+            }, SettingsEventAggregator.TIMEOUT);
+
+            this.createEvents.set(projectName, debouncedFunc);
+
+            debouncedFunc();
+        }
+    }
+
+    public appendDeleteEvent(projectName: string) {
+        if (this.deleteEvents.get(projectName)) {
+            this.deleteEvents.get(projectName)();
+        } else {
+            const debouncedFunc: Function = AppHelper.debounce(() => {
+                this.deleteEvents.delete(projectName);
+                new ProjectDeletedEvent(projectName).fire();
+            }, SettingsEventAggregator.TIMEOUT);
+
+            this.deleteEvents.set(projectName, debouncedFunc);
+
+            debouncedFunc();
+        }
+    }
+
+}
