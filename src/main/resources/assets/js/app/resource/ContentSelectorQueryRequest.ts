6e4d97f36853a1bf2292c79126abb58f445cf8c0
Alan Semenov <ase@enonic.com>
2020-05-12 11:26:34 +0200

    Extracted common code from `ContentSelectorQueryRequest` and `ContentTreeSelectorQueryRequest` into a new class `ContentSelectorRequest`

--- src/main/resources/assets/js/app/resource/ContentSelectorQueryRequest.ts
+++ src/main/resources/assets/js/app/resource/ContentSelectorQueryRequest.ts
@@ -1,171 +1,23 @@
 import * as Q from 'q';
 import {ContentSummary} from 'lib-admin-ui/content/ContentSummary';
 import {JsonResponse} from 'lib-admin-ui/rest/JsonResponse';
-import {OrderExpr} from 'lib-admin-ui/query/expr/OrderExpr';
-import {FieldOrderExpr} from 'lib-admin-ui/query/expr/FieldOrderExpr';
-import {OrderDirection} from 'lib-admin-ui/query/expr/OrderDirection';
-import {FieldExpr} from 'lib-admin-ui/query/expr/FieldExpr';
-import {Expression} from 'lib-admin-ui/query/expr/Expression';
-import {QueryField} from 'lib-admin-ui/query/QueryField';
-import {QueryExpr} from 'lib-admin-ui/query/expr/QueryExpr';
 import {ContentSummaryJson} from 'lib-admin-ui/content/json/ContentSummaryJson';
-import {ContentResourceRequest} from './ContentResourceRequest';
+import {ContentSelectorRequest} from './ContentSelectorRequest';
 import {ContentQueryResultJson} from './json/ContentQueryResultJson';
 import {ContentJson} from '../content/ContentJson';
 import {Expand} from 'lib-admin-ui/rest/Expand';
-import {PathMatchExpressionBuilder} from 'lib-admin-ui/query/PathMatchExpression';
-import {HttpMethod} from 'lib-admin-ui/rest/HttpMethod';
 
 export class ContentSelectorQueryRequest<CONTENT_JSON extends ContentSummaryJson, CONTENT extends ContentSummary>
-    extends ContentResourceRequest<CONTENT[]> {
-
-    public static DEFAULT_SIZE: number = 15;
-
-    public static MODIFIED_TIME_DESC: FieldOrderExpr = new FieldOrderExpr(new FieldExpr('modifiedTime'), OrderDirection.DESC);
-
-    public static SCORE_DESC: FieldOrderExpr = new FieldOrderExpr(new FieldExpr('_score'), OrderDirection.DESC);
-
-    public static DEFAULT_ORDER: OrderExpr[] = [ContentSelectorQueryRequest.SCORE_DESC, ContentSelectorQueryRequest.MODIFIED_TIME_DESC];
-
-    private queryExpr: QueryExpr;
-
-    private from: number = 0;
+    extends ContentSelectorRequest<CONTENT> {
 
     private loadingFrom: number;
 
-    private size: number = ContentSelectorQueryRequest.DEFAULT_SIZE;
-
-    private expand: Expand = Expand.SUMMARY;
-
-    private content: ContentSummary;
-
-    private inputName: string;
-
-    private contentTypeNames: string[] = [];
-
-    private allowedContentPaths: string[] = [];
-
-    private relationshipType: string;
-
-    private loaded: boolean;
-
-    private results: CONTENT[] = [];
+    protected size: number = 15;
 
     constructor() {
         super();
-        this.setMethod(HttpMethod.POST);
 
-        this.setSearchString();
         this.addRequestPathElements('selectorQuery');
-    }
-
-    setInputName(name: string) {
-        this.inputName = name;
-    }
-
-    getInputName(): string {
-        return this.inputName;
-    }
-
-    setContent(content: ContentSummary) {
-        this.content = content;
-    }
-
-    getContent(): ContentSummary {
-        return this.content;
-    }
-
-    setFrom(from: number) {
-        this.from = from;
-    }
-
-    getFrom(): number {
-        return this.from;
-    }
-
-    setSize(size: number) {
-        this.size = size;
-    }
-
-    getSize(): number {
-        return this.size;
-    }
-
-    setContentTypeNames(contentTypeNames: string[]) {
-        this.contentTypeNames = contentTypeNames;
-    }
-
-    setAllowedContentPaths(allowedContentPaths: string[]) {
-        this.allowedContentPaths = allowedContentPaths;
-    }
-
-    setRelationshipType(relationshipType: string) {
-        this.relationshipType = relationshipType;
-    }
-
-    setExpand(expand: Expand) {
-        this.expand = expand;
-    }
-
-    getExpand(): Expand {
-        return this.expand;
-    }
-
-    setSearchString(searchString: string = '') {
-        let fulltextExpression = this.createSearchExpression(searchString);
-
-        this.queryExpr = new QueryExpr(fulltextExpression, ContentSelectorQueryRequest.DEFAULT_ORDER);
-    }
-
-    setQueryExpr(queryExpr: QueryExpr) {
-        this.queryExpr = queryExpr;
-    }
-
-    private createSearchExpression(searchString: string): Expression {
-        return new PathMatchExpressionBuilder()
-            .setSearchString(searchString)
-            .setPath(this.content ? this.content.getPath().toString() : '')
-            .addField(new QueryField(QueryField.DISPLAY_NAME, 5))
-            .addField(new QueryField(QueryField.NAME, 3))
-            .addField(new QueryField(QueryField.ALL))
-            .build();
-    }
-
-    getQueryExpr(): QueryExpr {
-        return this.queryExpr;
-    }
-
-    isPartiallyLoaded(): boolean {
-        return this.results.length > 0 && !this.loaded;
-    }
-
-    isLoaded(): boolean {
-        return this.loaded;
-    }
-
-    resetParams() {
-        this.from = 0;
-        this.loaded = false;
-    }
-
-    getParams(): Object {
-        let queryExprAsString = this.getQueryExpr() ? this.getQueryExpr().toString() : '';
-
-        return {
-            queryExpr: queryExprAsString,
-            from: this.getFrom(),
-            size: this.getSize(),
-            expand: this.expandAsString(),
-            contentId: this.content ? this.content.getId().toString() : null,
-            inputName: this.getInputName(),
-            contentTypeNames: this.contentTypeNames,
-            allowedContentPaths: this.allowedContentPaths,
-            relationshipType: this.relationshipType
-        };
-    }
-
-    private isConcurrentLoad() {
-        return this.from === this.loadingFrom;
     }
 
     sendAndParse(): Q.Promise<CONTENT[]> {
@@ -180,27 +32,18 @@ export class ContentSelectorQueryRequest<CONTENT_JSON extends ContentSummaryJson
         });
     }
 
-    private expandAsString(): string {
-        switch (this.expand) {
-        case Expand.FULL:
-            return 'full';
-        case Expand.SUMMARY:
-            return 'summary';
-        case Expand.NONE:
-            return 'none';
-        default:
-            return 'summary';
-        }
+    private isConcurrentLoad() {
+        return this.from === this.loadingFrom;
     }
 
     protected parseResponse(response: JsonResponse<ContentQueryResultJson<CONTENT_JSON>>): CONTENT[] {
-        let responseResult: ContentQueryResultJson<CONTENT_JSON> = response.getResult();
+        const responseResult: ContentQueryResultJson<CONTENT_JSON> = response.getResult();
 
-        let contentsAsJson: ContentSummaryJson[] = responseResult.contents;
+        const contentsAsJson: ContentSummaryJson[] = responseResult.contents;
 
         let contents: CONTENT[];
 
-        if (this.expand === Expand.SUMMARY) {
+        if (this.getExpand() === Expand.SUMMARY) {
             contents = <any[]> this.fromJsonToContentSummaryArray(<ContentSummaryJson[]>contentsAsJson);
         } else {
             contents = <any[]>this.fromJsonToContentArray(<ContentJson[]>contentsAsJson);
--- src/main/resources/assets/js/app/resource/ContentTreeSelectorQueryRequest.ts
+++ src/main/resources/assets/js/app/resource/ContentTreeSelectorQueryRequest.ts
@@ -1,43 +1,16 @@
 import {ContentPath} from 'lib-admin-ui/content/ContentPath';
 import {ContentSummary} from 'lib-admin-ui/content/ContentSummary';
 import {JsonResponse} from 'lib-admin-ui/rest/JsonResponse';
-import {QueryExpr} from 'lib-admin-ui/query/expr/QueryExpr';
-import {Expression} from 'lib-admin-ui/query/expr/Expression';
-import {QueryField} from 'lib-admin-ui/query/QueryField';
 import {ChildOrder} from 'lib-admin-ui/content/order/ChildOrder';
-import {ContentResourceRequest} from './ContentResourceRequest';
-import {ContentSelectorQueryRequest} from './ContentSelectorQueryRequest';
 import {ContentTreeSelectorListJson} from './ContentTreeSelectorListResult';
 import {ContentTreeSelectorItem} from '../item/ContentTreeSelectorItem';
 import {ContentMetadata} from '../content/ContentMetadata';
-import {PathMatchExpressionBuilder} from 'lib-admin-ui/query/PathMatchExpression';
-import {Expand} from 'lib-admin-ui/rest/Expand';
-import {HttpMethod} from 'lib-admin-ui/rest/HttpMethod';
+import {ContentSelectorRequest} from './ContentSelectorRequest';
 
 export class ContentTreeSelectorQueryRequest<DATA extends ContentTreeSelectorItem>
-    extends ContentResourceRequest<DATA[]> {
+    extends ContentSelectorRequest<DATA> {
 
-    private queryExpr: QueryExpr;
-
-    private from: number = 0;
-
-    private size: number = 10;//ContentTreeSelectorQueryRequest.DEFAULT_SIZE;
-
-    private expand: Expand = Expand.SUMMARY;
-
-    private content: ContentSummary;
-
-    private inputName: string;
-
-    private contentTypeNames: string[] = [];
-
-    private allowedContentPaths: string[] = [];
-
-    private relationshipType: string;
-
-    private loaded: boolean;
-
-    private results: ContentSummary[] = [];
+    protected size: number = 10;
 
     private metadata: ContentMetadata;
 
@@ -47,61 +20,13 @@ export class ContentTreeSelectorQueryRequest<DATA extends ContentTreeSelectorIte
 
     constructor() {
         super();
-        this.setMethod(HttpMethod.POST);
 
-        this.setQueryExpr();
         this.addRequestPathElements('treeSelectorQuery');
     }
 
-    setInputName(name: string) {
-        this.inputName = name;
-    }
-
-    getInputName(): string {
-        return this.inputName;
-    }
-
     setContent(content: ContentSummary) {
-        this.content = content;
-        this.setQueryExpr();
-    }
-
-    getContent(): ContentSummary {
-        return this.content;
-    }
-
-    setFrom(from: number) {
-        this.from = from;
-    }
-
-    getFrom(): number {
-        return this.from;
-    }
-
-    setSize(size: number) {
-        this.size = size;
-    }
-
-    getSize(): number {
-        return this.size;
-    }
-
-    setContentTypeNames(contentTypeNames: string[]) {
-        this.contentTypeNames = contentTypeNames;
-    }
-
-    setAllowedContentPaths(allowedContentPaths: string[]) {
-        this.allowedContentPaths = allowedContentPaths;
-    }
-
-    setRelationshipType(relationshipType: string) {
-        this.relationshipType = relationshipType;
-    }
-
-    setQueryExpr(searchString: string = '') {
-        let fulltextExpression = this.createSearchExpression(searchString);
-
-        this.queryExpr = new QueryExpr(fulltextExpression, ContentSelectorQueryRequest.DEFAULT_ORDER);
+        super.setContent(content);
+        this.setSearchString();
     }
 
     setParentContent(content: ContentSummary) {
@@ -109,61 +34,12 @@ export class ContentTreeSelectorQueryRequest<DATA extends ContentTreeSelectorIte
         this.childOrder = content ? content.getChildOrder() : null;
     }
 
-    protected createSearchExpression(searchString: string): Expression {
-        return new PathMatchExpressionBuilder()
-            .setSearchString(searchString)
-            .setPath(this.content ? this.content.getPath().toString() : '')
-            .addField(new QueryField(QueryField.DISPLAY_NAME, 5))
-            .addField(new QueryField(QueryField.NAME, 3))
-            .addField(new QueryField(QueryField.ALL))
-            .build();
-    }
-
-    getAllowedContentPaths(): string[] {
-        return this.allowedContentPaths;
-    }
-
-    getContentTypeNames(): string[] {
-        return this.contentTypeNames;
-    }
-
-    getRelationshipType(): string {
-        return this.relationshipType;
-    }
-
-    getQueryExpr(): QueryExpr {
-        return this.queryExpr;
-    }
-
-    isPartiallyLoaded(): boolean {
-        return this.results.length > 0 && !this.loaded;
-    }
-
-    isLoaded(): boolean {
-        return this.loaded;
-    }
-
-    resetParams() {
-        this.from = 0;
-        this.loaded = false;
-    }
-
     getParams(): Object {
-        let queryExprAsString = this.getQueryExpr() ? this.getQueryExpr().toString() : '';
-
-        return {
-            queryExpr: queryExprAsString,
-            from: this.getFrom(),
-            size: this.getSize(),
-            expand: this.expandAsString(),
-            contentId: this.content ? this.content.getId().toString() : null,
-            inputName: this.getInputName(),
-            contentTypeNames: this.contentTypeNames,
-            allowedContentPaths: this.allowedContentPaths,
-            relationshipType: this.relationshipType,
+        let params = super.getParams();
+        return Object.assign(params, {
             parentPath: this.parentPath ? this.parentPath.toString() : null,
             childOrder: this.childOrder ? this.childOrder.toString() : ''
-        };
+        });
     }
 
     getMetadata(): ContentMetadata {
@@ -174,22 +50,9 @@ export class ContentTreeSelectorQueryRequest<DATA extends ContentTreeSelectorIte
         if (response.getResult() && response.getResult().items.length > 0) {
             this.metadata = new ContentMetadata(response.getResult().metadata['hits'], response.getResult().metadata['totalHits']);
             return response.getResult().items.map(json => <any>ContentTreeSelectorItem.fromJson(json));
-        } else {
-            this.metadata = new ContentMetadata(0, 0);
-            return [];
         }
-    }
 
-    private expandAsString(): string {
-        switch (this.expand) {
-        case Expand.FULL:
-            return 'full';
-        case Expand.SUMMARY:
-            return 'summary';
-        case Expand.NONE:
-            return 'none';
-        default:
-            return 'summary';
-        }
+        this.metadata = new ContentMetadata(0, 0);
+        return [];
     }
 }
