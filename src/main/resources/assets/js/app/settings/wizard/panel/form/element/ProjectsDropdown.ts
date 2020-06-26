6257e80516014ba1c0910686ea57b3f23ab9abc7
ashklianko <asl@enonic.com>
2020-06-26 16:08:36 +0300

    Show project inheritance chain in the project wizard #1958

--- src/main/resources/assets/js/app/settings/wizard/panel/form/element/ProjectsDropdown.ts
+++ src/main/resources/assets/js/app/settings/wizard/panel/form/element/ProjectsDropdown.ts
@@ -7,8 +7,11 @@ import {DefaultErrorHandler} from 'lib-admin-ui/DefaultErrorHandler';
 import {ProjectGetRequest} from '../../../../resource/ProjectGetRequest';
 import {ProjectsLoader} from './ProjectsLoader';
 import {i18n} from 'lib-admin-ui/util/Messages';
+import {ProjectsChainBlock} from '../../../../dialog/ProjectsChainBlock';
 
 export class ProjectsDropdown extends RichDropdown<Project> {
+
+    private projectsChainBlock: ProjectsChainBlock;
 
     constructor() {
         super({
@@ -16,6 +19,8 @@ export class ProjectsDropdown extends RichDropdown<Project> {
             inputPlaceholderText: `<${i18n('settings.field.project.parent.notset')}>`,
             dataIdProperty: 'value'
         });
+
+        this.projectsChainBlock = new ProjectsChainBlock();
     }
 
     protected createLoader(): ProjectsLoader {
@@ -34,14 +39,15 @@ export class ProjectsDropdown extends RichDropdown<Project> {
         });
     }
 
-    selectProjectByName(projectName: string) {
+    selectProjectByName(projectName: string): Q.Promise<void> {
         if (!projectName) {
-            return;
+            return Q(null);
         }
 
-        new ProjectGetRequest(projectName).sendAndParse().then((project: Project) => {
+        return new ProjectGetRequest(projectName).sendAndParse().then((project: Project) => {
             this.selectOption(this.createOption(project));
-        }).catch(DefaultErrorHandler.handle);
+            return Q(null);
+        });
     }
 
     selectProject(project: Project) {
@@ -50,5 +56,57 @@ export class ProjectsDropdown extends RichDropdown<Project> {
         }
 
         this.selectOption(this.createOption(project));
+    }
+
+    disable() {
+        super.disable();
+        this.showProjectsChain();
+    }
+
+    private showProjectsChain() {
+        const selectedOption: Option<Project> = this.getSelectedOption();
+        const selectedProject: Project = !!selectedOption ? selectedOption.displayValue : null;
+
+        if (!selectedProject) {
+            this.doShowProjectsChain([]);
+            return;
+        }
+
+        this.getAllProjects().then((projects: Project[]) => {
+            const projectsChain: Project[] = this.buildProjectsChain(selectedProject, projects);
+            this.doShowProjectsChain(projectsChain);
+        }).catch(DefaultErrorHandler.handle);
+    }
+
+    private doShowProjectsChain(projects: Project[]) {
+        this.projectsChainBlock.setProjectsChain(projects);
+        this.prependChild(this.projectsChainBlock);
+    }
+
+    private getAllProjects(): Q.Promise<Project[]> {
+        if (this.loader.isLoaded()) {
+            return Q(this.loader.getResults());
+        }
+
+        return this.loader.load();
+    }
+
+    private buildProjectsChain(selectedProject: Project, allProjects: Project[]): Project[] {
+        const parentProjects: Project[] = [selectedProject];
+
+        let parentProjectName: string = selectedProject.getParent();
+
+        while (parentProjectName) {
+                const parentProject: Project = allProjects.find((project: Project) => project.getName() === parentProjectName);
+
+                if (parentProject) {
+                    parentProjects.unshift(parentProject);
+                    parentProjectName = parentProject.getParent();
+                } else {
+                    parentProjectName = null;
+                }
+        }
+
+        return parentProjects;
     }
 }
