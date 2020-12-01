75d119840d32e684d3a894fae2873996cd8c04f1
Mikita Taukachou <edloidas@gmail.com>
2020-12-01 18:27:39 +0300

    Configure build of lib-contentstudio #2548 (#2598)
    * Configure build of lib-contentstudio #2548
    
    * Configure build of lib-contentstudio #2548
    
    Moved app to the subproject.
    
    * Configure build of lib-contentstudio #2548
    
    Added lib to the subproject.
    
    * Configure build of lib-contentstudio #2548
    
    Fixed typing and compiler errors.
    
    * Configure build of lib-contentstudio #2548
    
    Moved subprojects to module folder.
    
    * Configure build of lib-contentstudio #2548
    
    Fixed node_modules detection, when compiling src with webpack (modules/app is not a folder webpack searches in).
    
    * Configure build of lib-contentstudio #2548
    
    Fixed conflicting node_modules jquery definitions.
    
    * Configure build of lib-contentstudio #2548
    
    Fixed lib-contentstudio JAR content.
    
    * Configure build of lib-contentstudio #2548
    
    Fixed missing lint package.
    Moved bnd config to app module, since it's not needed in lib.
    
    * Configure build of lib-contentstudio #2548
    
    Updated subprojects and publish artifacts names.
    
    * Configure build of lib-contentstudio #2548
    
    Updated the path to lib-admin-ui module, when lib is used in .xp folder.
    Changed the name of the root project, since app name can not contain dashes in name, when included to another build.
    Excluded some of the unused DTS files.
    
    * Configure build of lib-contentstudio #2548
    
    Removed unused npm packages.
    
    * Configure build of lib-contentstudio #2548
    
    Fixed relative path to lib-admin-ui in .xp and node_modules.
    
    * Configure build of lib-contentstudio #2548
    
    Added `lib` (`-Plib`) parameter to include lib subproject.
    
    Co-authored-by: Alan Semenov <alansemenov@users.noreply.github.com>

---
+++ modules/lib/util/modify.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const argv = require('minimist')(process.argv.slice(2));
+const packageData = require('../package.json');
+
+const outDir = argv['out'];
+const libPathInDotXp = 'file:../lib-admin-ui';
+
+function updateLibPath() {
+    if (!outDir) {
+        throw '--out=... argument is not defined';
+    }
+
+    const packageFile = path.join(outDir, 'package.json');
+
+    packageData.dependencies['lib-admin-ui'] = libPathInDotXp;
+
+    fs.writeFileSync(packageFile, JSON.stringify(packageData, null, '    '));
+}
+
+updateLibPath();
