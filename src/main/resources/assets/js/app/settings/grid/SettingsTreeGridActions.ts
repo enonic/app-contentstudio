8532143c3c0dd141ff9e393764ea6f5e64445cd0
Aliaksandr Shklianko <asl@enonic.com>
2021-01-05 12:25:26 +0300

    Do not allow deletion of multiple projects #2708
    (cherry picked from commit 74104df1e8363e848bd05b2442e60a86d0829c28)

--- src/main/resources/assets/js/app/settings/grid/SettingsTreeGridActions.ts
+++ src/main/resources/assets/js/app/settings/grid/SettingsTreeGridActions.ts
@@ -65,7 +65,7 @@ export class SettingsTreeGridActions
     }
 
     private isDeleteAllowed(selectedItems: SettingsViewItem[], loginResult: LoginResult): boolean {
-        return selectedItems.length > 0 ?
+        return selectedItems.length === 1 ?
                selectedItems.every((item: SettingsViewItem) => !this.itemHasChildren(item) && item.isDeleteAllowed(loginResult)) : false;
     }
 
