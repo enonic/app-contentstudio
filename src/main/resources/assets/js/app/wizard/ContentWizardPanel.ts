34ae90f77e953284cd0c08738cf4fa388d1a5335
Pavel Milkevich <pmi@enonic.com>
2021-02-23 18:21:29 +0300

    Wizard does not load for mixin with nested option set #2898 (#2899)
    * Wizard does not load for mixin with nested option set #2898
    
    * Optional chaining (#2898)
    
    Co-authored-by: Alan Semenov <alansemenov@users.noreply.github.com>

--- src/main/resources/assets/js/app/wizard/ContentWizardPanel.ts
+++ src/main/resources/assets/js/app/wizard/ContentWizardPanel.ts
@@ -2168,7 +2168,7 @@ export class ContentWizardPanel
                                       data.getPropertySet(formOptionSet.getPath().getParentPath()) :
                                       data;
 
-            const optionSetOccurrences = parentPropertySet.getPropertyArray(formOptionSet.getName());
+            const optionSetOccurrences = parentPropertySet?.getPropertyArray(formOptionSet.getName());
             if (!optionSetOccurrences) {
                 return;
             }
