722e4a5e922b9fffce3d238ed4a6e444928c5e9c
Alan Semenov <alansemenov@users.noreply.github.com>
2021-03-05 10:48:24 +0100

    Change app structure to allow for Admin Rest API (#2938)
    * New app structure (#2860)
    
    * Added missing files (#2860)
    
    * Some build fixes (#2860)
    
    * Moved application icon/description to modules/app (#2860)
    
    * Change app structure #2860
    
    Enabled lib build.
    
    * Change app structure #2860
    
    Added app build.
    
    * Change app structure #2860
    
    Fixed build errors in application:
    * App and Lib is not compatible with target > ES5.
    * Dynamic import of the InputTypes should filter all files, except the pure JS.
    
    * Change app structure #2860
    
    Added lint config.
    
    * Change app structure #2860
    
    Fixed missing packages and invalid less paths.
    
    * Fixed broken links in the docs
    
    * Turned off deploy for composite jars (#2860)
    
    * Disabled deploy in the root module (#2860)
    
    * Transferred StatusResource (#2860)
    
    * Transferred StatusResource #2860
    
    * Transferred StatusResource #2860
    moved rest out of lib
    
    * Updated package-lock.json (#2860)
    
    * Codacy (#2860)
    
    * Codacy (#2860)
    
    * Codacy (#2860)
    
    * Codacy (#2860)
    
    Co-authored-by: Mikita Taukachou <edloidas@gmail.com>
    Co-authored-by: rymsha <rymsha@yandex.ru>

---
+++ modules/rest/src/main/java/com/enonic/xp/app/contentstudio/rest/resource/AdminCmsRestActivator.java
@@ -0,0 +1,35 @@
+package com.enonic.xp.app.contentstudio.rest.resource;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+import com.enonic.xp.jaxrs.JaxRsService;
+import com.enonic.xp.jaxrs.JaxRsServiceFactory;
+import com.enonic.xp.web.dispatch.DispatchConstants;
+
+@Component(immediate = true)
+public final class AdminCmsRestActivator
+{
+    private JaxRsService service;
+
+    @Activate
+    public void activate()
+    {
+
+        this.service.init();
+    }
+
+    @Deactivate
+    public void deactivate()
+    {
+        this.service.destroy();
+    }
+
+    @Reference
+    public void setService( final JaxRsServiceFactory factory )
+    {
+        this.service = factory.newService( "cms", "/admin/cms/rest", DispatchConstants.XP_CONNECTOR );
+    }
+}
