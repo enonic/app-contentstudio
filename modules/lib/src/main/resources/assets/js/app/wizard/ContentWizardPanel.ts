f3b874957837c06d80115d86e233bd90450ab11e
Alan Semenov <ase@enonic.com>
2021-03-16 16:49:24 +0100

    Changed localisation to set language after render. Removed duplicate check for modify permissions (#2975)

--- modules/lib/src/main/resources/assets/js/app/wizard/ContentWizardPanel.ts
+++ modules/lib/src/main/resources/assets/js/app/wizard/ContentWizardPanel.ts
@@ -473,10 +473,6 @@ export class ContentWizardPanel
 
             this.appendChild(this.getContentWizardToolbarPublishControls().getMobilePublishControls());
 
-            if (this.getLivePanel()) {
-                this.getLivePanel().setModifyPermissions(this.modifyPermissions);
-            }
-
             if (this.contentType.hasDisplayNameExpression()) {
                 this.displayNameResolver.setExpression(this.contentType.getDisplayNameExpression());
             }
@@ -1725,20 +1721,9 @@ export class ContentWizardPanel
                     this.setSteps(steps);
 
                     return this.layoutWizardStepForms(content).then(() => {
-                        new IsAuthenticatedRequest().sendAndParse().then((loginResult: LoginResult) => {
-                            this.setModifyPermissions(loginResult);
-                            this.toggleStepFormsVisibility(loginResult);
-                            this.updateUrlAction();
-
-                            if (this.isLocalizeInUrl()) {
-                                this.settingsWizardStepForm.updateInitialLanguage();
-                            }
-
-                            if (!this.modifyPermissions) {
-                                NotifyManager.get().showFeedback(i18n('notify.item.readonly'));
-                            }
-                        });
-
+                        if (this.isLocalizeInUrl()) {
+                            this.onRendered(() => this.settingsWizardStepForm.updateInitialLanguage());
+                        }
                         this.syncPersistedItemWithContentData(content.getContentData());
                         this.xDataWizardStepForms.resetState();
 
@@ -2421,12 +2406,19 @@ export class ContentWizardPanel
         return this.formContext;
     }
 
-    private setModifyPermissions(loginResult: LoginResult) {
+    private setModifyPermissions() {
+        if (!this.loginResult) {
+            return;
+        }
         this.modifyPermissions =
-            this.getPersistedItem().isAnyPrincipalAllowed(loginResult.getPrincipals(), Permission.MODIFY);
+            this.getPersistedItem().isAnyPrincipalAllowed(this.loginResult.getPrincipals(), Permission.MODIFY);
         this.getEl().toggleClass('no-modify-permissions', !this.modifyPermissions);
         if (this.getLivePanel()) {
             this.getLivePanel().setModifyPermissions(this.modifyPermissions);
+        }
+
+        if (!this.modifyPermissions) {
+            NotifyManager.get().showFeedback(i18n('notify.item.readonly'));
         }
     }
 
@@ -2730,16 +2722,9 @@ export class ContentWizardPanel
     protected handleCanModify(canModify: boolean): void {
         super.handleCanModify(canModify);
 
-        if (this.getLivePanel()) {
-            this.getLivePanel().setModifyPermissions(this.canModify);
-        }
-
+        this.setModifyPermissions();
         this.toggleStepFormsVisibility(this.loginResult);
         this.updateUrlAction();
-
-        if (this.isLocalizeInUrl()) {
-            this.settingsWizardStepForm.updateInitialLanguage();
-        }
     }
 
     private handleCUD() {
