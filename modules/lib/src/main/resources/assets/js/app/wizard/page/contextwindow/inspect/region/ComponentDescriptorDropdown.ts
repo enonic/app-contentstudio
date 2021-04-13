bad66eab7f01082b6aa67791f1f9d76db73ad154
Alan Semenov <ase@enonic.com>
2021-04-13 11:43:41 +0200

    Switched to new endpoints for fetching layout and part descriptors (#3049)

--- modules/lib/src/main/resources/assets/js/app/wizard/page/contextwindow/inspect/region/ComponentDescriptorDropdown.ts
+++ modules/lib/src/main/resources/assets/js/app/wizard/page/contextwindow/inspect/region/ComponentDescriptorDropdown.ts
@@ -1,14 +1,14 @@
 import {DescriptorBasedDropdown} from '../DescriptorBasedDropdown';
 import {ComponentDescriptorLoader} from './ComponentDescriptorLoader';
 import {Descriptor} from 'lib-admin-ui/content/page/Descriptor';
-import {ApplicationKey} from 'lib-admin-ui/application/ApplicationKey';
+import {ContentId} from 'lib-admin-ui/content/ContentId';
 
 export abstract class ComponentDescriptorDropdown<DESCRIPTOR extends Descriptor>
     extends DescriptorBasedDropdown<DESCRIPTOR> {
 
     protected loader: ComponentDescriptorLoader<DESCRIPTOR>;
 
-    setApplicationKeys(applicationKeys: ApplicationKey[]) {
-        this.loader.setApplicationKeys(applicationKeys);
+    setContentId(contentId: ContentId) {
+        this.loader.setContentId(contentId);
     }
 }
