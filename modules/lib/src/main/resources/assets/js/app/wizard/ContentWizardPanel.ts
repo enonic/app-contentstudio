a5f20c474cae9fdc32990d29fa44835c6e42a0ae
Aliaksandr Shklianko <asl@enonic.com>
2021-06-03 15:42:42 +0300

    Page Component View remains visible after resetting changes in a child layer #3188

--- modules/lib/src/main/resources/assets/js/app/wizard/ContentWizardPanel.ts
+++ modules/lib/src/main/resources/assets/js/app/wizard/ContentWizardPanel.ts
@@ -573,6 +573,7 @@ export class ContentWizardPanel
             }
 
             this.wizardActions.setDeleteOnlyMode(viewedContent, false);
+            this.updateButtonsState();
         } else {
             // this update was triggered by our changes, so reset dirty state after save
             this.resetWizard();
