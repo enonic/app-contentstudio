69dfb95ba80b647223f77488de7c583c70d28d6f
Aliaksandr Shklianko <asl@enonic.com>
2021-07-06 16:36:25 +0300

    Content Wizard - Show Context Panel button is not displayed for content without Page Editor #3267
    -Adjusted behaviour

--- modules/lib/src/main/resources/assets/js/app/view/context/button/NonMobileContextPanelToggleButton.ts
+++ modules/lib/src/main/resources/assets/js/app/view/context/button/NonMobileContextPanelToggleButton.ts
@@ -1,4 +1,7 @@
 import {Button} from 'lib-admin-ui/ui/button/Button';
+import {InspectEvent} from '../../../event/InspectEvent';
+import {i18n} from 'lib-admin-ui/util/Messages';
+import {ToggleContextPanelEvent} from '../ToggleContextPanelEvent';
 
 export class NonMobileContextPanelToggleButton
     extends Button {
@@ -6,5 +9,13 @@ export class NonMobileContextPanelToggleButton
     constructor() {
         super();
         this.addClass('icon-list non-mobile-details-panel-toggle-button');
+        this.onClicked(this.handleClick.bind(this));
+    }
+
+    private handleClick() {
+        new ToggleContextPanelEvent().fire();
+
+        // this.toggleClass('expanded', this.open);
+        // this.setTitle(this.open ? i18n('tooltip.contextPanel.hide') : i18n('tooltip.contextPanel.show'), false);
     }
 }
