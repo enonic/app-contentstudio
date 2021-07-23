5719c7a7a0e08ed68d45a94fbfbdba94566d9434
Pavel Milkevich <pavel.milkevich@gmail.com>
2021-07-23 16:47:48 +0300

    Move LayoutDescriptorResource and PartDescriptorResource endpoints #3330

---
+++ modules/rest/src/main/java/com/enonic/xp/app/contentstudio/json/content/page/region/PartDescriptorsJson.java
@@ -0,0 +1,33 @@
+package com.enonic.xp.app.contentstudio.json.content.page.region;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.enonic.xp.app.contentstudio.rest.resource.schema.content.LocaleMessageResolver;
+import com.enonic.xp.app.contentstudio.rest.resource.schema.mixin.InlineMixinResolver;
+import com.enonic.xp.region.PartDescriptors;
+
+
+@SuppressWarnings("UnusedDeclaration")
+public class PartDescriptorsJson
+{
+    private final List<PartDescriptorJson> descriptorJsonList;
+
+    public PartDescriptorsJson( final List<PartDescriptorJson> descriptorJsonList )
+    {
+        this.descriptorJsonList = List.copyOf( descriptorJsonList );
+    }
+
+    public PartDescriptorsJson( final PartDescriptors descriptors, final LocaleMessageResolver localeMessageResolver,
+                                final InlineMixinResolver inlineMixinResolver )
+    {
+        this.descriptorJsonList = descriptors.stream()
+            .map( descriptor -> new PartDescriptorJson( descriptor, localeMessageResolver, inlineMixinResolver ) )
+            .collect( Collectors.toUnmodifiableList() );
+    }
+
+    public List<PartDescriptorJson> getDescriptors()
+    {
+        return descriptorJsonList;
+    }
+}
