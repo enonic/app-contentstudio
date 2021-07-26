ade2f8a4157ca0311d5a805e5f9246db52bf19d5
Siarhei Hauruseu <sig@enonic.com>
2021-07-26 16:37:09 +0300

    ui-tests - first test added issue#3290

---
+++ testing/specs/base/create.site.spec.js
@@ -0,0 +1,38 @@
+/**
+ * Created on 02.07.2021.
+ */
+const chai = require('chai');
+const assert = chai.assert;
+const webDriverHelper = require('../../libs/WebDriverHelper');
+const appConstant = require('../../libs/app_const');
+const studioUtils = require('../../libs/studio.utils.js');
+const ContentWizardPanel = require('../../page_objects/wizardpanel/content.wizard.panel');
+const SiteForm = require('../../page_objects/wizardpanel/site.form.panel');
+const contentBuilder = require("../../libs/content.builder");
+
+describe('base.add.site.spec - Open site wizard and save new site', function () {
+    this.timeout(appConstant.SUITE_TIMEOUT);
+    webDriverHelper.setupBrowser();
+    let SITE;
+    const CONTROLLER_NAME = 'main region';
+
+    it(`WHEN site wizard is opened and an application and page descriptor have been selected THEN Save button gets disabled`,
+        async () => {
+            let displayName = contentBuilder.generateRandomName('site');
+            SITE = contentBuilder.buildSite(displayName, 'description', [appConstant.APP_CONTENT_TYPES], CONTROLLER_NAME);
+            let contentWizardPanel = new ContentWizardPanel();
+            let siteForm = new SiteForm();
+            //1. Open new site-wizard, verify that the wizard is loaded in the browser tab:
+            await studioUtils.openContentWizard(appConstant.contentTypes.SITE);
+            await contentWizardPanel.typeDisplayName(displayName);
+            await siteForm.addApplications([appConstant.APP_CONTENT_TYPES]);
+            await contentWizardPanel.selectPageDescriptor(CONTROLLER_NAME);
+            await contentWizardPanel.waitForSaveButtonDisabled();
+        });
+
+    beforeEach(() => studioUtils.navigateToContentStudioApp());
+    afterEach(() => studioUtils.doCloseAllWindowTabsAndSwitchToHome());
+    before(() => {
+        return console.log('specification is starting: ' + this.title);
+    });
+});
