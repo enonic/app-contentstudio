f1f42b1553496574ba6513b74b2e5db7878f7ca9
Pavel Milkevich <pavel.milkevich@gmail.com>
2021-07-28 15:25:50 +0300

    Move XDataResource endpoint #3340

---
+++ modules/rest/src/main/java/com/enonic/xp/app/contentstudio/rest/resource/content/CmsResourceDynamicFeature.java
@@ -0,0 +1,38 @@
+package com.enonic.xp.app.contentstudio.rest.resource.content;
+
+import java.util.Set;
+
+import javax.ws.rs.container.DynamicFeature;
+import javax.ws.rs.container.ResourceInfo;
+import javax.ws.rs.core.FeatureContext;
+import javax.ws.rs.ext.Provider;
+
+import com.enonic.xp.app.contentstudio.rest.resource.content.page.PageResource;
+import com.enonic.xp.app.contentstudio.rest.resource.content.page.PageTemplateResource;
+import com.enonic.xp.app.contentstudio.rest.resource.content.page.fragment.FragmentResource;
+import com.enonic.xp.app.contentstudio.rest.resource.issue.IssueResource;
+import com.enonic.xp.app.contentstudio.rest.resource.schema.content.ContentTypeResource;
+import com.enonic.xp.app.contentstudio.rest.resource.schema.content.FilterByContentResource;
+import com.enonic.xp.app.contentstudio.rest.resource.schema.xdata.XDataResource;
+import com.enonic.xp.jaxrs.JaxRsComponent;
+
+
+@Provider
+public final class CmsResourceDynamicFeature
+    implements DynamicFeature, JaxRsComponent
+{
+    private final Set<Class<? extends JaxRsComponent>> supportedResources =
+        Set.of( ContentResource.class, ContentImageResource.class, ContentIconResource.class, ContentMediaResource.class,
+                IssueResource.class, FilterByContentResource.class, ContentTypeResource.class, PageResource.class,
+                PageTemplateResource.class, FragmentResource.class, XDataResource.class );
+
+
+    @Override
+    public void configure( final ResourceInfo resourceInfo, final FeatureContext context )
+    {
+        if ( supportedResources.contains( resourceInfo.getResourceClass() ) )
+        {
+            context.register( new CmsResourceFilter() );
+        }
+    }
+}
