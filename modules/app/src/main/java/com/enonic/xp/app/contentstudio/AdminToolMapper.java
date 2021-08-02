3b9bc94c65316d12563cc74a01d030ce2b30fa7a
Aliaksandr Shklianko <asl@enonic.com>
2021-08-02 14:55:10 +0300

    Fix Sidebar issues #3361
    -Currently app-main shows in menu only those apps that have display-name set (settings and archive admin tools have display-name commented out to prevent showing them), thus some admin tools have no localized name to present in admin tools sidebar menu. Fixed by adding default localisations for those
    -Tooltip has been moved into lib-contentstudio so it is common for all apps
    -Sidebar css fixed

--- modules/app/src/main/java/com/enonic/xp/app/contentstudio/AdminToolMapper.java
+++ modules/app/src/main/java/com/enonic/xp/app/contentstudio/AdminToolMapper.java
@@ -1,5 +1,8 @@
 package com.enonic.xp.app.contentstudio;
 
+import java.util.Objects;
+import java.util.Optional;
+
 import com.enonic.xp.admin.tool.AdminToolDescriptor;
 import com.enonic.xp.script.serializer.MapGenerator;
 import com.enonic.xp.script.serializer.MapSerializable;
@@ -11,18 +14,25 @@ final class AdminToolMapper
 
     private final String icon;
 
-    AdminToolMapper( final AdminToolDescriptor value, final String icon )
+    private final StringTranslator stringTranslator;
+
+    AdminToolMapper( final AdminToolDescriptor value, final String icon, final StringTranslator stringTranslator )
     {
         this.value = value;
         this.icon = icon;
+        this.stringTranslator = stringTranslator;
     }
 
     private void serialize( final MapGenerator gen, final AdminToolDescriptor value )
     {
+        final String displayNameI18nKey = Objects.requireNonNullElse( value.getDisplayNameI18nKey(), "app.admin.tool." + value.getName() );
+        final String displayName = Objects.requireNonNullElse( value.getDisplayName(), value.getKey().toString() );
+
         gen.value( "application", value.getKey().getApplicationKey().toString() );
         gen.value( "name", value.getKey().getName() );
         gen.value( "key", value.getKey().toString() );
         gen.value( "icon", icon );
+        gen.value( "displayName", this.stringTranslator.localize( value.getKey().getApplicationKey(), displayNameI18nKey, displayName ) );
     }
 
 
