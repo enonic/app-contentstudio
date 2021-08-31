4ab3d6d192e568ea843625df204bf87e08df9a57
Aliaksandr Shklianko <asl@enonic.com>
2021-08-31 13:43:04 +0300

    B/I/U disappeared from Text component's toolbar #3476
    (cherry picked from commit 5090803be27fe8f32ca8a385470477cba3ccbad8)

--- modules/lib/src/main/resources/assets/js/app/inputtype/ui/text/HtmlEditor.ts
+++ modules/lib/src/main/resources/assets/js/app/inputtype/ui/text/HtmlEditor.ts
@@ -991,7 +991,7 @@ class HtmlEditorConfigBuilder {
             this.includeTool('Fullscreen');
         }
 
-        if (this.editorParams.isFullScreenMode()) {
+        if (this.editorParams.isFullScreenMode() || this.editorParams.isInline()) {
             ['Bold', 'Italic', 'Underline']
                 .filter((tool: string) =>!this.isToolDisabled(tool))
                 .forEach((tool: string) => this.includeTool(tool));
