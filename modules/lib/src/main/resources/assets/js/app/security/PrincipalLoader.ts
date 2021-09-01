c21e4ecc9dace891c8cc15e77667fcb3931ad172
Alan Semenov <ase@enonic.com>
2021-09-01 15:48:49 +0200

    Small renaming of aliases (#3443)

--- modules/lib/src/main/resources/assets/js/app/security/PrincipalLoader.ts
+++ modules/lib/src/main/resources/assets/js/app/security/PrincipalLoader.ts
@@ -1,10 +1,10 @@
-import {PrincipalLoader as LibPrincipalLoader} from 'lib-admin-ui/security/PrincipalLoader';
+import {PrincipalLoader as BasePrincipalLoader} from 'lib-admin-ui/security/PrincipalLoader';
 import {PrincipalKey} from 'lib-admin-ui/security/PrincipalKey';
 import {FindPrincipalsRequest} from './FindPrincipalsRequest';
 import {GetPrincipalsByKeysRequest} from './GetPrincipalsByKeysRequest';
 
 export class PrincipalLoader
-    extends LibPrincipalLoader {
+    extends BasePrincipalLoader {
 
     protected createRequest(): FindPrincipalsRequest {
         return <FindPrincipalsRequest>new FindPrincipalsRequest().setSize(10);
