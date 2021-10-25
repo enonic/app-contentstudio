0da4752cf8a515cf542df3b7c8283ba39dd79937
Aliaksandr Shklianko <asl@enonic.com>
2021-10-25 16:48:16 +0300

    Not using i18n in static context since i18n might not be initialized yet

--- modules/lib/src/main/resources/assets/js/app/settings/wizard/panel/form/element/ProjectAccessSelector.ts
+++ modules/lib/src/main/resources/assets/js/app/settings/wizard/panel/form/element/ProjectAccessSelector.ts
@@ -5,71 +5,40 @@ import {NavigatorEvent} from 'lib-admin-ui/ui/NavigatorEvent';
 import {ProjectAccessValueChangedEvent} from '../../../../event/ProjectAccessValueChangedEvent';
 import {i18n} from 'lib-admin-ui/util/Messages';
 
-interface ProjectAccessSelectorOption {
-    value: ProjectAccess;
-    label: string;
-}
-
 export class ProjectAccessSelector
     extends TabMenu {
 
-    private options: ProjectAccessSelectorOption[] = [];
+    private options: ProjectAccess[] = [ProjectAccess.CONTRIBUTOR, ProjectAccess.AUTHOR, ProjectAccess.EDITOR, ProjectAccess.OWNER];
     private value: ProjectAccess;
     private valueChangedListeners: { (event: ProjectAccessValueChangedEvent): void }[] = [];
 
     constructor() {
         super('access-selector');
 
-        this.initOptions();
-
-        this.options.forEach((option: ProjectAccessSelectorOption) => {
-            const menuItem: TabMenuItem = new TabMenuItemBuilder().setLabel(option.label).build();
+        this.options.forEach((option: ProjectAccess) => {
+            const menuItem: TabMenuItem = new TabMenuItemBuilder().setLabel(i18n(`settings.projects.access.${option}`)).build();
             this.addNavigationItem(menuItem);
         });
 
         this.onNavigationItemSelected((event: NavigatorEvent) => {
-            const item: TabMenuItem = <TabMenuItem> event.getItem();
-            this.setValue(this.options[item.getIndex()].value);
+            const item: TabMenuItem = <TabMenuItem>event.getItem();
+            this.setValue(this.options[item.getIndex()]);
         });
 
-    }
-
-    private initOptions() {
-        this.options =
-            [
-                {value: ProjectAccess.CONTRIBUTOR, label: i18n('settings.projects.access.contributor')},
-                {value: ProjectAccess.AUTHOR, label: i18n('settings.projects.access.author')},
-                {value: ProjectAccess.EDITOR, label: i18n('settings.projects.access.editor')},
-                {value: ProjectAccess.OWNER, label: i18n('settings.projects.access.owner')}
-            ];
     }
 
     getValue(): ProjectAccess {
         return this.value;
     }
 
-    setValue(value: ProjectAccess, silent?: boolean): ProjectAccessSelector {
-        let option: ProjectAccessSelectorOption = this.findOptionByValue(value);
+    setValue(value: ProjectAccess, silent?: boolean) {
+        this.selectNavigationItem(this.options.indexOf(value));
 
-        if (option) {
-            this.selectNavigationItem(this.options.indexOf(option));
-            if (!silent) {
-                this.notifyValueChanged(new ProjectAccessValueChangedEvent(this.value, value));
-            }
-            this.value = value;
+        if (!silent) {
+            this.notifyValueChanged(new ProjectAccessValueChangedEvent(this.value, value));
         }
 
-        return this;
-    }
-
-    private findOptionByValue(value: ProjectAccess): ProjectAccessSelectorOption {
-        for (let i = 0; i < this.options.length; i++) {
-            let option = this.options[i];
-            if (option.value === value) {
-                return option;
-            }
-        }
-        return undefined;
+        this.value = value;
     }
 
     onValueChanged(listener: (event: ProjectAccessValueChangedEvent) => void) {
