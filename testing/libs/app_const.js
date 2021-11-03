04895dfa082c680b26a1903b09b692f644e574b2
Siarhei Hauruseu <sig@enonic.com>
2021-11-03 16:53:49 +0300

    ui-tests issue#3499

--- testing/libs/app_const.js
+++ testing/libs/app_const.js
@@ -250,6 +250,7 @@ module.exports = Object.freeze({
         CUSTOM_SELECTOR_0_2: 'custom-selector0_2',
         CUSTOM_SELECTOR_1_1: 'custom-selector1_1',
         OPTION_SET: 'optionset',
+        ITEM_SET_0_0: 'item-set0_0'
     },
     permissions: {
         FULL_ACCESS: `Full Access`,
