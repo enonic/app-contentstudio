cbac9303e2d689a3ca2ce714f68d3bc3e8245ff7
Mikita Taukachou <edloidas@gmail.com>
2021-11-25 15:07:20 +0300

    Remove unnamed subname for folders in Settings statistics panel #3885
    Removed unnamed subtitle on settings folder.
    Made localization text on highlighted node more visible.
    
    (cherry picked from commit 30278d49d957ae9a13bdda5424d5e6591b43920f)

--- modules/lib/src/main/resources/assets/js/app/settings/browse/statistics/view/folder/FolderStatisticsView.ts
+++ modules/lib/src/main/resources/assets/js/app/settings/browse/statistics/view/folder/FolderStatisticsView.ts
@@ -1,7 +1,5 @@
 import {SettingsStatisticsView} from '../SettingsStatisticsView';
 import {FolderViewItem} from '../../../../view/FolderViewItem';
-import {FolderItemViewer} from '../../../viewer/FolderItemViewer';
-import {NamesAndIconViewer} from 'lib-admin-ui/ui/NamesAndIconViewer';
 import {FolderStatisticsViewer} from './FolderStatisticsViewer';
 
 export class FolderStatisticsView extends SettingsStatisticsView<FolderViewItem> {
