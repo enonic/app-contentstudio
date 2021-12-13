a1d7f263b4d8bb590110d9fd2ebed6b0f30b8cba
Bruno Reis <reisfmb@gmail.com>
2021-12-13 17:25:35 -0300

    Added projects tab cycle in the 'select project' dialog (#2989)

--- modules/lib/src/main/resources/assets/js/app/settings/dialog/ProjectSelectionDialog.ts
+++ modules/lib/src/main/resources/assets/js/app/settings/dialog/ProjectSelectionDialog.ts
@@ -55,6 +55,8 @@ export class ProjectSelectionDialog
                 }
             });
         });
+
+        this.setLastProjectCycleOnTabEvent();
     }
 
     setUpdateOnOpen(value: boolean) {
@@ -134,6 +136,21 @@ export class ProjectSelectionDialog
             this.appendChildToContentPanel(this.noItemsInfoBlock);
 
             return rendered;
+        });
+    }
+
+    // Cycle to the first project element when tabbing in the last element
+    private setLastProjectCycleOnTabEvent(): void {
+        if(this.projectsList.getItemViews().length === 0) return;
+
+        const firstProject: ProjectListItem =
+            this.projectsList.getItemViews()[0] as ProjectListItem;
+
+        const lastProject: ProjectListItem =
+            this.projectsList.getItemViews()[this.projectsList.getItemViews().length - 1] as ProjectListItem;
+
+        lastProject.onKeyDown((event: KeyboardEvent) => {
+            setTimeout(() => event.key === 'Tab' && firstProject.getHTMLElement().focus(), 1);
         });
     }
 
