7f538f640420c7929c3c428263b650324fe514a0
Siarhei Hauruseu <sig@enonic.com>
2021-12-15 17:30:13 +0300

    ui-tests- issue#3895 (#3972)

--- testing/specs/content-types-2/occurrences.double.spec.js
+++ testing/specs/content-types-2/occurrences.double.spec.js
@@ -20,6 +20,8 @@ describe('occurrences.double.spec: tests for content with Double inputs', functi
     const MIN_SAFE_DOUBLE = "-9007199254740991";
     const LESS_MIN_SAFE_DOUBLE = "-9007199254740991.8";
     const CONTENT_1 = contentBuilder.generateRandomName('double');
+    const CONTENT_2 = contentBuilder.generateRandomName('double');
+    const INVALID_VALUE = "123q";
 
     it(`Preconditions: new site should be added`,
         async () => {
@@ -69,9 +71,9 @@ describe('occurrences.double.spec: tests for content with Double inputs', functi
             let doubleForm = new DoubleForm();
             let contentWizard = new ContentWizard();
             await studioUtils.selectSiteAndOpenNewWizard(SITE.displayName, appConst.contentTypes.DOUBLE_0_1);
+            await contentWizard.typeDisplayName(CONTENT_1);
             //1. Type max safe value:
             await doubleForm.typeDouble(MIN_SAFE_DOUBLE);
-            await contentWizard.typeDisplayName(CONTENT_1);
             await studioUtils.saveScreenshot('double_min_safe');
             //2. Verify that input validation message(Invalid value entered) is displayed:
             let recording = await doubleForm.getOccurrenceValidationRecording(0);
@@ -88,9 +90,9 @@ describe('occurrences.double.spec: tests for content with Double inputs', functi
             let doubleForm = new DoubleForm();
             let contentWizard = new ContentWizard();
             await studioUtils.selectSiteAndOpenNewWizard(SITE.displayName, appConst.contentTypes.DOUBLE_0_1);
+            await contentWizard.typeDisplayName(CONTENT_1);
             //1. Type max safe value:
             await doubleForm.typeDouble(LESS_MIN_SAFE_DOUBLE);
-            await contentWizard.typeDisplayName(CONTENT_1);
             await studioUtils.saveScreenshot('double_less_min_safe');
             //2. Verify that input validation message(Invalid value entered) is displayed:
             let recording = await doubleForm.getOccurrenceValidationRecording(0);
@@ -102,7 +104,7 @@ describe('occurrences.double.spec: tests for content with Double inputs', functi
             await doubleForm.waitForRedBorderInDoubleInput(0);
         });
 
-    it(`GIVEN wizard for 'double 2:4' content is opened WHEN values in both inputs have been typed and one is invalid THEN red border should be present in the second double input`,
+    it(`GIVEN wizard for 'double 2:4' content is opened WHEN one valid value and one invalid value have been typed THEN red border should be present in the second double input`,
         async () => {
             let doubleForm = new DoubleForm();
             let contentWizard = new ContentWizard();
@@ -118,12 +120,59 @@ describe('occurrences.double.spec: tests for content with Double inputs', functi
             //3. Verify that the content is not valid, because Min 2 valid occurrence(s) required
             let isInvalid = await contentWizard.isContentInvalid();
             assert.isTrue(isInvalid, "Content should be invalid");
-            //4. Verify thar red border appears in the second double input:
+            //4. Verify that red border appears in the second double input:
             await doubleForm.waitForRedBorderInDoubleInput(1);
             //5. Verify that 'Min 2 valid occurrence(s) required' appears after clicking on Save button
             await contentWizard.waitAndClickOnSave();
             let formRecording = await doubleForm.getFormValidationRecording();
             assert.equal(formRecording, "Min 2 valid occurrence(s) required", 'Form validation recording should be displayed');
+        });
+
+    it(`GIVEN invalid value have been typed in both inputs('double 2:4') WHEN 'Save' button has been pressed THEN validation recording and 'Hide Details' button should appear`,
+        async () => {
+            let doubleForm = new DoubleForm();
+            let contentWizard = new ContentWizard();
+            await studioUtils.selectSiteAndOpenNewWizard(SITE.displayName, appConst.contentTypes.DOUBLE_2_4);
+            await contentWizard.typeDisplayName(CONTENT_2);
+            //1. Type an invalid value in both inputs:
+            await doubleForm.typeDouble(INVALID_VALUE);
+            await doubleForm.typeDouble(INVALID_VALUE, 1);
+            await studioUtils.saveScreenshot('double_invalid_values');
+            //2. Verify the validation recordings for both inputs(Invalid value entered):
+            let recording = await doubleForm.getOccurrenceValidationRecording(1);
+            assert.equal(recording, appConst.VALIDATION_MESSAGE.INVALID_VALUE_ENTERED, 'Input validation recording should be displayed');
+            recording = await doubleForm.getOccurrenceValidationRecording(0);
+            assert.equal(recording, appConst.VALIDATION_MESSAGE.INVALID_VALUE_ENTERED, 'Input validation recording should be displayed');
+            //3. Verify the red borders in both double inputs:
+            await doubleForm.waitForRedBorderInDoubleInput(1);
+            await doubleForm.waitForRedBorderInDoubleInput(0);
+            //4. click on 'Save' button:
+            await contentWizard.waitAndClickOnSave();
+            //5. Verify that 'Hide details' button gets visible:
+            await doubleForm.waitForHideDetailsButtonDisplayed();
+            //6. Click on 'Hide details' button:
+            await doubleForm.clickOnHideDetailsButton();
+            //7. Verify that 'Show Details' button gets visible:
+            await doubleForm.waitForShowDetailsButtonDisplayed();
+            //8. Verify that 'occurrence validation' recordings are cleared:
+            let result1 = await doubleForm.getOccurrenceValidationRecording(1);
+            let result2 = await doubleForm.getOccurrenceValidationRecording(0);
+            assert.equal(result1, "", "The first 'occurrence validation' recording should be cleared");
+            assert.equal(result2, "", "The second 'occurrence validation' recording should be cleared");
+        });
+
+
+    it(`WHEN invalid value have been typed in both inputs('double 2:4') THEN 'Hide Details' and 'Show details' buttons should not be displayed before saving the content`,
+        async () => {
+            let doubleForm = new DoubleForm();
+            await studioUtils.selectSiteAndOpenNewWizard(SITE.displayName, appConst.contentTypes.DOUBLE_2_4);
+            //1. Type an invalid value in both inputs:
+            await doubleForm.typeDouble(INVALID_VALUE);
+            await doubleForm.typeDouble(INVALID_VALUE, 1);
+            await doubleForm.pause(700);
+            await studioUtils.saveScreenshot('double_invalid_values_2');
+            await doubleForm.waitForShowDetailsButtonNotDisplayed();
+            await doubleForm.waitForHideDetailsButtonNotDisplayed();
         });
 
     beforeEach(() => studioUtils.navigateToContentStudioApp());
