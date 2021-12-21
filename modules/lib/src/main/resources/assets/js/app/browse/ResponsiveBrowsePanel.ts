1600f754af0ca600ff48a0f7ec7185d1bab5bc62
Aliaksandr Shklianko <asl@enonic.com>
2021-12-21 14:38:58 +0300

    75_25

--- modules/lib/src/main/resources/assets/js/app/browse/ResponsiveBrowsePanel.ts
+++ modules/lib/src/main/resources/assets/js/app/browse/ResponsiveBrowsePanel.ts
@@ -47,7 +47,7 @@ export abstract class ResponsiveBrowsePanel extends BrowsePanel {
         const rightPanel: DockedContextPanel = new DockedContextPanel(this.contextView);
 
         this.contextSplitPanel = ContextSplitPanel.create(leftPanel, rightPanel)
-            .setSecondPanelSize(SplitPanelSize.Percents(38))
+            .setSecondPanelSize(SplitPanelSize.Percents(25))
             .setContextView(this.contextView)
             .build();
 
