af465459232ce635a3e34c60517b3efda90f534c
Siarhei Hauruseu <sig@enonic.com>
2022-01-04 13:42:22 +0300

    ui-tests- issue#4052 (#4066)

--- testing/specs/browse.panel.grid.context.menu.spec.js
+++ testing/specs/browse.panel.grid.context.menu.spec.js
@@ -10,6 +10,7 @@ const studioUtils = require('../libs/studio.utils.js');
 const contentBuilder = require("../libs/content.builder");
 const ContentBrowsePanel = require('../page_objects/browsepanel/content.browse.panel');
 const ContentPublishDialog = require('../page_objects/content.publish.dialog');
+const ContentItemPreviewPanel = require('../page_objects/browsepanel/contentItem.preview.panel');
 
 describe('browse.panel.grid.context.menu.spec - Tests for grid context menu', function () {
     this.timeout(appConst.SUITE_TIMEOUT);
@@ -58,7 +59,6 @@ describe('browse.panel.grid.context.menu.spec - Tests for grid context menu', fu
             await contentBrowsePanel.waitForContextMenuItemEnabled(appConst.GRID_CONTEXT_MENU.MOVE);
             await contentBrowsePanel.waitForContextMenuItemEnabled(appConst.GRID_CONTEXT_MENU.PUBLISH);
             await contentBrowsePanel.waitForContextMenuItemEnabled(appConst.GRID_CONTEXT_MENU.DUPLICATE);
-
             //4.Verify that New, Preview, Sort  menu items are disabled in the context menu:
             await contentBrowsePanel.waitForContextMenuItemDisabled(appConst.GRID_CONTEXT_MENU.NEW);
             await contentBrowsePanel.waitForContextMenuItemDisabled(appConst.GRID_CONTEXT_MENU.PREVIEW);
@@ -89,7 +89,6 @@ describe('browse.panel.grid.context.menu.spec - Tests for grid context menu', fu
             await contentBrowsePanel.waitForContextMenuItemNotDisplayed(appConst.GRID_CONTEXT_MENU.PUBLISH);
         });
 
-
     it(`GIVEN one published and one new folders are selected WHEN do right click on the selected items THEN Publish and Unpublish menu items should be enabled`,
         async () => {
             let contentBrowsePanel = new ContentBrowsePanel();
@@ -119,6 +118,23 @@ describe('browse.panel.grid.context.menu.spec - Tests for grid context menu', fu
             let contentWizard = new ContentWizard();
             let displayName = await contentWizard.getDisplayName();
             assert.equal(displayName, TEST_FOLDER_1.displayName, "Expected and actual display name should be equal");
+        });
+
+    it(`WHEN do right click on an existing image THEN Preview menu item should be enabled in the context menu`,
+        async () => {
+            let contentBrowsePanel = new ContentBrowsePanel();
+            let contentItemPreviewPanel = new ContentItemPreviewPanel();
+            //1. Select an image:
+            await studioUtils.findAndSelectItem(appConst.TEST_IMAGES.RENAULT);
+            //2. Do the right click on the image:
+            await contentBrowsePanel.rightClickOnItemByDisplayName(appConst.TEST_IMAGES.RENAULT);
+            //3.Verify that New, Preview, Sort  menu items are disabled in the context menu:
+            await studioUtils.saveScreenshot("image-context-menu-1");
+            await contentBrowsePanel.waitForContextMenuItemDisabled(appConst.GRID_CONTEXT_MENU.PREVIEW);
+            await contentBrowsePanel.waitForContextMenuItemDisabled(appConst.GRID_CONTEXT_MENU.NEW);
+            await contentBrowsePanel.waitForContextMenuItemDisabled(appConst.GRID_CONTEXT_MENU.SORT);
+            //4. Verify that Img element gets visible in the Item Preview Panel:
+            await contentItemPreviewPanel.waitForImageDisplayed();
         });
 
     beforeEach(() => studioUtils.navigateToContentStudioApp());
--- testing/specs/content.item.preview.spec.js
+++ testing/specs/content.item.preview.spec.js
@@ -4,12 +4,12 @@
 const chai = require('chai');
 const assert = chai.assert;
 const webDriverHelper = require('../libs/WebDriverHelper');
-const appConstant = require('../libs/app_const');
+const appConst = require('../libs/app_const');
 const studioUtils = require('../libs/studio.utils.js');
 const ContentItemPreviewPanel = require('../page_objects/browsepanel/contentItem.preview.panel');
 
-describe('attachment.preview.spec - Select a *.txt file and check expected text in Preview Panel', function () {
-    this.timeout(appConstant.SUITE_TIMEOUT);
+describe('content.item.preview.spec - Select a content file and check expected info in Item Preview Panel', function () {
+    this.timeout(appConst.SUITE_TIMEOUT);
     webDriverHelper.setupBrowser();
 
     it(`WHEN existing *.txt file is selected WHEN expected text should be loaded in Preview Panel`,
@@ -19,6 +19,15 @@ describe('attachment.preview.spec - Select a *.txt file and check expected text 
             await contentItemPreviewPanel.pause(500);
             let result = await contentItemPreviewPanel.getTextInAttachmentPreview();
             assert.isTrue(result.includes('Belarus'), "expected text should be present in the Preview Panel");
+        });
+
+    it(`WHEN existing folder has been selected THEN 'Preview not available' should be shown in Item Preview Panel`,
+        async () => {
+            let contentItemPreviewPanel = new ContentItemPreviewPanel();
+            //1. Select an existing folder:
+            await studioUtils.findAndSelectItem(appConst.TEST_FOLDER_2_NAME);
+            //2. Verify that 'Preview not available' is displayed
+            await contentItemPreviewPanel.waitForPreviewNotAvailAbleMessageDisplayed();
         });
 
     beforeEach(() => studioUtils.navigateToContentStudioApp());
