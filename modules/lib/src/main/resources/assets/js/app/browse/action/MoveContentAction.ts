3e3d60be264234c54c5ccec94e76d740328d733c
Aliaksandr Shklianko <asl@enonic.com>
2022-01-05 16:45:17 +0300

    "Move" action is disabled in the search view #4035

--- modules/lib/src/main/resources/assets/js/app/browse/action/MoveContentAction.ts
+++ modules/lib/src/main/resources/assets/js/app/browse/action/MoveContentAction.ts
@@ -4,8 +4,10 @@ import {ContentSummaryAndCompareStatus} from '../../content/ContentSummaryAndCom
 import {i18n} from 'lib-admin-ui/util/Messages';
 import {ContentTreeGridAction} from './ContentTreeGridAction';
 import {ContentTreeGridItemsState} from './ContentTreeGridItemsState';
+import {ArrayHelper} from 'lib-admin-ui/util/ArrayHelper';
 
-export class MoveContentAction extends ContentTreeGridAction {
+export class MoveContentAction
+    extends ContentTreeGridAction {
 
     constructor(grid: ContentTreeGrid) {
         super(grid, i18n('action.moveMore'));
@@ -13,18 +15,24 @@ export class MoveContentAction extends ContentTreeGridAction {
     }
 
     protected handleExecuted() {
-        const contents: ContentSummaryAndCompareStatus[]
-            = this.grid.getSelectedDataList();
+        const contents: ContentSummaryAndCompareStatus[] = this.grid.getSelectedDataList();
         new MoveContentEvent(contents, this.grid).fire();
     }
 
     isToBeEnabled(state: ContentTreeGridItemsState): boolean {
-        return !state.isEmpty() && !state.isManagedActionExecuting() &&
-               (this.getCurrentSelectedOrHighlightedTotal() < this.grid.getDefaultFullTotal()) &&
-               state.canDelete();
+        return !state.isEmpty() && !state.isManagedActionExecuting() && this.isAnyRootItemNotSelected() && state.canDelete();
     }
 
-    private getCurrentSelectedOrHighlightedTotal(): number {
-        return this.grid.hasHighlightedNode() ? 1 : this.grid.getTotalSelected();
+    private isAnyRootItemNotSelected(): boolean {
+        // if there's at least one non-selected root item then there's at least one option where to move selected items
+        const selectedIds: string[] = this.getSelectedOrHighlightedItemsIds();
+        const rootItemsIds: string[] = this.grid.getRootItemsIds();
+        const diff: string[] = ArrayHelper.difference(rootItemsIds, selectedIds, (a: string, b: string) => a === b);
+
+        return diff.length > 0;
+    }
+
+    private getSelectedOrHighlightedItemsIds(): string[] {
+        return this.grid.getSelectedOrHighlightedItems().map((item: ContentSummaryAndCompareStatus) => item.getId());
     }
 }
