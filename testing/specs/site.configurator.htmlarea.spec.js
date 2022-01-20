6356ce3b6621523a1744b637a035a97326082823
Siarhei Hauruseu <sig@enonic.com>
2022-01-20 08:28:08 +0300

    ui-tests screenshot added

--- testing/specs/site.configurator.htmlarea.spec.js
+++ testing/specs/site.configurator.htmlarea.spec.js
@@ -98,8 +98,11 @@ describe('site.configurator.htmlarea.spec: tests for site configurator with html
             await studioUtils.selectContentAndOpenWizard(SITE.displayName);
             //2. Click on 'Edit' icon and open 'Site Configurator' Dialog:
             await siteFormPanel.openSiteConfiguratorDialog(appConst.APP_CONTENT_TYPES);
+            await studioUtils.saveScreenshot("site_config_loaded");
+            await siteConfiguratorDialog.pause(2000);
             //3. Click on Cancel top button in the modal dialog:
             await siteConfiguratorDialog.clickOnCancelTopButton();
+            await studioUtils.saveScreenshot("site_config_closed");
             //4. Dialog should be closed:
             await siteConfiguratorDialog.waitForDialogClosed();
         });
