7e5759de36e36a88484188bb431cd38c28c298a8
sgauruseu <sig@eninic.com>
2022-05-12 09:01:57 +0300

    ui-tests issue#4551 update path

--- testing/wdio.wizard.grid.ff.conf.js
+++ testing/wdio.wizard.grid.ff.conf.js
@@ -28,10 +28,9 @@ exports.config = {
         browserName: 'firefox',
         'moz:firefoxOptions': {
             "args": [
-                "--headless","--disable-gpu", "--no-sandbox",
+                "--headless", "--disable-gpu", "--no-sandbox",
                 "--lang=en",
                 '--disable-extensions',
-                //'--window-size=1970,1100'
                 "--width=1920",
                 "--height=1000"
             ]
@@ -80,7 +79,7 @@ exports.config = {
     // see also: http://webdriver.io/guide/testrunner/reporters.html
     reporters: ['spec',
         [HtmlReporter, {
-            outputDir: './build/mochawesome-report/',
+            outputDir: __dirname +'/build/mochawesome-report/',
             filename: 'report.html',
             reportTitle: 'Suite Report Title',
             linkScreenshots: true,
@@ -101,75 +100,17 @@ exports.config = {
         ui: 'bdd',
         timeout: 60000
     },
-    //
-    // =====
-    // Hooks
-    // =====
-    // WebdriverIO provides a several hooks you can use to intefere the test process in order to enhance
-    // it and build services around it. You can either apply a single function to it or an array of
-    // methods. If one of them returns with a promise, WebdriverIO will wait until that promise got
-    // resolved to continue.
-    //
-    // Gets executed once before all workers get launched.
-    // onPrepare: function (config, capabilities) {
-    // },
-    //
-    // Gets executed before test execution begins. At this point you can access to all global
-    // variables like `browser`. It is the perfect place to define custom commands.
-    // before: function (capabilities, specs) {
-     //    browser.init()
-    // },
-    //
+
     // Hook that gets executed before the suite starts
      beforeSuite: function (suite) {
          browser.url(this.baseUrl);
     },
-    //
-    // Hook that gets executed _before_ a hook within the suite starts (e.g. runs before calling
-    // beforeEach in Mocha)
-    // beforeHook: function () {
-    // },
-    //
-    // Hook that gets executed _after_ a hook within the suite starts (e.g. runs after calling
-    // afterEach in Mocha)
-    // afterHook: function () {
-    // },
-    //
-    // Function to be executed before a test (in Mocha/Jasmine) or a step (in Cucumber) starts.
-    // beforeTest: function (test) {
-    // },
-    //
-    // Runs before a WebdriverIO command gets executed.
-    // beforeCommand: function (commandName, args) {
-    // },
-    //
-    // Runs after a WebdriverIO command gets executed
-    // afterCommand: function (commandName, args, result, error) {
-    // },
-    //
-    // Function to be executed after a test (in Mocha/Jasmine) or a step (in Cucumber) starts.
-    // afterTest: function (test) {
-    // },
-    //
-    // Hook that gets executed after the suite has ended
-    // afterSuite: function (suite) {
-    // },
-    //
-   
-    //
-    // Gets executed after all tests are done. You still have access to all global variables from
-    // the test.
-    // after: function (result, capabilities, specs) {
-    // },
-    //
-    // Gets executed after all workers got shut down and the process is about to exit.
-    // onComplete: function(exitCode, config, capabilities) {
-    // }
+
     onPrepare: function (config, capabilities) {
 
         reportAggregator = new ReportAggregator({
             //outputDir:  path.join(__dirname, '/build/mochawesome-report'),
-            outputDir: './build/mochawesome-report/',
+            outputDir: __dirname +'/build/mochawesome-report/',
             filename: 'report.html',
             reportTitle: 'Content Studio, Wizard, Grid Report',
             browserName : capabilities.browserName,
