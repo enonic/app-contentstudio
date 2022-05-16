d0b9e2ddbe2918a9b78c9faca134b2f46bf49197
Aliaksandr Shklianko <asl@enonic.com>
2022-05-16 09:52:23 +0300

    Replace checkboxes with dropdowns for Owner and Modifier #4538
    -Added new filterable buckets views, allowing to keep all buckets in a dropdown, and show them on the top when selected. Certain items may be always kept on top no matter selected or not

---
+++ modules/lib/src/main/resources/assets/js/app/browse/filter/FilterableAggregationGroupView.ts
@@ -0,0 +1,30 @@
+import {Aggregation} from 'lib-admin-ui/aggregation/Aggregation';
+import {AggregationGroupView} from 'lib-admin-ui/aggregation/AggregationGroupView';
+import {AggregationView} from 'lib-admin-ui/aggregation/AggregationView';
+import {BucketAggregation} from 'lib-admin-ui/aggregation/BucketAggregation';
+import {ContentAggregation} from './ContentAggregation';
+import {FilterableBucketAggregationView} from './FilterableBucketAggregationView';
+
+export class FilterableAggregationGroupView
+    extends AggregationGroupView {
+
+    private idsToKeepOnToTop: string[];
+
+    protected createAggregationView(aggregation: Aggregation): AggregationView {
+        const aggregationView: FilterableBucketAggregationView = new FilterableBucketAggregationView(<BucketAggregation>aggregation);
+        aggregationView.setIdsToKeepOnToTop(this.idsToKeepOnToTop);
+
+        return aggregationView;
+    }
+
+    setIdsToKeepOnToTop(ids: string[]) {
+        this.idsToKeepOnToTop = ids;
+
+        this.aggregationViews.forEach((item: AggregationView) => {
+           if (item instanceof FilterableBucketAggregationView) {
+               (<FilterableBucketAggregationView>item).setIdsToKeepOnToTop(ids);
+           }
+        });
+    }
+
+}
