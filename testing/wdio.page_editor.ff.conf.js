bb2f56443298ca0c4d93a9ca481e3db6210b258a
sgauruseu <sig@eninic.com>
2022-06-21 12:17:01 +0300

    ui-tests issue#4620 path to reporting updated

--- testing/wdio.page_editor.ff.conf.js
+++ testing/wdio.page_editor.ff.conf.js
@@ -54,11 +54,11 @@ exports.config = {
         timeout: 70000
     },
     // Set directory to store all logs into
-    outputDir: path.join(__dirname ,"/../build/mochawesome-report/"),
+    outputDir: path.join(__dirname ,"/build/mochawesome-report/"),
 
     reporters: ['spec',
         ["html-nice", {
-            outputDir: './build/mochawesome-report/',
+            outputDir: '/build/mochawesome-report/',
             filename: 'spec-report.html',
             reportTitle: 'Tests for Page Editor',
             linkScreenshots: true,
@@ -85,7 +85,7 @@ exports.config = {
 
     onPrepare: function (config, capabilities) {
         reportAggregator = new ReportAggregator({
-            outputDir: './build/mochawesome-report/',
+            outputDir: '/build/mochawesome-report/',
             filename: 'app-report.html',
             reportTitle: 'Content Studio, Page Editor Tests Report',
             browserName: capabilities.browserName,
