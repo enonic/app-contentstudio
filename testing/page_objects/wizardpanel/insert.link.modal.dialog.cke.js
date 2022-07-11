3d2f7b319c7113b89269e262b287bde7b6ecd0cd
sgauruseu <sig@eninic.com>
2022-07-11 17:59:13 +0300

    ui-tests minor change

--- testing/page_objects/wizardpanel/insert.link.modal.dialog.cke.js
+++ testing/page_objects/wizardpanel/insert.link.modal.dialog.cke.js
@@ -257,10 +257,6 @@ class InsertLinkDialog extends Page {
         return this.waitForElementDisplayed(this.emailInputValidationRecording, appConst.mediumTimeout);
     }
 
-    waitForValidationMessageForEmailInputDisplayed() {
-        return this.waitForElementDisplayed(this.emailInputValidationRecording, appConst.mediumTimeout);
-    }
-
     async getUrlInputValidationMessage() {
         await this.waitForValidationMessageForUrlInputDisplayed();
         return await this.getText(this.urlInputValidationRecording);
