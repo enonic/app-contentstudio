390bf9eadec5af00f07db5f62b7fde8e68671a76
Alan Semenov <ase@enonic.com>
2022-09-21 17:27:29 +0200

    Cache response of GetMimeTypesByContentTypeNamesRequest (#5135)

--- modules/lib/src/main/resources/assets/js/app/resource/GetMimeTypesByContentTypeNamesRequest.ts
+++ modules/lib/src/main/resources/assets/js/app/resource/GetMimeTypesByContentTypeNamesRequest.ts
@@ -1,3 +1,4 @@
+import * as Q from 'q';
 import {JsonResponse} from '@enonic/lib-admin-ui/rest/JsonResponse';
 import {ContentTypeResourceRequest} from './ContentTypeResourceRequest';
 import {ContentTypeName} from '@enonic/lib-admin-ui/schema/content/ContentTypeName';
@@ -5,21 +6,35 @@ import {ContentTypeName} from '@enonic/lib-admin-ui/schema/content/ContentTypeNa
 export class GetMimeTypesByContentTypeNamesRequest
     extends ContentTypeResourceRequest<String[]> {
 
-    private names: ContentTypeName[];
+    private readonly names: ContentTypeName[];
+    private readonly namesAsString: string;
+
+    private static cache: Map<string, String[]> = new Map<string, String[]>();
 
     constructor(names: ContentTypeName[]) {
         super();
         this.names = names;
+        this.namesAsString = this.names.map(name => name.toString()).sort().join(',');
         this.addRequestPathElements('getMimeTypes');
+    }
+
+    sendAndParse(): Q.Promise<String[]> {
+        if (!GetMimeTypesByContentTypeNamesRequest.cache.has(this.namesAsString)) {
+            return super.sendAndParse();
+        }
+
+        return Q(GetMimeTypesByContentTypeNamesRequest.cache.get(this.namesAsString));
     }
 
     getParams(): Object {
         return {
-            typeNames: this.names.map(name => name.toString()).join(',')
+            typeNames: this.namesAsString
         };
     }
 
     protected parseResponse(response: JsonResponse<String[]>): String[] {
-        return response.getJson();
+        const responseAsJson: String[] = response.getJson();
+        GetMimeTypesByContentTypeNamesRequest.cache.set(this.namesAsString, responseAsJson);
+        return responseAsJson;
     }
 }
