3ced09b9407559d805e137e864493a42beaef455
sgauruseu <sig@eninic.com>
2022-09-21 15:02:59 +0300

    ui-tests issue#5170

--- testing/specs/project-apps/project.wizard.applications.step.spec.js
+++ testing/specs/project-apps/project.wizard.applications.step.spec.js
@@ -20,36 +20,35 @@ describe('project.wizard.dialog.applications.step.spec - ui-tests for Applicatio
         webDriverHelper.setupBrowser();
     }
 
-    it(`GIVEN wizard dialog is opened WHEN navigated to Applications wizard step THEN 'Skip' button should be enabled`,
-        async () => {
-            let settingsBrowsePanel = new SettingsBrowsePanel();
-            let languageStep = new ProjectWizardDialogLanguageStep();
-            let parentProjectStep = new ProjectWizardDialogParentProjectStep();
-            let accessModeStep = new ProjectWizardDialogAccessModeStep();
-            let permissionsStep = new ProjectWizardDialogPermissionsStep();
-            let applicationsStep = new ProjectWizardDialogApplicationsStep();
-            //1.Open new project wizard:
-            await settingsBrowsePanel.openProjectWizardDialog();
-            //2. Select 'Default' project and go to 'Applications' step
-            await parentProjectStep.clickOnLayerRadioButton();
-            await parentProjectStep.selectParentProject("Default");
-            await parentProjectStep.clickOnNextButton();
-            await languageStep.clickOnSkipButton();
-            await accessModeStep.clickOnAccessModeRadio(appConst.PROJECT_ACCESS_MODE.PUBLIC);
-            await accessModeStep.clickOnNextButton();
-            await permissionsStep.clickOnSkipButton();
-            //3. Verify that 'Skip' button is enabled in Applications step
-            await applicationsStep.waitForSkipButtonEnabled();
-            //4. Select an application:
-            await applicationsStep.selectApplication(appConst.APP_CONTENT_TYPES);
-            await studioUtils.saveScreenshot("proj_wizard_app_selected");
-            //5. Verify that 'Next' button gets visible:
-            await applicationsStep.waitForNextButtonEnabled();
-            //6. Click on remove icon:
-            await applicationsStep.removeApplication(appConst.APP_CONTENT_TYPES);
-            //7. Verify that 'Skip' button appears again:
-            await applicationsStep.waitForSkipButtonEnabled();
-        });
+    it(`GIVEN wizard dialog is opened WHEN navigated to Applications wizard step THEN 'Skip' button should be enabled`, async () => {
+        let settingsBrowsePanel = new SettingsBrowsePanel();
+        let languageStep = new ProjectWizardDialogLanguageStep();
+        let parentProjectStep = new ProjectWizardDialogParentProjectStep();
+        let accessModeStep = new ProjectWizardDialogAccessModeStep();
+        let permissionsStep = new ProjectWizardDialogPermissionsStep();
+        let applicationsStep = new ProjectWizardDialogApplicationsStep();
+        //1.Open new project wizard:
+        await settingsBrowsePanel.openProjectWizardDialog();
+        //2. Select 'Default' project and go to 'Applications' step
+        await parentProjectStep.clickOnLayerRadioButton();
+        await parentProjectStep.selectParentProject("Default");
+        await parentProjectStep.clickOnNextButton();
+        await languageStep.clickOnSkipButton();
+        await accessModeStep.clickOnAccessModeRadio(appConst.PROJECT_ACCESS_MODE.PUBLIC);
+        await accessModeStep.clickOnNextButton();
+        await permissionsStep.clickOnSkipButton();
+        //3. Verify that 'Skip' button is enabled in Applications step
+        await applicationsStep.waitForSkipButtonEnabled();
+        //4. Select an application:
+        await applicationsStep.selectApplication(appConst.APP_CONTENT_TYPES);
+        await studioUtils.saveScreenshot("proj_wizard_app_selected");
+        //5. Verify that 'Next' button gets visible:
+        await applicationsStep.waitForNextButtonEnabled();
+        //6. Click on remove icon:
+        await applicationsStep.removeApplication(appConst.APP_CONTENT_TYPES);
+        //7. Verify that 'Skip' button appears again:
+        await applicationsStep.waitForSkipButtonEnabled();
+    });
 
     it(`GIVEN application is selected in the step WHEN go to the next step then go back to the previous step THEN selected application should be present in the app-step`,
         async () => {
