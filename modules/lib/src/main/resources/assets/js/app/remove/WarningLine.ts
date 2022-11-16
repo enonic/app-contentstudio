b09e744496cfafcc9503a62bcc8c9fd26e2389c2
Mikita Taukachou <edloidas@gmail.com>
2022-11-16 17:07:12 +0300

    Refresh contents of the Archive dialog on updates of inbound references #5435
    Replaced global events with events manager.
    
    referringIds -> referenceIds
    
    Refresh contents of the Archive dialog on updates of inbound references #5435
    
    Lock action button to prevent archiving content with inbound references #5434
    
    Change visualisation of inbound references in the Archive dialog #5433

---
+++ modules/lib/src/main/resources/assets/js/app/remove/WarningLine.ts
@@ -0,0 +1,62 @@
+import * as Q from 'q';
+import {i18n} from '@enonic/lib-admin-ui/util/Messages';
+import {DivEl} from '@enonic/lib-admin-ui/dom/DivEl';
+import {SpanEl} from '@enonic/lib-admin-ui/dom/SpanEl';
+import {ActionButton} from '@enonic/lib-admin-ui/ui/button/ActionButton';
+import {Action} from '@enonic/lib-admin-ui/ui/Action';
+
+export interface WarningLineConfig {
+    ignoreHandler: () => void;
+}
+
+export class WarningLine
+    extends DivEl {
+
+    private icon: DivEl;
+
+    private warning: SpanEl;
+
+    private ignoreButton: ActionButton;
+
+    constructor({ignoreHandler}: WarningLineConfig) {
+        super('warning-line');
+
+        this.initElements();
+        this.initListeners(ignoreHandler);
+    }
+
+    doRender(): Q.Promise<boolean> {
+        return super.doRender().then((rendered: boolean) => {
+            this.appendChildren(this.icon, this.warning, this.ignoreButton);
+
+            return rendered;
+        });
+    }
+
+    protected initElements() {
+        this.icon = new SpanEl('icon icon-big-plus');
+
+        this.warning = new SpanEl('warning-text');
+        this.warning.setHtml(i18n('dialog.archive.warning.text'));
+
+        const ignoreAction = new Action(i18n('dialog.archive.warning.ignore'));
+        this.ignoreButton = new ActionButton(ignoreAction);
+        this.ignoreButton.addClass('ignore-button');
+    }
+
+    protected initListeners(ignoreHandler: () => void) {
+        this.ignoreButton.getAction().onExecuted(() => {
+            ignoreHandler();
+            this.hide();
+        });
+    }
+
+    updateCount(count: number): void {
+        this.warning.getEl().setAttribute('data-count', `(${count})`);
+    }
+
+    setIgnoreEnabled(enabled: boolean): void {
+        this.ignoreButton.setEnabled(enabled);
+    }
+
+}
