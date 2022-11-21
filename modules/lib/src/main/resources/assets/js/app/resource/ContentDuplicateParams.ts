ee7d03905feb5a8cb93bb638633a42a00812819f
Anatol Sialitski <asi@enonic.com>
2022-11-21 13:57:23 +0100

    Fixed tests
    Refactor ContentDuplicate dialog to support creation of variants #5476
    
    Display an icon for variants (#5474)
    
    Implement admin API getVariants #5439
    
    Add variantOf field to Content data object and classes #5438

---
+++ modules/lib/src/main/resources/assets/js/app/resource/ContentDuplicateParams.ts
@@ -0,0 +1,64 @@
+import {ContentId} from '../content/ContentId';
+import {ContentDuplicateParamsJson} from './ContentDuplicateParamsJson';
+
+export class ContentDuplicateParams {
+    private readonly contentId: ContentId;
+    private includeChildren: boolean;
+    private variant: boolean;
+    private parent: string;
+    private name: string;
+
+    constructor(contentId: ContentId) {
+        this.contentId = contentId;
+    }
+
+    setIncludeChildren(value: boolean): ContentDuplicateParams {
+        this.includeChildren = value;
+        return this;
+    }
+
+    setVariant(value: boolean): ContentDuplicateParams {
+        this.variant = value;
+        return this;
+    }
+
+    setParent(value: string): ContentDuplicateParams {
+        this.parent = value;
+        return this;
+    }
+
+    setName(value: string): ContentDuplicateParams {
+        this.name = value;
+        return this;
+    }
+
+    getContentId(): ContentId {
+        return this.contentId;
+    }
+
+    isIncludeChildren(): boolean {
+        return this.includeChildren;
+    }
+
+    isVariant(): boolean {
+        return this.variant;
+    }
+
+    getParent(): string {
+        return this.parent;
+    }
+
+    getName(): string {
+        return this.name;
+    }
+
+    toJson(): ContentDuplicateParamsJson {
+        return {
+            contentId: this.contentId.toString(),
+            includeChildren: !!this.includeChildren,
+            variant: !!this.variant,
+            parent: this.parent,
+            name: this.name
+        };
+    }
+}
