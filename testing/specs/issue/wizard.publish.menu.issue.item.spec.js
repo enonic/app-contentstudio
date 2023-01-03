1b665400452564fcf1a3d9f27277b4e0c708c88e
sgauruseu <sig@eninic.com>
2023-01-03 14:48:46 +0300

    ui-tests - minor change

--- testing/specs/issue/wizard.publish.menu.issue.item.spec.js
+++ testing/specs/issue/wizard.publish.menu.issue.item.spec.js
@@ -13,12 +13,13 @@ const IssueDetailsDialog = require('../../page_objects/issue/issue.details.dialo
 const IssueDetailsItemsTab = require('../../page_objects/issue/issue.details.items.tab');
 const IssueListDialog = require('../../page_objects/issue/issue.list.dialog');
 
-describe('wizard.publish.menu.issue.item.spec - Publish menu(in wizard) ', function () {
+describe('wizard.publish.menu.issue.item.spec - Publish menu(in wizard)', function () {
     this.timeout(appConst.SUITE_TIMEOUT);
-    if (typeof browser === "undefined") {
+    if (typeof browser === 'undefined') {
         webDriverHelper.setupBrowser();
     }
     let TEST_FOLDER;
+    const ISSUE_TITLE = 'issue1';
 
     // verifies https://github.com/enonic/app-contentstudio/issues/808
     // Publish Menu is not updated after an item is removed from an issue or request.
@@ -36,19 +37,19 @@ describe('wizard.publish.menu.issue.item.spec - Publish menu(in wizard) ', funct
 
             // Create a new issue: (the folder will be automatically saved)
             await contentWizard.openPublishMenuSelectItem(appConst.PUBLISH_MENU.CREATE_ISSUE);
-            await createIssueDialog.typeTitle("issue1");
+            await createIssueDialog.typeTitle(ISSUE_TITLE);
             await createIssueDialog.clickOnCreateIssueButton();
             // Close Issue Details dialog and Issue List dialog:
             await issueDetailsDialog.clickOnCancelTopButton();
             await issueDetailsDialog.waitForDialogClosed();
             await issueListDialog.waitForDialogClosed();
             // New menu item should appear in the Wizard Publish Menu:
-            await contentWizard.openPublishMenuSelectItem("issue1");
+            await contentWizard.openPublishMenuSelectItem(ISSUE_TITLE);
             // Issue details dialog should be loaded after clicking on the menu item:
             await issueDetailsDialog.waitForDialogOpened();
         });
 
-    //verifies Task Details Dialog switches to the Comments tab after save #1571
+    // verifies Task Details Dialog switches to the Comments tab after save #1571
     // verifies https://github.com/enonic/app-contentstudio/issues/808
     // Publish Menu is not updated after an item is removed from an issue or request.
     it(`GIVEN folder is opened AND existing issue-name has been clicked in the publish menu WHEN this folder has been excluded in the items-tab THEN this menu-item should be removed in Publish Menu`,
@@ -59,26 +60,26 @@ describe('wizard.publish.menu.issue.item.spec - Publish menu(in wizard) ', funct
             // 1. Open existing folder:
             await studioUtils.selectContentAndOpenWizard(TEST_FOLDER.displayName);
             // 2. Expand Publish Menu in the wizard and click on the issue-name(load the issue in modal dialog):
-            await contentWizard.openPublishMenuSelectItem("issue1");
+            await contentWizard.openPublishMenuSelectItem(ISSUE_TITLE);
             // 3. Click on the remove icon and exclude this folder in Items:
             await taskDetailsDialog.clickOnItemsTabBarItem();
             await issueDetailsItemsTab.excludeItem(TEST_FOLDER.displayName);
             await taskDetailsDialog.pause(1000);
             // 4. Verify that Items tab remains active:
             let isActive = await taskDetailsDialog.isItemsTabBarItemActive();
-            assert.isTrue(isActive, "Items tab remains active");
+            assert.isTrue(isActive, 'Items tab remains active');
             // 5. Close the modal dialog:
             await taskDetailsDialog.clickOnCancelTopButton();
-            await studioUtils.saveScreenshot("publish_menu_item_hidden");
+            await studioUtils.saveScreenshot('publish_menu_item_hidden');
             // 6. Expand Publish Menu in wizard and verify that task-name is not present in the menu:
-            let result = await contentWizard.isPublishMenuItemPresent("issue1");
+            let result = await contentWizard.isPublishMenuItemPresent(ISSUE_TITLE);
             assert.isFalse(result, "'issue1' menu item should not be present in the Publish Menu");
         });
 
     beforeEach(() => studioUtils.navigateToContentStudioApp());
     afterEach(() => studioUtils.doCloseAllWindowTabsAndSwitchToHome());
     before(async () => {
-        if (typeof browser !== "undefined") {
+        if (typeof browser !== 'undefined') {
             await studioUtils.getBrowser().setWindowSize(appConst.BROWSER_WIDTH, appConst.BROWSER_HEIGHT);
         }
         return console.log('specification starting: ' + this.title);
