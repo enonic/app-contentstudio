620153e27f378e4b5efe404cfc5129fc775d6eac
ashklianko <asl@enonic.com>
2023-01-11 15:20:26 +0100

    Uploaded media doesn't get selected in Content Selector #5691

--- modules/lib/src/main/resources/assets/js/app/create/NewContentDialog.ts
+++ modules/lib/src/main/resources/assets/js/app/create/NewContentDialog.ts
@@ -62,6 +62,8 @@ export class NewContentDialog
 
     private typeSelectedHandler?: (contentType: ContentTypeSummary, parentContent: ContentSummary) => void;
 
+    private uploadHandler?: (items: UploadItem<Content>[]) => void;
+
     protected header: NewContentDialogHeader;
 
     constructor() {
@@ -148,10 +150,16 @@ export class NewContentDialog
     private initFileInputEvents() {
         this.newContentUploader.onUploadStarted(this.closeAndFireEventFromMediaUpload.bind(this));
         this.newContentUploader.onUploadStarted((event: UploadStartedEvent<Content>) => {
-            const names = event.getUploadItems().map((uploadItem: UploadItem<Content>) => {
+            const names: string[] = event.getUploadItems().map((uploadItem: UploadItem<Content>) => {
                 return uploadItem.getName();
             });
             this.fileInput.setText(names.join(', '));
+
+            if (this.uploadHandler) {
+                this.uploadHandler(event.getUploadItems());
+            }
+
+            this.uploadHandler = null;
         });
 
         this.fileInput.onValueChanged(() => {
@@ -221,6 +229,11 @@ export class NewContentDialog
 
     setContentTypes(types: ContentTypeSummary[]): NewContentDialog {
         this.contentTypes = types;
+        return this;
+    }
+
+    setUploadHandler(handler: (items: UploadItem<Content>[]) => void): NewContentDialog {
+        this.uploadHandler = handler;
         return this;
     }
 
