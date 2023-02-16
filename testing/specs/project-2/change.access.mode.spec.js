b4d0a048c6bf0744cd36db9a45e6f7165315b8ca
sgauruseu <sig@eninic.com>
2023-02-16 18:26:57 +0300

    update task names for ui-tests

--- testing/specs/project-2/change.access.mode.spec.js
+++ testing/specs/project-2/change.access.mode.spec.js
@@ -19,22 +19,22 @@ describe('change.access.mode.spec - Update Access Mode in project wizard', funct
         webDriverHelper.setupBrowser();
     }
     let FOLDER;
-    let TEST_FOLDER_NAME = studioUtils.generateRandomName("folder");
-    let PROJECT_DISPLAY_NAME = studioUtils.generateRandomName("project");
+    let TEST_FOLDER_NAME = studioUtils.generateRandomName('folder');
+    let PROJECT_DISPLAY_NAME = studioUtils.generateRandomName('project');
 
     it(`Preconditions: new projects with 'Private' access mode should be added`,
         async () => {
-            //1. Navigate to Settings Panel:
+            // 1. Navigate to Settings Panel:
             await studioUtils.openSettingsPanel();
-            //1. Save new project (mode access is Private):
+            // 2. Save new project (mode access is Private):
             await projectUtils.saveTestProject(PROJECT_DISPLAY_NAME, "test description", null, null, "Private");
         });
 
     it("Precondition: new folder should be added in existing project(Private mode access)",
         async () => {
-            //1. Select the project in 'Select Context' dialog
+            // 1. Select the project in 'Select Context' dialog
             await studioUtils.openProjectSelectionDialogAndSelectContext(PROJECT_DISPLAY_NAME);
-            //2. add new folder:
+            // 2. add new folder:
             FOLDER = contentBuilder.buildFolder(TEST_FOLDER_NAME);
             await studioUtils.doAddFolder(FOLDER);
         });
@@ -45,37 +45,37 @@ describe('change.access.mode.spec - Update Access Mode in project wizard', funct
             let projectWizard = new ProjectWizard();
             let confirmationDialog = new ConfirmationDialog();
             await studioUtils.openSettingsPanel();
-            //1.Open existing project:
+            // 1.Open existing project:
             await settingsBrowsePanel.clickOnRowByDisplayName(PROJECT_DISPLAY_NAME);
             await settingsBrowsePanel.clickOnEditButton();
             await projectWizard.waitForLoaded();
-            //2. Click on Public radio and confirm the changes:
+            // 2. Click on Public radio and confirm the changes:
             await projectWizard.clickOnAccessModeRadio("Public");
             await confirmationDialog.waitForDialogOpened();
             await confirmationDialog.clickOnYesButton();
             await confirmationDialog.waitForDialogClosed();
-            //3. Save the changes:
+            // 3. Save the changes:
             await projectWizard.waitAndClickOnSave();
             let actualMessages = await projectWizard.waitForNotificationMessages();
-            //4. Verify that 2  notification messages appear: 'Project is modified' and 'Permissions are applied'
+            // 4. Verify that 2  notification messages appear: 'Project is modified' and 'Permissions are applied'
             await studioUtils.saveScreenshot("project_access_mode_updated");
             assert.equal(actualMessages[1], appConst.projectModifiedMessage(PROJECT_DISPLAY_NAME));
             assert.isTrue(actualMessages[0].includes("Permissions"), "Permissions are applied - the second expected notification message");
             assert.isTrue(actualMessages[0].includes("are applied"), "Permissions are applied - the second expected notification message");
         });
 
-    //Verifies https://github.com/enonic/app-contentstudio/issues/1889
-    //Project Wizard - access mode is not updated after the mode change has been confirmed
+    // Verifies https://github.com/enonic/app-contentstudio/issues/1889
+    // Project Wizard - access mode is not updated after the mode change has been confirmed
     it("GIVEN existing project was switched to 'Public' mode WHEN the project has been reopened THEN 'Public' Access Mode (radio button) should be selected",
         async () => {
             let settingsBrowsePanel = new SettingsBrowsePanel();
             let projectWizard = new ProjectWizard();
             await studioUtils.openSettingsPanel();
-            //1. reopen the project:
+            // 1. reopen the project:
             await settingsBrowsePanel.clickOnRowByDisplayName(PROJECT_DISPLAY_NAME);
             await settingsBrowsePanel.clickOnEditButton();
             await projectWizard.waitForLoaded();
-            //2. Verify that 'Public' radio is selected:
+            // 2. Verify that 'Public' radio is selected:
             let isSelected = await projectWizard.isAccessModeRadioSelected("Public");
             assert.isTrue(isSelected, "'Public' radio button should be selected");
             isSelected = await projectWizard.isAccessModeRadioSelected("Private");
@@ -87,13 +87,13 @@ describe('change.access.mode.spec - Update Access Mode in project wizard', funct
     it(`GIVEN existing project context is selected WHEN existing folder has been clicked THEN 'Everyone can read this item' header should be displayed in Access Widget`,
         async () => {
             let userAccessWidget = new UserAccessWidget();
-            //1. Select the project in 'Select Context' dialog
+            // 1. Select the project in 'Select Context' dialog
             await studioUtils.openProjectSelectionDialogAndSelectContext(PROJECT_DISPLAY_NAME);
-            //2. Select existing folder:
+            // 2. Select existing folder:
             await studioUtils.findAndSelectItem(FOLDER.displayName);
-            //3. Verify that Widget Access is updated:
+            // 3. Verify that Widget Access is updated:
             await studioUtils.openBrowseDetailsPanel();
-            studioUtils.saveScreenshot("project_access_mode_updated_widget");
+            await studioUtils.saveScreenshot('project_access_mode_updated_widget');
             let actualHeader = await userAccessWidget.getHeader();
             assert.equal(actualHeader, appConst.ACCESS_WIDGET_HEADER.EVERYONE_CAN_READ,
                 "'Everyone can read this item' - header should be displayed");
--- testing/specs/project-2/default.project.spec.js
+++ testing/specs/project-2/default.project.spec.js
@@ -11,68 +11,67 @@ const appConst = require('../../libs/app_const');
 
 describe('default.project.spec - ui-tests for Default project', function () {
     this.timeout(appConst.SUITE_TIMEOUT);
-    if (typeof browser === "undefined") {
+    if (typeof browser === 'undefined') {
         webDriverHelper.setupBrowser();
     }
 
-    let DEFAULT_DESCRIPTION = "Test description";
+    let DEFAULT_DESCRIPTION = 'Test description';
 
     it("GIVEN SU is logged in WHEN description has been updated in 'Default' project and 'Save' button pressed THEN expected notification should appear",
         async () => {
             let settingsBrowsePanel = new SettingsBrowsePanel();
             let projectWizard = new ProjectWizard();
-            //1. Open 'Default' project:
-            await settingsBrowsePanel.clickOnRowByDisplayName("Default");
+            // 1. Open 'Default' project:
+            await settingsBrowsePanel.clickOnRowByDisplayName('Default');
             await settingsBrowsePanel.clickOnEditButton();
             await projectWizard.waitForLoaded();
             await projectWizard.typeDescription(DEFAULT_DESCRIPTION);
-            //3. Verify that 'Save' button gets enabled, then click on it
+            // 3. Verify that 'Save' button gets enabled, then click on it
             await projectWizard.waitAndClickOnSave();
             let actualMessage = await projectWizard.waitForNotificationMessage();
-            await studioUtils.saveScreenshot("default_project_updated_1");
-            assert.equal(actualMessage, 'Project "default" is modified.', "Expected message should appear");
+            await studioUtils.saveScreenshot('default_project_updated_1');
+            assert.equal(actualMessage, 'Project "default" is modified.', 'Expected message should appear');
         });
 
     it("GIVEN SU is logged in WHEN language has been selected in 'Default' project and 'Save' button pressed THEN expected notification should appear",
         async () => {
             let settingsBrowsePanel = new SettingsBrowsePanel();
             let projectWizard = new ProjectWizard();
-            //1. Open 'Default' project:
-            await settingsBrowsePanel.clickOnRowByDisplayName("Default");
+            // 1. Open 'Default' project:
+            await settingsBrowsePanel.clickOnRowByDisplayName('Default');
             await settingsBrowsePanel.clickOnEditButton();
             await projectWizard.waitForLoaded();
             await projectWizard.selectLanguage(appConst.LANGUAGES.EN);
-            //2. Verify that 'Save' button gets enabled, then click on it
+            // 2. Verify that 'Save' button gets enabled, then click on it
             await projectWizard.waitAndClickOnSave();
             let actualMessage = await projectWizard.waitForNotificationMessage();
-            await studioUtils.saveScreenshot("default_project_updated_2");
-            assert.equal(actualMessage, 'Project "default" is modified.', "Expected message should appear");
+            await studioUtils.saveScreenshot('default_project_updated_2');
+            assert.equal(actualMessage, 'Project "default" is modified.', 'Expected message should appear');
         });
 
     it("WHEN Default project has been reopened THEN expected language and description should be displayed",
         async () => {
             let settingsBrowsePanel = new SettingsBrowsePanel();
             let projectWizard = new ProjectWizard();
-            //1. Open 'Default' project:
-            await settingsBrowsePanel.clickOnRowByDisplayName("Default");
+            // 1. Open 'Default' project:
+            await settingsBrowsePanel.clickOnRowByDisplayName('Default');
             await settingsBrowsePanel.clickOnEditButton();
             await projectWizard.waitForLoaded();
-            //3. Verify the language
+            // 3. Verify the language
             let actualLanguage = await projectWizard.getSelectedLanguage();
-            assert.equal(actualLanguage, appConst.LANGUAGES.EN, "Expected language should be disaplayed");
-            //4. Verify the description:
+            assert.equal(actualLanguage, appConst.LANGUAGES.EN, 'Expected language should be disaplayed');
+            // 4. Verify the description:
             let actualDescription = await projectWizard.getDescription();
-            assert.equal(actualLanguage, appConst.LANGUAGES.EN, "Expected description should be displayed");
-            studioUtils.saveScreenshot("default_project_updated_3");
-            assert.equal(actualDescription, DEFAULT_DESCRIPTION, "Expected message should appear");
-
-            //5. Remove the language and clear the description:
+            assert.equal(actualLanguage, appConst.LANGUAGES.EN, 'Expected description should be displayed');
+            await studioUtils.saveScreenshot('default_project_updated_3');
+            assert.equal(actualDescription, DEFAULT_DESCRIPTION, 'Expected message should appear');
+            // 5. Remove the language and clear the description:
             await projectWizard.clickOnRemoveLanguage();
-            await projectWizard.typeDescription("");
-            //6.Verify that locale options filter input gets visible and enabled:
+            await projectWizard.typeDescription('');
+            // 6.Verify that locale options filter input gets visible and enabled:
             await projectWizard.isLocaleOptionsFilterInputClickable();
             actualDescription = await projectWizard.getDescription();
-            assert.equal(actualDescription, "", "Description should be cleared now");
+            assert.equal(actualDescription, '', 'Description should be cleared now');
             await projectWizard.waitAndClickOnSave();
             await projectWizard.waitForNotificationMessage();
         });
@@ -83,7 +82,7 @@ describe('default.project.spec - ui-tests for Default project', function () {
     });
     afterEach(() => studioUtils.doCloseAllWindowTabsAndSwitchToHome());
     before(async () => {
-        if (typeof browser !== "undefined") {
+        if (typeof browser !== 'undefined') {
             await studioUtils.getBrowser().setWindowSize(appConst.BROWSER_WIDTH, appConst.BROWSER_HEIGHT);
         }
         return console.log('specification starting: ' + this.title);
