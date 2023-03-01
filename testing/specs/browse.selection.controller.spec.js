e437b69b2c00df9f114bdc1b759ead80bcc34929
sgauruseu <sig@eninic.com>
2023-03-01 12:38:19 +0300

    added filtering in failed tests

--- testing/specs/browse.selection.controller.spec.js
+++ testing/specs/browse.selection.controller.spec.js
@@ -11,36 +11,38 @@ const appConst = require('../libs/app_const');
 
 describe('Browse panel selection controller spec. Tests for Selection Controller checkBox and Show/Hide selection toggler', function () {
     this.timeout(appConst.SUITE_TIMEOUT);
-    if (typeof browser === "undefined") {
+    if (typeof browser === 'undefined') {
         webDriverHelper.setupBrowser();
     }
 
-    //Verifies https://github.com/enonic/lib-admin-ui/issues/1790
-    //Selection checkboxes are not working in the Show Selection mode
+    // Verifies https://github.com/enonic/lib-admin-ui/issues/1790
+    // Selection checkboxes are not working in the Show Selection mode
     it("GIVEN two items have been checked AND switched to Show Selection mode WHEN one item has been unchecked THEN one item remains visible in the filtered grid",
         async () => {
             let contentBrowsePanel = new ContentBrowsePanel();
-            //1. Select 2 folders:
+            // 1. Click on checkboxes and select 2 folders:
+            await studioUtils.typeNameInFilterPanel(appConst.TEST_FOLDER_NAME);
             await contentBrowsePanel.clickOnCheckboxAndSelectRowByName(appConst.TEST_FOLDER_NAME);
+            await studioUtils.typeNameInFilterPanel(appConst.TEST_FOLDER_2_NAME);
             await contentBrowsePanel.clickOnCheckboxAndSelectRowByName(appConst.TEST_FOLDER_2_NAME);
-            //3. Click on Selection Toggle (circle, Show Selection):
+            // 2. Click on 'Selection Toggle' (circle, Show Selection):
             await contentBrowsePanel.clickOnSelectionToggler();
             await contentBrowsePanel.pause(500);
-            //4. Verify that 2 items are displayed in the filtered grid:
+            // 3. Verify that 2 items are displayed in the filtered grid:
             let displayNames = await contentBrowsePanel.getDisplayNamesInGrid();
-            assert.equal(displayNames.length, 2, "2 items should be present in the filtered grid");
+            assert.equal(displayNames.length, 2, '2 items should be present in the filtered grid');
             let result = await contentBrowsePanel.isSelectionControllerSelected();
-            assert.isTrue(result, "Selection Controller checkBox should be selected");
-            //5. Verify that checkboxes are clickable: Unselect one item in the filtered grid:
+            assert.isTrue(result, 'Selection Controller checkBox should be selected');
+            // 4. Verify that checkboxes are clickable: Unselect one item in the filtered grid:
             await contentBrowsePanel.clickOnCheckbox(appConst.TEST_FOLDER_2_NAME);
             await contentBrowsePanel.pause(1000);
-            await studioUtils.saveScreenshot("filtered_grid_one_item_unselected");
-            //6. Verify that only one item remains visible in the filtered grid now:
+            await studioUtils.saveScreenshot('filtered_grid_one_item_unselected');
+            // 5. Verify that only one item remains visible in the filtered grid now:
             displayNames = await contentBrowsePanel.getDisplayNamesInGrid();
-            assert.equal(displayNames.length, 1, "Only one item should be present in the filtered grid");
+            assert.equal(displayNames.length, 1, 'Only one item should be present in the filtered grid');
         });
 
-    //Verifies: https://github.com/enonic/lib-admin-ui/issues/1266 Incorrect behavior of 'Show selection' when a single content is highlighted
+    // Verifies: https://github.com/enonic/lib-admin-ui/issues/1266 Incorrect behavior of 'Show selection' when a single content is highlighted
     //          https://github.com/enonic/lib-admin-ui/issues/1201 Selection filter doesn't work when an item is highlighted in the Content Grid #1201
     it("GIVEN existing folder is highlighted WHEN highlighted row has been checked AND 'Show Selection' clicked THEN 'Selection Controller' checkbox gets selected",
         async () => {
@@ -54,21 +56,22 @@ describe('Browse panel selection controller spec. Tests for Selection Controller
             await contentBrowsePanel.pause(500);
             //4. Verify that the grid is filtered:
             let displayNames = await contentBrowsePanel.getDisplayNamesInGrid();
-            assert.equal(displayNames.length, 1, "Only one item should be present in the filtered grid");
+            assert.equal(displayNames.length, 1, 'Only one item should be present in the filtered grid');
             let result = await contentBrowsePanel.isSelectionControllerSelected();
-            assert.isTrue(result, "Selection Controller checkBox should be selected");
+            assert.isTrue(result, 'Selection Controller checkBox should be selected');
         });
 
-    //Verifies https://github.com/enonic/app-contentstudio/issues/595  'Preview' button is enabled after 'selection controller' has been unchecked
+    // Verifies https://github.com/enonic/app-contentstudio/issues/595
+    // 'Preview' button is enabled after 'selection controller' has been unchecked
     it("GIVEN Selection Controller checkbox is selected (All items are checked) WHEN Selection Controller checkbox has been unselected THEN 'Preview' button should be disabled AND 'New' is enabled",
         async () => {
             let contentBrowsePanel = new ContentBrowsePanel();
-            //1. 'Selection Controller' checkbox is selected (All items are checked):
+            // 1. 'Selection Controller' checkbox is selected (All items are checked):
             await contentBrowsePanel.clickOnSelectionControllerCheckbox();
-            //2. 'Selection Controller' checkbox has been unselected:
+            // 2. 'Selection Controller' checkbox has been unselected:
             await contentBrowsePanel.clickOnSelectionControllerCheckbox();
             await contentBrowsePanel.pause(1000);
-            //3. Verify that 'Preview', Edit button are disabled:
+            // 3. Verify that 'Preview', Edit button are disabled:
             await contentBrowsePanel.waitForPreviewButtonDisabled();
             await contentBrowsePanel.waitForEditButtonDisabled();
             await contentBrowsePanel.waitForArchiveButtonDisabled();
@@ -81,26 +84,26 @@ describe('Browse panel selection controller spec. Tests for Selection Controller
             let contentFilterPanel = new ContentFilterPanel();
             let contentBrowsePanel = new ContentBrowsePanel();
             await studioUtils.openFilterPanel();
-            //1. Click on 'Image' checkbox in Filter Panel:
-            await contentFilterPanel.clickOnCheckboxInContentTypesBlock("Image");
-            //2. Select 2 images:
-            await contentBrowsePanel.clickOnCheckboxAndSelectRowByName("book");
-            await contentBrowsePanel.clickOnCheckboxAndSelectRowByName("cape");
-            //"Selection Controller checkbox shows that the selection is partial"
+            // 1. Click on 'Image' checkbox in Filter Panel:
+            await contentFilterPanel.clickOnCheckboxInContentTypesBlock('Image');
+            // 2. Select 2 images:
+            await contentBrowsePanel.clickOnCheckboxAndSelectRowByName(appConst.TEST_IMAGES.BOOK);
+            await contentBrowsePanel.clickOnCheckboxAndSelectRowByName(appConst.TEST_IMAGES.CAPE);
+            // "Selection Controller checkbox shows that the selection is partial"
             await contentBrowsePanel.waitForSelectionControllerPartial();
-            //3. Click on Selection Toggle (circle, Show Selection):
+            // 3. Click on Selection Toggle (circle, Show Selection):
             await contentBrowsePanel.clickOnSelectionToggler();
             await contentBrowsePanel.pause(1000);
-            //4. Verify 'Selection Controller' checkBox is selected:
-            await studioUtils.saveScreenshot("selection_toggle_clicked_checkbox_selected");
+            // 4. Verify 'Selection Controller' checkBox is selected:
+            await studioUtils.saveScreenshot('selection_toggle_clicked_checkbox_selected');
             let result = await contentBrowsePanel.isSelectionControllerSelected();
-            assert.isTrue(result, "Selection Controller checkBox should be selected");
-            //5. Verify that two items should be present in the filtered grid;
+            assert.isTrue(result, "'Selection Controller' checkBox should be selected");
+            // 5. Verify that two items should be present in the filtered grid;
             let items = await contentBrowsePanel.getDisplayNamesInGrid();
-            assert.equal(items.length, 2, "Two items should be in the filtered grid");
-            //6. Number 2  should be displayed in the selection toggler circle:
+            assert.equal(items.length, 2, 'Two items should be in the filtered grid');
+            // 6. Number 2  should be displayed in the selection toggler circle:
             let number = await contentBrowsePanel.getNumberInSelectionToggler();
-            assert.equal(number, 2, "2 should be displayed in the circle");
+            assert.equal(number, '2', '2 should be displayed in the circle');
         });
 
     it("WHEN 'Show Selection' and 'Hide Selection' sequentially clicked in filtered grid THEN 'Selection Controller' checkbox gets partial AND initial state of the grid is restored",
@@ -108,59 +111,59 @@ describe('Browse panel selection controller spec. Tests for Selection Controller
             let contentFilterPanel = new ContentFilterPanel();
             let contentBrowsePanel = new ContentBrowsePanel();
             await studioUtils.openFilterPanel();
-            //1. Click on 'Image' checkbox in Filter Panel:
-            await contentFilterPanel.clickOnCheckboxInContentTypesBlock("Image");
-            //2. Select 2 images:
-            await contentBrowsePanel.clickOnCheckboxAndSelectRowByName("book");
-            await contentBrowsePanel.clickOnCheckboxAndSelectRowByName("cape");
-            //3. Click on Selection Toggle (circle, Show Selection),grid gets filtered:
+            // 1. Click on 'Image' checkbox in Filter Panel:
+            await contentFilterPanel.clickOnCheckboxInContentTypesBlock('Image');
+            // 2. Select 2 images:
+            await contentBrowsePanel.clickOnCheckboxAndSelectRowByName(appConst.TEST_IMAGES.BOOK);
+            await contentBrowsePanel.clickOnCheckboxAndSelectRowByName(appConst.TEST_IMAGES.CAPE);
+            // 3. Click on Selection Toggle (circle, Show Selection),grid gets filtered:
             await contentBrowsePanel.clickOnSelectionToggler();
             await contentBrowsePanel.pause(1000);
-            //4. Click on Selection Toggle (circle, Hide Selection), initial state of thr grid is restored:
+            // 4. Click on Selection Toggle (circle, Hide Selection), initial state of thr grid is restored:
             await contentBrowsePanel.clickOnSelectionToggler();
             await contentBrowsePanel.pause(500);
-            await studioUtils.saveScreenshot("selection_toggle_checkbox_partial");
-            //5. Verify 'Selection Controller' checkBox shows that the selection is partial:
+            await studioUtils.saveScreenshot('selection_toggle_checkbox_partial');
+            // 5. Verify 'Selection Controller' checkBox shows that the selection is partial:
             await contentBrowsePanel.waitForSelectionControllerPartial();
         });
 
-    //Verifies https://github.com/enonic/lib-admin-ui/issues/1287
-    //selecting/deselecting hidden or collapsed nodes spawns error
+    // Verifies https://github.com/enonic/lib-admin-ui/issues/1287
+    // selecting/deselecting hidden or collapsed nodes spawns error
     it("GIVEN 'Show Selection' and 'Hide Selection' sequentially clicked in filtered grid WHEN 'Selection Controller' checkbox has been clicked THEN the checkbox gets unselected",
         async () => {
             let contentFilterPanel = new ContentFilterPanel();
             let contentBrowsePanel = new ContentBrowsePanel();
             await studioUtils.openFilterPanel();
-            //1. Click on 'Image' checkbox in Filter Panel:
-            await contentFilterPanel.clickOnCheckboxInContentTypesBlock("Image");
-            //2. Select 2 images:
-            await contentBrowsePanel.clickOnCheckboxAndSelectRowByName("book");
-            await contentBrowsePanel.clickOnCheckboxAndSelectRowByName("cape");
-            //3. Click on Selection Toggle (circle, Show Selection):
+            // 1. Click on 'Image' checkbox in Filter Panel:
+            await contentFilterPanel.clickOnCheckboxInContentTypesBlock('Image');
+            // 2. Select 2 images:
+            await contentBrowsePanel.clickOnCheckboxAndSelectRowByName(appConst.TEST_IMAGES.BOOK);
+            await contentBrowsePanel.clickOnCheckboxAndSelectRowByName(appConst.TEST_IMAGES.CAPE);
+            // 3. Click on Selection Toggle (circle, Show Selection):
             await contentBrowsePanel.clickOnSelectionToggler();
             await contentBrowsePanel.pause(1000);
-            //4. Verify that only 2 images are present in  the grid
+            // 4. Verify that only 2 images are present in  the grid
             let result1 = await contentBrowsePanel.getDisplayNamesInGrid();
-            assert.equal(result1.length, 2, "Two items should be present in the grid");
-            //5. Click on 'Selection Toggle' (circle, Hide Selection):
+            assert.equal(result1.length, 2, 'Two items should be present in the grid');
+            // 5. Click on 'Selection Toggle' (circle, Hide Selection):
             await contentBrowsePanel.clickOnSelectionToggler();
-            await studioUtils.saveScreenshot("selection_toggle_hide_selection");
-            //6. Click on 'Selection Controller' checkbox:
+            await studioUtils.saveScreenshot('selection_toggle_hide_selection');
+            // 6. Click on 'Selection Controller' checkbox:
             await contentBrowsePanel.clickOnSelectionControllerCheckbox();
             await contentBrowsePanel.pause(1000);
-            //7. Verify that initial grid is loaded:
-            await studioUtils.saveScreenshot("selection_toggle_initial_grid_restored");
+            // 7. Verify that initial grid is loaded:
+            await studioUtils.saveScreenshot('selection_toggle_initial_grid_restored');
             let result2 = await contentBrowsePanel.getDisplayNamesInGrid();
             assert.isAbove(result2.length, result1.length);
-            //8. Verify that Selection Controller checkBox gets unselected:
+            // 8. Verify that Selection Controller checkBox gets unselected:
             let isSelected = await contentBrowsePanel.isSelectionControllerSelected();
-            assert.isFalse(isSelected, "Selection Controller checkbox should be unselected");
+            assert.isFalse(isSelected, "'Selection Controller' checkbox should be unselected");
         });
 
     beforeEach(() => studioUtils.navigateToContentStudioApp());
     afterEach(() => studioUtils.doCloseAllWindowTabsAndSwitchToHome());
     before(async () => {
-        if (typeof browser !== "undefined") {
+        if (typeof browser !== 'undefined') {
             await studioUtils.getBrowser().setWindowSize(appConst.BROWSER_WIDTH, appConst.BROWSER_HEIGHT);
         }
         return console.log('specification starting: ' + this.title);
