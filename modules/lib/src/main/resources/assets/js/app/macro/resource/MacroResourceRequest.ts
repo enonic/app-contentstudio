de394ef9ca6a1fb954d38a7ef3b75474bc68d318
alansemenov <ase@enonic.com>
2023-08-02 12:47:14 +0200

    Incorrect repository/project in macro context #6617

--- modules/lib/src/main/resources/assets/js/app/macro/resource/MacroResourceRequest.ts
+++ modules/lib/src/main/resources/assets/js/app/macro/resource/MacroResourceRequest.ts
@@ -1,7 +1,7 @@
-import {CmsResourceRequest} from '../../resource/CmsResourceRequest';
+import {CmsProjectBasedResourceRequest} from '../../wizard/CmsProjectBasedResourceRequest';
 
 export class MacroResourceRequest<PARSED_TYPE>
-    extends CmsResourceRequest<PARSED_TYPE> {
+    extends CmsProjectBasedResourceRequest<PARSED_TYPE> {
 
     constructor() {
         super();
