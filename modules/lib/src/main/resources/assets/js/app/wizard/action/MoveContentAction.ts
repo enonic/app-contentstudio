1b243f327fd8a443fbe208c5213c73ce79ad41a5
Mikita Taukachou <edloidas@gmail.com>
2023-09-07 18:18:24 +0300

    Enable move from edit mode #6678
    Removed unnecessary events.

--- modules/lib/src/main/resources/assets/js/app/wizard/action/MoveContentAction.ts
+++ modules/lib/src/main/resources/assets/js/app/wizard/action/MoveContentAction.ts
@@ -2,8 +2,7 @@ import {WizardPanel} from '@enonic/lib-admin-ui/app/wizard/WizardPanel';
 import {Action} from '@enonic/lib-admin-ui/ui/Action';
 import {i18n} from '@enonic/lib-admin-ui/util/Messages';
 import {Content} from '../../content/Content';
-import {ContentSummaryAndCompareStatus} from '../../content/ContentSummaryAndCompareStatus';
-import {MoveContentEvent} from '../../move/MoveContentEvent';
+import {ContentMovePromptEvent} from '../../move/ContentMovePromptEvent';
 
 export class MoveContentAction
     extends Action {
@@ -11,8 +10,8 @@ export class MoveContentAction
     constructor(wizardPanel: WizardPanel<Content>) {
         super(i18n('action.moveMore'), 'alt+m');
         this.onExecuted(() => {
-            const content = ContentSummaryAndCompareStatus.fromContentSummary(wizardPanel.getPersistedItem());
-            new MoveContentEvent([content]).fire();
+            const content = wizardPanel.getPersistedItem();
+            new ContentMovePromptEvent([content]).fire();
         });
     }
 }
