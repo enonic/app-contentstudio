51e4a9855040d50f3636dac2ba932883cb223448
sgauruseu <sig@eninic.com>
2023-11-15 16:53:57 +0300

    ui-tests issue #7069

--- testing/specs/modal-dialog/page.template.widget.spec.js
+++ testing/specs/modal-dialog/page.template.widget.spec.js
@@ -13,18 +13,18 @@ const PageTemplateForm = require('../../page_objects/wizardpanel/page.template.f
 
 describe('page.template.widget.spec: Tests for page template widget in Details Panel', function () {
     this.timeout(appConst.SUITE_TIMEOUT);
-    if (typeof browser === "undefined") {
+    if (typeof browser === 'undefined') {
         webDriverHelper.setupBrowser();
     }
-    let COUNTRY_LIST_CONTROLLER = "Country List";
+    const COUNTRY_LIST_CONTROLLER = 'Country List';
     let SITE;
-    const TEMPLATE_NAME = appConst.generateRandomName("template");
+    const TEMPLATE_NAME = appConst.generateRandomName('template');
 
     it("WHEN image content is selected THEN 'Page Template is not used' should be present",
         async () => {
             let pageTemplateWidget = new PageTemplateWidget();
             await studioUtils.findAndSelectItem(appConst.TEST_IMAGES.HAND);
-            await studioUtils.saveScreenshot("template_widget_image");
+            await studioUtils.saveScreenshot('template_widget_image');
             await pageTemplateWidget.waitForNoTemplateMessageDisplayed();
         });
 
@@ -34,15 +34,15 @@ describe('page.template.widget.spec: Tests for page template widget in Details P
             SITE = contentBuilder.buildSite(displayName, 'My first Site', [appConst.MY_FIRST_APP], COUNTRY_LIST_CONTROLLER);
             await studioUtils.doAddSite(SITE);
             let pageTemplateWidget = new PageTemplateWidget();
-            //1. Select a site with a controller
+            // 1. Select a site with a controller
             await studioUtils.findAndSelectItem(SITE.displayName);
-            await studioUtils.saveScreenshot("template_widget_site");
-            //2. Verify that controller's name is displayed in the widget
+            await studioUtils.saveScreenshot('template_widget_site');
+            // 2. Verify that controller's name is displayed in the widget
             let controllerName = await pageTemplateWidget.getControllerName();
             assert.equal(controllerName, COUNTRY_LIST_CONTROLLER, "Expected controller name should be displayed in the widget");
-            //7. Verify that 'Custom' controller type is displayed:
+            // 7. Verify that 'Custom' controller type is displayed:
             let type = await pageTemplateWidget.getControllerType();
-            assert.equal(type, "Custom", "'Custom' template should be present in the widget");
+            assert.equal(type, 'Custom', "'Custom' template should be present in the widget");
         });
 
     it("WHEN site with a template has been selected THEN link to template should be displayed in the widget",
@@ -51,29 +51,29 @@ describe('page.template.widget.spec: Tests for page template widget in Details P
             let pageTemplateForm = new PageTemplateForm();
             let displayName = appConst.generateRandomName('site');
             SITE = contentBuilder.buildSite(displayName, 'My first Site', [appConst.MY_FIRST_APP]);
-            //1. Add a site:
+            // 1. Add a site:
             await studioUtils.doAddSite(SITE);
-            //2. Open new wizard for template
+            // 2. Open new wizard for template
             await studioUtils.doOpenPageTemplateWizard(SITE.displayName);
-            //3. Fill in the name input, select a page descriptor:
+            // 3. Fill in the name input, select a page descriptor:
             await contentWizard.typeDisplayName(TEMPLATE_NAME);
-            //4.Select 'Site' in support selector:
+            // 4.Select 'Site' in support selector:
             await pageTemplateForm.filterOptionsAndSelectSupport(appConst.TEMPLATE_SUPPORT.SITE)
             await contentWizard.selectPageDescriptor(COUNTRY_LIST_CONTROLLER);
             await contentWizard.waitForSaveButtonDisabled();
 
             await studioUtils.doCloseCurrentBrowserTab();
             await studioUtils.doSwitchToContentBrowsePanel();
-            //5. Select the site:
+            // 5. Select the site:
             let pageTemplateWidget = new PageTemplateWidget();
             await studioUtils.findAndSelectItem(SITE.displayName);
-            await studioUtils.saveScreenshot("template_widget_site_with_template");
-            //6. Verify that the template's name is displayed in the link
+            await studioUtils.saveScreenshot('template_widget_site_with_template');
+            // 6. Verify that the template's name is displayed in the link
             let templateName = await pageTemplateWidget.getControllerLink();
             assert.equal(templateName, TEMPLATE_NAME, "Expected template name should be displayed in the template widget");
-            //7. Verify that Automatic controller type is displayed:
+            // 7. Verify that Automatic controller type is displayed:
             let type = await pageTemplateWidget.getControllerType();
-            assert.equal(type, "Automatic", "Automatic template should be present in the widget");
+            assert.equal(type, 'Automatic', "Automatic template should be present in the widget");
         });
 
     beforeEach(() => studioUtils.navigateToContentStudioApp());
@@ -81,7 +81,7 @@ describe('page.template.widget.spec: Tests for page template widget in Details P
         return studioUtils.doCloseAllWindowTabsAndSwitchToHome();
     });
     before(async () => {
-        if (typeof browser !== "undefined") {
+        if (typeof browser !== 'undefined') {
             await studioUtils.getBrowser().setWindowSize(appConst.BROWSER_WIDTH, appConst.BROWSER_HEIGHT);
         }
         return console.log('specification starting: ' + this.title);
---
+++ testing/specs/modal-dialog/publish.wizard.deleted.dependencies.js
@@ -0,0 +1,115 @@
+/**
+ * Created on 15.11.2023
+ */
+const chai = require('chai');
+const assert = chai.assert;
+const webDriverHelper = require('../../libs/WebDriverHelper');
+const studioUtils = require('../../libs/studio.utils.js');
+const contentBuilder = require("../../libs/content.builder");
+const appConst = require('../../libs/app_const');
+const ContentBrowsePanel = require('../../page_objects/browsepanel/content.browse.panel');
+const ContentPublishDialog = require('../../page_objects/content.publish.dialog');
+const ShortcutForm = require('../../page_objects/wizardpanel/shortcut.form.panel');
+const ContentWizard = require('../../page_objects/wizardpanel/content.wizard.panel');
+const ContentDuplicateDialog = require('../../page_objects/content.duplicate.dialog');
+const PageComponentView = require('../../page_objects/wizardpanel/liveform/page.components.view');
+const TextComponentCke = require('../../page_objects/components/text.component');
+const InsertImageDialog = require('../../page_objects/wizardpanel/html-area/insert.image.dialog.cke');
+const DeleteContentDialog = require('../../page_objects/delete.content.dialog');
+
+
+describe('publish.dialog.dependant.items.spec: tests for dependant items', function () {
+    this.timeout(appConst.SUITE_TIMEOUT);
+    if (typeof browser === 'undefined') {
+        webDriverHelper.setupBrowser();
+    }
+    let SITE;
+    let SHORTCUT_NAME = appConst.generateRandomName('sh');
+    const CONTROLLER_NAME = 'Page';
+    const TEST_IMAGE_NAME = appConst.TEST_IMAGES.CAPE + '.jpg-copy';
+
+    it(`Precondition 1: new site should be created`,
+        async () => {
+            let contentBrowsePanel = new ContentBrowsePanel();
+            let contentDuplicateDialog = new ContentDuplicateDialog();
+            let displayName = contentBuilder.generateRandomName('site');
+            SITE = contentBuilder.buildSite(displayName, 'description', [appConst.APP_CONTENT_TYPES], CONTROLLER_NAME);
+            await studioUtils.doAddSite(SITE);
+            await studioUtils.findAndSelectItem(appConst.TEST_IMAGES.CAPE);
+            await contentBrowsePanel.clickOnDuplicateButtonAndWait();
+            await contentDuplicateDialog.clickOnDuplicateButton();
+            await contentDuplicateDialog.waitForDialogClosed();
+        });
+
+    it(`Precondition 2: new shortcut should be added`,
+        async () => {
+            let shortcutForm = new ShortcutForm();
+            let contentWizard = new ContentWizard();
+            // 1. Open shortcut-wizard:
+            await studioUtils.openContentWizard(appConst.contentTypes.SHORTCUT);
+            await contentWizard.typeDisplayName(SHORTCUT_NAME);
+            // 2. Select the site in target-selector:
+            await shortcutForm.filterOptionsAndSelectTarget(SITE.displayName);
+            await contentWizard.waitAndClickOnSave();
+            await contentWizard.waitForNotificationMessage();
+        });
+
+    it(`Precondition 3: insert the image in the site`,
+        async () => {
+            let pageComponentView = new PageComponentView();
+            let contentWizard = new ContentWizard();
+            let textComponentCke = new TextComponentCke();
+            let insertImageDialog = new InsertImageDialog();
+            // 1. Open shortcut-wizard:
+            await studioUtils.selectContentAndOpenWizard(SITE.displayName);
+            // 2. Maximize the Live Edit:
+            await contentWizard.clickOnMinimizeLiveEditToggler();
+            // 3. Insert the text component:
+            await pageComponentView.openMenu('main');
+            await pageComponentView.selectMenuItem(['Insert', 'Text']);
+            await textComponentCke.switchToLiveEditFrame();
+            // 4. Open 'Insert Image' dialog and insert an image in htmlArea:
+            await textComponentCke.clickOnInsertImageButton();
+            await insertImageDialog.filterAndSelectImageByPath(TEST_IMAGE_NAME);
+            await insertImageDialog.clickOnInsertButton();
+            await insertImageDialog.waitForDialogClosed();
+            await contentWizard.clickOnMarkAsReadyButton();
+            await contentWizard.waitForNotificationMessage();
+        });
+
+    // Verify Missing outbound references prevent publishing of content #7066
+    // https://github.com/enonic/app-contentstudio/issues/7066
+    it(`GIVEN outbound dependency has been deleted WHEN shortcut with outbound dependency has been selected AND Publish menu item has been clicked THEN Publish wizard should be loaded with 'Mark as Ready' button`,
+        async () => {
+            let deleteContentDialog = new DeleteContentDialog();
+            let contentPublishDialog = new ContentPublishDialog();
+            let contentBrowsePanel = new ContentBrowsePanel();
+            // 1. Delete the image (outbound dependency in the site)
+            await studioUtils.findAndSelectItem(TEST_IMAGE_NAME);
+            await contentBrowsePanel.clickOnArchiveButton();
+            await deleteContentDialog.waitForDialogOpened();
+            await deleteContentDialog.clickOnIgnoreInboundReferences();
+            await deleteContentDialog.clickOnDeleteMenuItem();
+            await deleteContentDialog.waitForDialogClosed();
+            // 2. Select the shortcut:
+            await studioUtils.findAndSelectItem(SHORTCUT_NAME);
+            // 3. Click on Publish... menu item
+            await contentBrowsePanel.openPublishMenuSelectItem(appConst.PUBLISH_MENU.PUBLISH);
+            await contentPublishDialog.waitForDialogOpened();
+            await studioUtils.saveScreenshot('publish_outbound_item_deleted');
+            // 4. Verify that 'Mark as Ready' button is displayed in the dialog, then click on it
+            await contentPublishDialog.waitForMarkAsReadyButtonDisplayed();
+            await contentPublishDialog.clickOnMarkAsReadyButton();
+            // 5. Verify that Publish Now button is enabled
+            await contentPublishDialog.waitForPublishNowButtonEnabled();
+        });
+
+    beforeEach(() => studioUtils.navigateToContentStudioApp());
+    afterEach(() => studioUtils.doCloseAllWindowTabsAndSwitchToHome());
+    before(async () => {
+        if (typeof browser !== 'undefined') {
+            await studioUtils.getBrowser().setWindowSize(appConst.BROWSER_WIDTH, appConst.BROWSER_HEIGHT);
+        }
+        return console.log('specification starting: ' + this.title);
+    });
+});
