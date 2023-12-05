2722416f156c4e8d36f96e92f54537c3ce45fb14
ashklianko <asl@enonic.com>
2023-12-05 14:33:10 +0100

    Slickgrid POC: replace ContentSelector's tree list with ListBox#7068

--- modules/lib/src/main/resources/assets/js/app/inputtype/selector/ContentTreeSelectionWrapper.ts
+++ modules/lib/src/main/resources/assets/js/app/inputtype/selector/ContentTreeSelectionWrapper.ts
@@ -3,4 +3,7 @@ import {ContentTreeSelectorItem} from '../../item/ContentTreeSelectorItem';
 
 export class ContentTreeSelectionWrapper extends SelectableListBoxWrapper<ContentTreeSelectorItem> {
 
+    toggleItemWrapperSelected(itemId: string, isSelected: boolean) {
+        super.toggleItemWrapperSelected(itemId, isSelected);
+    }
 }
