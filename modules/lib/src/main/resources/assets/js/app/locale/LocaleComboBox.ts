911c25b705431a6f168392ab7a52ee52a4667564
ashklianko <asl@enonic.com>
2024-01-05 14:38:00 +0100

    LocaleComboBox: refactor to use non-slickgrid dropdown #7228

--- modules/lib/src/main/resources/assets/js/app/locale/LocaleComboBox.ts
+++ modules/lib/src/main/resources/assets/js/app/locale/LocaleComboBox.ts
@@ -1,28 +1,126 @@
 import {Option} from '@enonic/lib-admin-ui/ui/selector/Option';
 import {Locale} from '@enonic/lib-admin-ui/locale/Locale';
 import {SelectedOption} from '@enonic/lib-admin-ui/ui/selector/combobox/SelectedOption';
-import {RichComboBox, RichComboBoxBuilder} from '@enonic/lib-admin-ui/ui/selector/combobox/RichComboBox';
 import {SelectedOptionView} from '@enonic/lib-admin-ui/ui/selector/combobox/SelectedOptionView';
 import {BaseSelectedOptionsView} from '@enonic/lib-admin-ui/ui/selector/combobox/BaseSelectedOptionsView';
 import {LocaleViewer} from './LocaleViewer';
-import {Viewer} from '@enonic/lib-admin-ui/ui/Viewer';
-import {SelectedOptionsView} from '@enonic/lib-admin-ui/ui/selector/combobox/SelectedOptionsView';
+import {FilterableListBoxWrapperWithSelectedView} from '@enonic/lib-admin-ui/ui/selector/list/FilterableListBoxWrapperWithSelectedView';
+import {LocaleListBox} from './LocaleListBox';
+import {FormInputEl} from '@enonic/lib-admin-ui/dom/FormInputEl';
 import {LocaleLoader} from './LocaleLoader';
+import {DefaultErrorHandler} from '@enonic/lib-admin-ui/DefaultErrorHandler';
+import {LoadedDataEvent} from '@enonic/lib-admin-ui/util/loader/event/LoadedDataEvent';
+import {SelectionChange} from '@enonic/lib-admin-ui/util/SelectionChange';
+import {StringHelper} from '@enonic/lib-admin-ui/util/StringHelper';
+
+export interface LocaleComboBoxOptions {
+    selectedOptionsView?: LocaleSelectedOptionsView;
+    maxSelected?: number;
+}
 
 export class LocaleComboBox
-    extends RichComboBox<Locale> {
-    constructor(builder: LocaleComboBoxBuilder = new LocaleComboBoxBuilder()) {
-        super(builder);
-        this.addClass('locale-combobox');
+    extends FilterableListBoxWrapperWithSelectedView<Locale> {
+
+    private selectedLocale: Locale;
+
+    private localeLoader: LocaleLoader;
+
+    constructor(options?: LocaleComboBoxOptions) {
+        super(new LocaleListBox(), {
+            selectedOptionsView: options?.selectedOptionsView || new LocaleSelectedOptionsView(),
+            maxSelected:  options?.maxSelected ?? 1,
+            className: 'locale-combobox',
+            filter: LocaleComboBox.filter
+        });
     }
 
-    static create(): LocaleComboBoxBuilder {
-        return new LocaleComboBoxBuilder();
+    protected initElements(): void {
+        super.initElements();
+
+        this.localeLoader = new LocaleLoader();
     }
 
-    clearSelection(forceClear: boolean = false) {
-        this.getLoader().search('');
-        super.clearSelection(forceClear);
+    protected initListeners(): void {
+        super.initListeners();
+
+        this.localeLoader.onLoadedData((event: LoadedDataEvent<Locale>) => {
+            if (this.listBox.getItemCount() === 0) {
+                this.listBox.setItems(event.getData());
+            }
+            return null;
+        });
+
+        this.listBox.whenShown(() => {
+            if (!this.localeLoader.isLoaded() && !this.localeLoader.isLoading()) {
+                this.localeLoader.load().catch(DefaultErrorHandler.handle);
+            }
+        });
+
+        this.onSelectionChanged((selection: SelectionChange<Locale>) => {
+            this.selectedLocale = selection.selected?.length > 0 ? selection.selected[0] : null;
+        });
+    }
+
+    createSelectedOption(item: Locale): Option<Locale> {
+        return Option.create<Locale>()
+            .setValue(item.getId())
+            .setDisplayValue(item)
+            .build();
+    }
+
+    setSelectedLocale(locale: string | Locale): void {
+        if (!locale) {
+            if (this.selectedLocale) {
+                this.deselect(this.selectedLocale);
+            }
+        } else if (locale instanceof Locale) {
+            this.select(locale);
+        } else {
+            if (this.localeLoader.isLoaded()) {
+                this.selectLocaleById(locale);
+            } else {
+                this.localeLoader.load().then(() => {
+                    this.selectLocaleById(locale);
+                }).catch(DefaultErrorHandler.handle);
+            }
+        }
+    }
+
+    getSelectedLocate(): Locale {
+        return this.selectedLocale;
+    }
+
+    openForTyping(): void {
+        this.optionFilterInput.openForTyping();
+    }
+
+    getValue(): string {
+        return this.selectedLocale?.getId() || null;
+    }
+
+    private selectLocaleById(id: string): void {
+        const locale: Locale = this.getLocaleById(id);
+
+        if (locale) {
+            this.select(locale);
+        }
+    }
+
+    private getLocaleById(id: string): Locale {
+        return this.localeLoader.getResults().find((locale: Locale) => locale.getId() === id);
+    }
+
+    private static filter(locale: Locale, searchString: string): boolean {
+        const str = searchString.trim().toLowerCase();
+
+        return locale.getTag().toLowerCase().indexOf(str) >= 0 ||
+               locale.getDisplayName().toLowerCase().indexOf(str) >= 0 ||
+               locale.getLanguage().toLowerCase().indexOf(str) >= 0 ||
+               locale.getDisplayLanguage().toLowerCase().indexOf(str) >= 0 ||
+               locale.getVariant().toLowerCase().indexOf(str) >= 0 ||
+               locale.getDisplayVariant().toLowerCase().indexOf(str) >= 0 ||
+               locale.getCountry().toLowerCase().indexOf(str) >= 0 ||
+               locale.getDisplayCountry().toLowerCase().indexOf(str) >= 0;
     }
 }
 
@@ -46,7 +144,6 @@ export class LocaleSelectedOptionView
     getOption(): Option<Locale> {
         return this.option;
     }
-
 }
 
 export class LocaleSelectedOptionsView
@@ -57,31 +154,28 @@ export class LocaleSelectedOptionsView
     }
 
     createSelectedOption(option: Option<Locale>): SelectedOption<Locale> {
-        let optionView = new LocaleSelectedOptionView(option);
+        const optionView = new LocaleSelectedOptionView(option);
         return new SelectedOption<Locale>(optionView, this.count());
     }
-
 }
 
-export class LocaleComboBoxBuilder
-    extends RichComboBoxBuilder<Locale> {
+export class LocaleFormInputElWrapper
+    extends FormInputEl {
 
-    rowHeight: number = 30;
+    private readonly selector: LocaleComboBox;
 
-    comboBoxName: string = 'localeSelector';
+    constructor(selector: LocaleComboBox) {
+        super('div', 'content-selector-wrapper');
 
-    loader: LocaleLoader = new LocaleLoader();
-
-    value: string;
-
-    optionDisplayValueViewer: Viewer<Locale> = new LocaleViewer();
-
-    delayedInputValueChangedHandling: number = 500;
-
-    selectedOptionsView: SelectedOptionsView<Locale> = new LocaleSelectedOptionsView();
-
-    build(): LocaleComboBox {
-        return new LocaleComboBox(this);
+        this.selector = selector;
+        this.appendChild(this.selector);
     }
 
+    getComboBox(): LocaleComboBox {
+        return this.selector;
+    }
+
+    getValue(): string {
+        return this.selector.getValue();
+    }
 }
---
+++ modules/lib/src/main/resources/assets/js/app/locale/LocaleListBox.ts
@@ -0,0 +1,23 @@
+import {ListBox} from '@enonic/lib-admin-ui/ui/selector/list/ListBox';
+import {Locale} from '@enonic/lib-admin-ui/locale/Locale';
+import {LocaleViewer} from './LocaleViewer';
+
+export class LocaleListBox extends ListBox<Locale> {
+
+    constructor() {
+        super('locale-list-box');
+    }
+
+    protected createItemView(item: Locale, readOnly: boolean): LocaleViewer {
+        const viewer = new LocaleViewer();
+
+        viewer.setObject(item);
+
+        return viewer;
+    }
+
+    protected getItemId(item: Locale): string {
+        return item.getId();
+    }
+
+}
