59b196b30d05fa8e21e488d33b641d08c77c6c89
ashklianko <asl@enonic.com>
2024-01-10 18:56:49 +0100

    ComponentDescriptorsComboBox: refactor to use non-slickgrid dropdown#7226

--- modules/lib/src/main/resources/assets/js/page-editor/DescriptorBasedComponentViewPlaceholder.ts
+++ modules/lib/src/main/resources/assets/js/page-editor/DescriptorBasedComponentViewPlaceholder.ts
@@ -1,16 +1,16 @@
 import {ItemViewPlaceholder} from './ItemViewPlaceholder';
-import {SelectedOptionEvent} from '@enonic/lib-admin-ui/ui/selector/combobox/SelectedOptionEvent';
-import {ComponentDescriptorsComboBox} from './ComponentDescriptorsComboBox';
 import {Descriptor} from '../app/page/Descriptor';
 import {ComponentType} from '../app/page/region/ComponentType';
 import {ContentId} from '../app/content/ContentId';
 import {SetComponentDescriptorEvent} from './event/outgoing/manipulation/SetComponentDescriptorEvent';
+import {ComponentDescriptorsDropdown} from '../app/wizard/page/contextwindow/inspect/region/ComponentDescriptorsDropdown';
+import {SelectionChange} from '@enonic/lib-admin-ui/util/SelectionChange';
 import {DescriptorBasedComponentView} from './DescriptorBasedComponentView';
 
 export abstract class DescriptorBasedComponentViewPlaceholder
     extends ItemViewPlaceholder {
 
-    private comboBox: ComponentDescriptorsComboBox;
+    private comboBox: ComponentDescriptorsDropdown;
 
     private componentView: DescriptorBasedComponentView;
 
@@ -27,16 +27,20 @@ export abstract class DescriptorBasedComponentViewPlaceholder
     }
 
     protected initElements(): void {
-        this.comboBox = new ComponentDescriptorsComboBox(this.getType());
+        this.comboBox = new ComponentDescriptorsDropdown();
+        this.comboBox.setComponentType(this.getType());
+        this.comboBox.setContentId(new ContentId(this.componentView.getLiveEditParams().contentId));
         this.appendChild(this.comboBox);
     }
 
     protected initListeners(): void {
-        this.comboBox.onOptionSelected((event: SelectedOptionEvent<Descriptor>) => {
-            this.componentView.showLoadingSpinner();
-            const descriptor: Descriptor = event.getSelectedOption().getOption().getDisplayValue();
+        this.comboBox.onSelectionChanged((selectionChange: SelectionChange<Descriptor>) => {
+           if (selectionChange.selected?.length > 0) {
+               this.componentView.showLoadingSpinner();
+               const descriptor: Descriptor = selectionChange.selected[0];
 
-            new SetComponentDescriptorEvent(this.componentView.getPath(), descriptor.getKey().toString()).fire();
+               new SetComponentDescriptorEvent(this.componentView.getPath(), descriptor.getKey().toString()).fire();
+           }
         });
 
         // not letting events to fire on ItemView
@@ -51,7 +55,7 @@ export abstract class DescriptorBasedComponentViewPlaceholder
 
     private reloadDescriptors(contentId: ContentId): void {
         this.comboBox.setContentId(contentId);
-        this.comboBox.getLoader().load();
+        this.comboBox.load();
     }
 
     select(): void {
