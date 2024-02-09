a5e5ad5065fb853e03f6541f294b0c8e37c43996
sgauruseu <sig@eninic.com>
2024-02-09 15:25:02 +0300

    ui-tests  issue #7325

--- testing/page_objects/wizardpanel/liveform/inspection/base.component.inspection.panel.js
+++ testing/page_objects/wizardpanel/liveform/inspection/base.component.inspection.panel.js
@@ -5,7 +5,7 @@ const Page = require('../../../page');
 const lib = require('../../../../libs/elements');
 const appConst = require('../../../../libs/app_const');
 
-//Context Window, Base Inspect tab for all components
+// Context Window, Base Inspect tab for all components
 class BaseComponentInspectionPanel extends Page {
 
     get applyButton() {
@@ -13,13 +13,33 @@ class BaseComponentInspectionPanel extends Page {
     }
 
     async clickOnApplyButton() {
-        await this.waitForApplyButtonDisplayed();
+        await this.waitForApplyButtonEnabled();
         await this.clickOnElement(this.applyButton);
         return await this.pause(2000);
     }
 
     waitForApplyButtonDisplayed() {
         return this.waitForElementDisplayed(this.applyButton, appConst.mediumTimeout);
+    }
+
+    async waitForApplyButtonEnabled() {
+        try {
+            await this.waitForElementEnabled(this.applyButton, appConst.mediumTimeout);
+            await this.pause(400);
+        } catch (err) {
+            let screenshot = await this.saveScreenshotUniqueName('err_apply_button');
+            throw new Error('Inspection Panel - Apply button is not enabled , screenshot: ' + screenshot + ' ' + err);
+        }
+    }
+
+    async waitForApplyButtonDisabled() {
+        try {
+            await this.waitForElementDisabled(this.applyButton, appConst.mediumTimeout);
+            await this.pause(400);
+        } catch (err) {
+            let screenshot = await this.saveScreenshotUniqueName('err_apply_button');
+            throw new Error('Inspection Panel - Apply button should be disabled , screenshot: ' + screenshot + ' ' + err);
+        }
     }
 }
 
