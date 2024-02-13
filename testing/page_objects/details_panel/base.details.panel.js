88812f9da03611863e642695161fe209a1cb504c
sgauruseu <sig@eninic.com>
2024-02-13 13:38:27 +0300

    update ui-tests for combobox issue #7359

--- testing/page_objects/details_panel/base.details.panel.js
+++ testing/page_objects/details_panel/base.details.panel.js
@@ -52,19 +52,18 @@ class BaseDetailsPanel extends Page {
         return await this.getTextInElements(locator);
     }
 
-    //clicks on dropdown handle and select the 'Version History' menu item
+    // clicks on dropdown handle and select the 'Version History' menu item
     async openVersionHistory() {
         try {
             await this.clickOnWidgetSelectorDropdownHandle();
-            let versionHistoryOption = this.widgetSelectorDropdown +
-                                       lib.itemByDisplayName(appConst.WIDGET_SELECTOR_OPTIONS.VERSION_HISTORY);
-            await this.waitForElementDisplayed(versionHistoryOption, appConst.mediumTimeout);
-            let elements = await this.getDisplayedElements(versionHistoryOption);
+            let dropdownOption = this.widgetSelectorDropdown + lib.itemByDisplayName(appConst.WIDGET_SELECTOR_OPTIONS.VERSION_HISTORY);
+            await this.waitForElementDisplayed(dropdownOption, appConst.mediumTimeout);
+            let elements = await this.getDisplayedElements(dropdownOption);
             await elements[0].click();
             return await this.pause(500);
         } catch (err) {
-            await this.saveScreenshot(appConst.generateRandomName('err_open_versions'));
-            throw new Error("Error when opening Version History: " + err);
+            let screenshot =  await this.saveScreenshotUniqueName('err_open_versions');
+            throw new Error("Error occurred during opening Version History widget, screenshot: " + screenshot+ ' ' + err);
         }
     }
 
