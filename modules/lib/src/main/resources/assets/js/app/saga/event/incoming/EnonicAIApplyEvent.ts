6c06e666d428f8e7ebf94358a5be913aaae00ef6
ashklianko <asl@enonic.com>
2024-02-20 13:46:56 +0100

    AI Assistant: handle event with resulting data #7369

--- modules/lib/src/main/resources/assets/js/app/saga/event/incoming/EnonicAIApplyEvent.ts
+++ modules/lib/src/main/resources/assets/js/app/saga/event/incoming/EnonicAIApplyEvent.ts
@@ -1,18 +1,21 @@
 import {Event} from '@enonic/lib-admin-ui/event/Event';
 import {ClassHelper} from '@enonic/lib-admin-ui/ClassHelper';
+import {ResultData} from '../data/EnonicAiAssistantData';
 
-export class EnonicAiStopEvent
+export class EnonicAIApplyEvent
     extends Event {
+
+    result: ResultData;
 
     private constructor() { // event is just to be received
         super();
     }
 
-    static on(handler: (event: EnonicAiStopEvent) => void) {
+    static on(handler: (event: EnonicAIApplyEvent) => void) {
         Event.bind(ClassHelper.getFullName(this), handler);
     }
 
-    static un(handler?: (event: EnonicAiStopEvent) => void) {
+    static un(handler?: (event: EnonicAIApplyEvent) => void) {
         Event.unbind(ClassHelper.getFullName(this), handler);
     }
 
