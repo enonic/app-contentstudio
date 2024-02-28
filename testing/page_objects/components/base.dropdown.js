fcae3b95547ede6842bf67758391e4f789041152
sgauruseu <sig@eninic.com>
2024-02-28 15:17:59 +0100

    ui-tests to verify bugs #7386

--- testing/page_objects/components/base.dropdown.js
+++ testing/page_objects/components/base.dropdown.js
@@ -69,7 +69,7 @@ class BaseDropdown extends Page {
         await this.waitUntilDisplayed(parentLocator + this.optionsFilterInput, appConst.mediumTimeout);
         let elements = await this.getDisplayedElements(parentLocator + this.optionsFilterInput);
         await elements[0].setValue(text);
-        return await this.pause(200);
+        return await this.pause(300);
     }
 
     async isOptionsFilterInputDisplayed(parentLocator) {
