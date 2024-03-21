36437f1dd3f96fae0f3c7d731d4660ce9a4dc1f9
ashklianko <asl@enonic.com>
2024-03-21 13:40:28 +0100

    HtmlArea: focusing editor should insert mention into open Assistant dialog #7455

--- modules/lib/src/main/resources/assets/js/app/inputtype/text/HtmlArea.ts
+++ modules/lib/src/main/resources/assets/js/app/inputtype/text/HtmlArea.ts
@@ -218,6 +218,7 @@ export class HtmlArea
             this.notifyFocused(e);
             this.scrollToSelected(textAreaWrapper, e);
 
+            textAreaWrapper.getHTMLElement().dispatchEvent(new CustomEvent('focus')); // for AI Assistant
             AppHelper.dispatchCustomEvent('focusin', this);
         };
 
