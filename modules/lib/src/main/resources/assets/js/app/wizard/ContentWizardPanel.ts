c30cbb327f978bc2aa50bcd44755cc73f0df5948
ashklianko <asl@enonic.com>
2024-07-01 16:05:16 +0200

    Sync CS and Saga apps #7706

--- modules/lib/src/main/resources/assets/js/app/wizard/ContentWizardPanel.ts
+++ modules/lib/src/main/resources/assets/js/app/wizard/ContentWizardPanel.ts
@@ -1932,7 +1932,8 @@ export class ContentWizardPanel
                         if (this.params.localized) {
                             this.onRendered(() => {
                                 NotifyManager.get().showFeedback(i18n('notify.content.localized'));
-                                if (this.isTranslateable()) {
+
+                                if (this.isTranslatable()) {
                                     this.openTranslateConfirmationDialog();
                                 }
                             });
@@ -2700,7 +2701,7 @@ export class ContentWizardPanel
         return this.splitPanel;
     }
 
-    isTranslateable(): boolean {
+    isTranslatable(): boolean {
         const content = this.getContent();
 
         return AI.get().canTranslate() &&
