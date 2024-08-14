3388755cd07aafb23448131070a56e7bf150d95f
ashklianko <asl@enonic.com>
2024-08-14 12:59:11 +0200

    ContentTreeGrid slickgrid replacement: add filtering functionality #7756

--- modules/lib/src/main/resources/assets/js/app/browse/action/MoveContentAction.ts
+++ modules/lib/src/main/resources/assets/js/app/browse/action/MoveContentAction.ts
@@ -1,4 +1,3 @@
-import {ArrayHelper} from '@enonic/lib-admin-ui/util/ArrayHelper';
 import {i18n} from '@enonic/lib-admin-ui/util/Messages';
 import {ContentSummaryAndCompareStatus} from '../../content/ContentSummaryAndCompareStatus';
 import {ContentMovePromptEvent} from '../../move/ContentMovePromptEvent';
@@ -24,15 +23,6 @@ export class MoveContentAction
     }
 
     private isAnyRootItemNotSelected(): boolean {
-        // if there's at least one non-selected root item then there's at least one option where to move selected items
-        const selectedIds: string[] = this.getSelectedOrHighlightedItemsIds();
-        const rootItemsIds: string[] = this.grid.getList().getItems().map((item: ContentSummaryAndCompareStatus) => item.getId());
-        const diff: string[] = ArrayHelper.difference(rootItemsIds, selectedIds, (a: string, b: string) => a === b);
-
-        return diff.length > 0;
-    }
-
-    private getSelectedOrHighlightedItemsIds(): string[] {
-        return this.grid.getSelectedItems().map((item: ContentSummaryAndCompareStatus) => item.getId());
+        return this.grid.getSelectedItems().length > 0;
     }
 }
