69e7375b5616abd08b51e1f4fd128b3392f2a9bd
Mikita Taukachou <edloidas@gmail.com>
2024-09-17 13:51:08 +0300

    Update AI part to use new apps instead of Saga #7857

--- modules/lib/src/main/resources/assets/js/app/wizard/ContentWizardPanel.ts
+++ modules/lib/src/main/resources/assets/js/app/wizard/ContentWizardPanel.ts
@@ -272,7 +272,7 @@ export class ContentWizardPanel
 
     private debouncedAppsChangeHandler: () => void;
 
-    private debouncedEnonicAiContentOperatorDataChangedHandler: () => void;
+    private debouncedEnonicAiDataChangedHandler: () => void;
 
     private isFirstUpdateAndRenameEventSkiped: boolean;
 
@@ -312,7 +312,7 @@ export class ContentWizardPanel
         this.displayNameResolver = new DisplayNameResolver();
         this.xDataWizardStepForms = new XDataWizardStepForms();
         this.workflowStateManager = new WorkflowStateManager(this);
-        this.debouncedEnonicAiContentOperatorDataChangedHandler = AppHelper.debounce(() => {
+        this.debouncedEnonicAiDataChangedHandler = AppHelper.debounce(() => {
             AI.get().setCurrentData({
                 fields: this.contentWizardStepForm.getData().toJson(),
                 topic: this.getWizardHeader().getDisplayName(),
@@ -445,7 +445,7 @@ export class ContentWizardPanel
         this.getWizardHeader().onPropertyChanged(this.dataChangedHandler);
         this.getWizardHeader().onPropertyChanged((event: PropertyChangedEvent) => {
             if (event.getPropertyName() === 'displayName') {
-                this.debouncedEnonicAiContentOperatorDataChangedHandler();
+                this.debouncedEnonicAiDataChangedHandler();
             }
         });
 
@@ -477,7 +477,7 @@ export class ContentWizardPanel
             }
 
             this.updateWizardStepForms(propertyTree, false).then(() => {
-                this.debouncedEnonicAiContentOperatorDataChangedHandler();
+                this.debouncedEnonicAiDataChangedHandler();
             });
         });
     }
@@ -2017,7 +2017,7 @@ export class ContentWizardPanel
     private layoutWizardStepForms(content: Content): Q.Promise<void> {
         const contentData = content.getContentData();
         contentData.onChanged(this.dataChangedHandler);
-        contentData.onChanged(this.debouncedEnonicAiContentOperatorDataChangedHandler);
+        contentData.onChanged(this.debouncedEnonicAiDataChangedHandler);
 
         const formViewLayoutPromises: Q.Promise<void>[] = [];
         formViewLayoutPromises.push(
@@ -2507,10 +2507,10 @@ export class ContentWizardPanel
 
     private updateWizardStepForms(propertyTree: PropertyTree, unchangedOnly: boolean = true): Q.Promise<void> {
         this.contentWizardStepForm.getData().unChanged(this.dataChangedHandler);
-        this.contentWizardStepForm.getData().unChanged(this.debouncedEnonicAiContentOperatorDataChangedHandler);
+        this.contentWizardStepForm.getData().unChanged(this.debouncedEnonicAiDataChangedHandler);
 
         propertyTree.onChanged(this.dataChangedHandler);
-        propertyTree.onChanged(this.debouncedEnonicAiContentOperatorDataChangedHandler);
+        propertyTree.onChanged(this.debouncedEnonicAiDataChangedHandler);
 
         return this.contentWizardStepForm.update(propertyTree, unchangedOnly).then(() => {
             setTimeout(this.contentWizardStepForm.validate.bind(this.contentWizardStepForm), 100);
