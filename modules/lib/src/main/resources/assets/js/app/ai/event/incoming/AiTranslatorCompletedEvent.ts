b40fc7a10b4cf4cd458c9560bbb9f9bb88a66ebf
Mikita Taukachou <edloidas@gmail.com>
2024-10-21 18:06:34 +0300

    Refactor AI events #7977

--- modules/lib/src/main/resources/assets/js/app/ai/event/incoming/AiTranslatorCompletedEvent.ts
+++ modules/lib/src/main/resources/assets/js/app/ai/event/incoming/AiTranslatorCompletedEvent.ts
@@ -1,18 +1,22 @@
 import {ClassHelper} from '@enonic/lib-admin-ui/ClassHelper';
 import {Event} from '@enonic/lib-admin-ui/event/Event';
 
-export class EnonicAiContentOperatorHideEvent
+export class AiTranslatorCompletedEvent
     extends Event {
+
+    readonly path: string;
+
+    readonly value: string;
 
     private constructor() {
         super();
     }
 
-    static on(handler: (event: EnonicAiContentOperatorHideEvent) => void) {
+    static on(handler: (event: AiTranslatorCompletedEvent) => void) {
         Event.bind(ClassHelper.getFullName(this), handler);
     }
 
-    static un(handler?: (event: EnonicAiContentOperatorHideEvent) => void) {
+    static un(handler?: (event: AiTranslatorCompletedEvent) => void) {
         Event.unbind(ClassHelper.getFullName(this), handler);
     }
 
--- modules/lib/src/main/resources/assets/js/app/ai/event/incoming/AiTranslatorDialogShownEvent.ts
+++ modules/lib/src/main/resources/assets/js/app/ai/event/incoming/AiTranslatorDialogShownEvent.ts
@@ -1,22 +1,18 @@
 import {ClassHelper} from '@enonic/lib-admin-ui/ClassHelper';
 import {Event} from '@enonic/lib-admin-ui/event/Event';
 
-export class EnonicAiTranslatorCompletedEvent
+export class AiTranslatorDialogShownEvent
     extends Event {
-
-    readonly path: string;
-
-    readonly value: string;
 
     private constructor() {
         super();
     }
 
-    static on(handler: (event: EnonicAiTranslatorCompletedEvent) => void) {
+    static on(handler: (event: AiTranslatorDialogShownEvent) => void) {
         Event.bind(ClassHelper.getFullName(this), handler);
     }
 
-    static un(handler?: (event: EnonicAiTranslatorCompletedEvent) => void) {
+    static un(handler?: (event: AiTranslatorDialogShownEvent) => void) {
         Event.unbind(ClassHelper.getFullName(this), handler);
     }
 
