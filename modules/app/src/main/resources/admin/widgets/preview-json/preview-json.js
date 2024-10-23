42b2aa5d9a42e1a657286cde73bf62032bfb0590
Pavel Milkevich <pmi@enonic.com>
2024-10-23 14:25:23 +0200

    Create JSON view widget #7869

--- modules/app/src/main/resources/admin/widgets/preview-json/preview-json.js
+++ modules/app/src/main/resources/admin/widgets/preview-json/preview-json.js
@@ -2,10 +2,54 @@
 
 const portalLib = require('/lib/xp/portal');
 const contentLib = require('/lib/xp/content');
+const contextLib = require('/lib/xp/context');
 
 function handleGet(req) {
     const key = req.params.contentId;
+    const branch = req.params.branch || 'master';
+    const repository = req.params.repo;
 
+    if (!key || !repository) {
+        return {
+            status: 400,
+            contentType: 'text/plain',
+            body: 'Missing required parameter: ' + (!key ? 'contentId' : 'repo')
+        };
+    }
+
+    log.debug('JSON preview: id=' + key + ', branch=' + branch + ', repo=' + repository);
+
+    return renderInContext(repository, branch, key);
+}
+
+function renderInContext(repository, branch, key) {
+    const context = contextLib.get();
+
+    if (context.repository !== repository || context.branch !== branch) {
+        try {
+            const newContext = {
+                principals: ["role:system.admin"],
+                repository,
+                branch
+            }
+
+            return contextLib.run(newContext, function () {
+                return renderContent(key);
+            });
+        } catch (e) {
+            log.error('JSON preview failed to render content: ' + e.message);
+            return {
+                status: 500,
+                contentType: 'text/plain',
+                body: 'Failed to render content: ' + e.message
+            }
+        }
+    } else {
+        return renderContent(key);
+    }
+}
+
+function renderContent(key) {
     let content;
     if (key) {
         content = contentLib.get({key});
@@ -21,3 +65,7 @@ function handleGet(req) {
 }
 
 exports.get = handleGet;
+
+exports.canRender = function () {
+    return true;
+}
