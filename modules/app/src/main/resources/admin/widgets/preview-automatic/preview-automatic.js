3cd22a0081eb22558a3acada40b8d742552bef6c
Pavel Milkevich <pmi@enonic.com>
2024-10-23 15:37:16 +0200

    Preview mode selector #7870

--- modules/app/src/main/resources/admin/widgets/preview-automatic/preview-automatic.js
+++ modules/app/src/main/resources/admin/widgets/preview-automatic/preview-automatic.js
@@ -3,7 +3,7 @@
 const bean = __.newBean("com.enonic.xp.app.contentstudio.widget.AutomaticPreviewBean");
 
 function handleGet(req) {
-    return bean.renderByInterface('contentstudio.preview');
+    return __.toNativeObject(bean.renderByInterface('contentstudio.preview'));
 }
 
 exports.get = handleGet;
