7ddc883ba784fd464d2d3fc51f1fc354efe0843a
Pavel Milkevich <pmi@enonic.com>
2024-10-31 16:51:09 +0100

    Create fallback rendering for 418 status #8000

--- modules/app/src/main/resources/admin/widgets/preview-site-engine/preview-site-engine.js
+++ modules/app/src/main/resources/admin/widgets/preview-site-engine/preview-site-engine.js
@@ -3,7 +3,6 @@
 const widgetLib = require('/lib/export/widget');
 
 exports.get = function (req) {
-
     let params;
     try {
         params = widgetLib.validateParams(req.params);
@@ -15,27 +14,63 @@ exports.get = function (req) {
         };
     }
 
-    const url = createUrl(params);
+    if (!exports.canRender(req)) {
+        // needed for the head request,
+        // return 418 if not able to render
+        log.info('Site [GET] can\'t render: ' + 418);
 
-    return {
-        status: 200,
-        contentType: 'text/html',
-        body: `<iframe src="${url}"></iframe>`
-    };
+        return {
+            status: 418,
+            contentType: 'text/plain',
+            body: 'Cannot render site'
+        }
+    }
+
+    try {
+        const url = createUrl(req, params);
+
+        log.info('Site [GET] redirecting: ' + url);
+
+        return {
+            redirect: url,
+            contentType: 'text/html',
+        }
+    } catch (e) {
+        return {
+            status: 500,
+            contentType: 'text/plain',
+            body: 'Failed to render site: ' + e.message
+        }
+    }
 }
 
 exports.canRender = function (req) {
+    let params;
     try {
-        const params = widgetLib.validateParams(req.params);
-        const url = createUrl(params);
-        const response = widgetLib.fetchHttp(url, 'HEAD', req.headers);
-        return !!response && response.status === 200;
+        params = widgetLib.validateParams(req.params);
     } catch (e) {
+        return false;
+    }
+
+    try {
+        const url = createUrl(req, params);
+        const response = widgetLib.fetchHttp(url, 'HEAD', req.headers);
+
+        // auto: 200
+        // manual: !418
+        const canRender = (params.auto && response.status === 200)
+                          || (!params.auto && response.status !== 418);
+
+        log.info('Site [CAN_RENDER]: ' + canRender);
+
+        return canRender;
+    } catch (e) {
+        log.error('Site widget canRender failed: ' + e.message);
         return false;
     }
 }
 
-function createUrl(params) {
+function createUrl(req, params) {
     const project = params.repository.substring('com.enonic.cms.'.length);
-    return `http://localhost:8080/admin/site/inline/${project}/${params.branch}` + params.path;   //TODO: use real domain
+    return `${req.scheme}://${req.host}:${req.port}/admin/site/inline/${project}/${params.branch}` + params.path;
 }
