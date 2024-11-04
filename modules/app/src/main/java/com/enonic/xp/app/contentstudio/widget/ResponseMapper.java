69ffaf96dc965d215c984490d32acce27b58163c
Pavel Milkevich <pmi@enonic.com>
2024-11-04 16:49:04 +0100

    Create a Java bean for serving media preview #8011

---
+++ modules/app/src/main/java/com/enonic/xp/app/contentstudio/widget/ResponseMapper.java
@@ -0,0 +1,86 @@
+package com.enonic.xp.app.contentstudio.widget;
+
+import java.util.Map;
+
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.NewCookie;
+import javax.ws.rs.core.Response;
+
+import com.enonic.xp.script.serializer.MapGenerator;
+import com.enonic.xp.script.serializer.MapSerializable;
+
+public final class ResponseMapper
+    implements MapSerializable
+{
+    private final Response response;
+
+    public ResponseMapper( final Response response )
+    {
+        this.response = response;
+    }
+
+    @Override
+    public void serialize( final MapGenerator gen )
+    {
+        System.out.println( "ResponseMapper.serialize() started" );
+
+        gen.value( "status", this.response.getStatus() );
+        gen.value( "mimeType", this.response.getMediaType() );
+        gen.value( "body", this.response.getEntity() );
+        serializeHeaders( gen );
+        serializeCookies( gen );
+
+        System.out.println( "ResponseMapper.serialize() finished" );
+    }
+
+    private void serializeHeaders( final MapGenerator gen )
+    {
+        final MultivaluedMap<String, Object> headers = response.getHeaders();
+        if ( headers.isEmpty() )
+        {
+            return;
+        }
+        gen.map( "headers" );
+
+        headers.forEach( ( key, values ) -> {
+            if ( values.size() == 1 )
+            {
+                gen.value( key, values.getFirst() );
+            }
+            else
+            {
+                gen.array( key );
+                values.forEach( gen::value );
+                gen.end();
+            }
+        } );
+
+        gen.end();
+    }
+
+
+    private void serializeCookies( final MapGenerator gen )
+    {
+        final Map<String, NewCookie> cookies = response.getCookies();
+        if ( cookies.isEmpty() )
+        {
+            return;
+        }
+        gen.map( "cookies" );
+
+        cookies.forEach( ( key, cookie ) -> {
+            gen.map( key );
+            gen.value( "value", cookie.getValue() );
+            gen.value( "path", cookie.getPath() );
+            gen.value( "domain", cookie.getDomain() );
+            gen.value( "comment", cookie.getComment() );
+            gen.value( "maxAge", cookie.getMaxAge() );
+            gen.value( "secure", cookie.isSecure() );
+            gen.value( "httpOnly", cookie.isHttpOnly() );
+            gen.end();
+        } );
+
+        gen.end();
+    }
+}
+
