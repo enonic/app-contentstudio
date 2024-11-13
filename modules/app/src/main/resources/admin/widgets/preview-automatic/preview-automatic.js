f408adb86a3ad3f381f72d2b4d645e5ac8edd36e
Pavel Milkevich <pmi@enonic.com>
2024-11-13 15:52:29 +0100

    Merge live view with master #8053

--- modules/app/src/main/resources/admin/widgets/preview-automatic/preview-automatic.js
+++ modules/app/src/main/resources/admin/widgets/preview-automatic/preview-automatic.js
@@ -6,7 +6,7 @@ function handleGet(req) {
     // Add automatic flag to the request parameters for other widgets
     // to know if they were called directly or not
     req.params.auto = true;
-    return __.toNativeObject(bean.renderByInterface('contentstudio.preview'));
+    return __.toNativeObject(bean.renderByInterface('contentstudio.liveview'));
 }
 
 exports.get = handleGet;
