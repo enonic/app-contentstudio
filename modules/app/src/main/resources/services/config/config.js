ef13807bacb85d158f72d2837dff3da091b5c901
Mikita Taukachou <edloidas@gmail.com>
2024-12-03 15:52:24 +0300

    Add WebSocket service URL for Content Operator #8121

--- modules/app/src/main/resources/services/config/config.js
+++ modules/app/src/main/resources/services/config/config.js
@@ -42,7 +42,7 @@ function handleGet() {
                 collaborationUrl: portal.serviceUrl({service: 'collaboration'}),
                 appServiceUrl: portal.serviceUrl({service: 'applications'}),
                 exportServiceUrl: portal.serviceUrl({service: 'export'}),
-                aiContentOperatorServiceUrl: portal.serviceUrl({service: 'rest', application: 'com.enonic.app.ai.contentoperator'}),
+                aiContentOperatorWsServiceUrl: portal.serviceUrl({service: 'ws', application: 'com.enonic.app.ai.contentoperator', type: 'websocket'}),
                 aiTranslatorRestServiceUrl: portal.serviceUrl({service: 'rest', application: 'com.enonic.app.ai.translator'}),
                 aiTranslatorWsServiceUrl: portal.serviceUrl(
                     {service: 'ws', application: 'com.enonic.app.ai.translator', type: 'websocket'}),
