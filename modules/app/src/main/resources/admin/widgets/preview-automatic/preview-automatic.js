b5fb1652d7bf0bf7ca7f51949d39cdb56c008c0a
Pavel Milkevich <pmi@enonic.com>
2024-12-03 13:11:38 +0100

    Image is not shown in preview panel when Automatic option is selected #8085

--- modules/app/src/main/resources/admin/widgets/preview-automatic/preview-automatic.js
+++ modules/app/src/main/resources/admin/widgets/preview-automatic/preview-automatic.js
@@ -3,9 +3,6 @@
 const bean = __.newBean("com.enonic.xp.app.contentstudio.widget.AutomaticPreviewBean");
 
 function handleGet(req) {
-    // Add automatic flag to the request parameters for other widgets
-    // to know if they were called directly or not
-    req.params.auto = true;
     return __.toNativeObject(bean.renderByInterface('contentstudio.liveview'));
 }
 
