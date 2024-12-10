677e9fd4d8a6ce93395def01239e57388e86a178
sgauruseu <sig@eninic.com>
2024-12-10 11:22:57 +0100

    updated tests for issues #8133

--- testing/specs/content-types/shortcut.target.spec.js
+++ testing/specs/content-types/shortcut.target.spec.js
@@ -10,6 +10,7 @@ const ShortcutForm = require('../../page_objects/wizardpanel/shortcut.form.panel
 const ContentWizard = require('../../page_objects/wizardpanel/content.wizard.panel');
 const WizardDetailsPanel = require('../../page_objects/wizardpanel/details/wizard.details.panel');
 const WizardVersionsWidget = require('../../page_objects/wizardpanel/details/wizard.versions.widget');
+const ContentItemPreviewPanel = require('../../page_objects/browsepanel/contentItem.preview.panel');
 
 describe("Shortcut's target specification", function () {
     this.timeout(appConst.SUITE_TIMEOUT);
@@ -68,6 +69,38 @@ describe("Shortcut's target specification", function () {
             // 2.Verify the selected option:
             let actualResult = await shortcutForm.getSelectedTargetDisplayName();
             assert.equal(actualResult, TARGET_2, "Expected display name should be present in the selected option");
+        });
+
+    it(`GIVEN shortcut with an image in its target are selected WHEN 'Media' has been selected THEN 'Unable to render' message should be displayed in the Preview panel`,
+        async () => {
+            let contentItemPreviewPanel = new ContentItemPreviewPanel();
+            // 1. Select the existing shortcut to an image:
+            await studioUtils.findAndSelectItem(SHORTCUT_NAME);
+            // 2. Select 'Media' in the Preview widget dropdown:
+            await contentItemPreviewPanel.selectOptionInPreviewWidget(appConst.PREVIEW_WIDGET.MEDIA);
+            await studioUtils.saveScreenshot('shortcut_target_image_media_selected');
+            // 3. Verify that Preview button is  disabled in the toolbar in Item Preview Panel:
+            await contentItemPreviewPanel.waitForPreviewButtonDisabled();
+            // 4. Verify the message in Preview panel:
+            let actualMessage = await contentItemPreviewPanel.getNoPreviewMessage();
+            assert.ok(actualMessage.includes('Unable to render'), "'Unable to render' message should be displayed");
+        });
+
+    it(`GIVEN shortcut with an image in its target is selected WHEN 'Automatic' has been selected THEN 'Preview not available' message should be displayed in the Preview panel`,
+        async () => {
+            let contentItemPreviewPanel = new ContentItemPreviewPanel();
+            // 1. Select the existing shortcut to an image:
+            await studioUtils.findAndSelectItem(SHORTCUT_NAME);
+            // 2. 'Automatic' should be in the Preview widget dropdown by default :
+            let actualOption = await contentItemPreviewPanel.getSelectedOptionInPreviewWidget();
+            assert.equal(actualOption, appConst.PREVIEW_WIDGET.AUTOMATIC,
+                'Automatic option should be selected in preview widget by default');
+            await studioUtils.saveScreenshot('shortcut_target_image_automatic_selected');
+            // 3. Verify that 'Preview' button is disabled in the toolbar in Item Preview Panel:
+            await contentItemPreviewPanel.waitForPreviewButtonDisabled();
+            // 4. Verify the message in Preview panel:
+            let actualMessage = await contentItemPreviewPanel.getNoPreviewMessage();
+            assert.ok(actualMessage.includes('Preview not available'), "'Preview not available' message should be displayed");
         });
 
     // Verifies https://github.com/enonic/app-contentstudio/issues/2462
