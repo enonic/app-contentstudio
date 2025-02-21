221b3cb7ef2c3d63bf769ed9e0bb1c0a6340d662
Mikita Taukachou <edloidas@gmail.com>
2025-02-21 10:56:16 +0300

    Make Collaborators use SharedWorker #8302

---
+++ modules/lib/src/main/resources/assets/js/app/stores/data/worker.ts
@@ -0,0 +1,71 @@
+import {InMessage, OutMessage} from './messages';
+
+interface WorkerMessage<T> {
+    type: T;
+}
+
+type WorkerMessageWithPayload<T, P extends Record<string, unknown> = Record<string, unknown>> = WorkerMessage<T> & {
+    payload: P;
+};
+
+//
+//* Client -> Worker
+//
+
+export type SubscribeWorkerMessage = WorkerMessageWithPayload<'subscribe', {operation: string}>;
+
+export type UnsubscribeWorkerMessage = WorkerMessageWithPayload<'unsubscribe', {operation: string}>;
+
+export type SendWorkerMessage = WorkerMessageWithPayload<'send', InMessage>;
+
+export type InWorkerMessage = SubscribeWorkerMessage | UnsubscribeWorkerMessage | SendWorkerMessage;
+
+//
+//* Worker -> Client
+//
+
+export type ConnectedWorkerMessage = WorkerMessageWithPayload<'connected', {clientId: string}>;
+
+export type DisconnectedWorkerMessage = WorkerMessage<'disconnected'>;
+
+export type StatusWorkerMessage = WorkerMessageWithPayload<'status', {ready: boolean}>;
+
+export type ReceivedWorkerMessage = WorkerMessageWithPayload<'received', OutMessage>;
+
+export type OutWorkerMessage =
+    | ConnectedWorkerMessage
+    | DisconnectedWorkerMessage
+    | StatusWorkerMessage
+    | ReceivedWorkerMessage;
+
+//
+//* Utils
+//
+
+export function isWorkerMessage(message: unknown): message is InWorkerMessage | OutWorkerMessage {
+    return (
+        typeof message === 'object' &&
+        message !== null &&
+        'type' in message &&
+        'payload' in message &&
+        typeof message.type === 'string' &&
+        typeof message.payload === 'object'
+    );
+}
+
+export function isInWorkerMessage(message: unknown): message is InWorkerMessage {
+    return (
+        isWorkerMessage(message) &&
+        (message.type === 'subscribe' || message.type === 'unsubscribe' || message.type === 'send')
+    );
+}
+
+export function isOutWorkerMessage(message: unknown): message is OutWorkerMessage {
+    return (
+        isWorkerMessage(message) &&
+        (message.type === 'connected' ||
+            message.type === 'disconnected' ||
+            message.type === 'status' ||
+            message.type === 'received')
+    );
+}
