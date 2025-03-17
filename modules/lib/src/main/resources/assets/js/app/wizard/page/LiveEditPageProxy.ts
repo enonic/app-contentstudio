b21b2fbeeb1c8ba6e5fe44f7720b68b4fec0c7e5
ashklianko <asl@enonic.com>
2025-03-17 16:55:03 +0100

    Live Edit is reloaded when applying config changes #8521

--- modules/lib/src/main/resources/assets/js/app/wizard/page/LiveEditPageProxy.ts
+++ modules/lib/src/main/resources/assets/js/app/wizard/page/LiveEditPageProxy.ts
@@ -481,8 +481,8 @@ export class LiveEditPageProxy
         }
     }
 
-    public loadComponent(path: ComponentPath, uri: string): void {
-        new LoadComponentViewEvent(path, uri).fire(this.liveEditWindow);
+    public loadComponent(path: ComponentPath, uri: string, isExisting = false): void {
+        new LoadComponentViewEvent(path, uri, isExisting).fire(this.liveEditWindow);
     }
 
     public stopListening(contextWindow: Window) {
