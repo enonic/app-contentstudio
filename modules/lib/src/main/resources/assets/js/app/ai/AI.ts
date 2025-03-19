eb65c5a6c72a34547a02dcb2affdff591151c38c
Mikita Taukachou <edloidas@gmail.com>
2025-03-19 21:44:53 +0300

    Translation completed handler fails if text is undefined #8560

--- modules/lib/src/main/resources/assets/js/app/ai/AI.ts
+++ modules/lib/src/main/resources/assets/js/app/ai/AI.ts
@@ -8,6 +8,8 @@ import {DefaultErrorHandler} from '@enonic/lib-admin-ui/DefaultErrorHandler';
 import {Locale} from '@enonic/lib-admin-ui/locale/Locale';
 import {NotifyManager} from '@enonic/lib-admin-ui/notify/NotifyManager';
 import {CONFIG} from '@enonic/lib-admin-ui/util/Config';
+import {i18n} from '@enonic/lib-admin-ui/util/Messages';
+import {CompareStatus} from '../content/CompareStatus';
 import {Content} from '../content/Content';
 import {ContentRequiresSaveEvent} from '../event/ContentRequiresSaveEvent';
 import {ContentType} from '../inputtype/schema/ContentType';
@@ -27,13 +29,11 @@ import {AiContentOperatorInteractionEvent} from './event/incoming/AiContentOpera
 import {AiContentOperatorResultAppliedEvent} from './event/incoming/AiContentOperatorResultAppliedEvent';
 import {AiTranslatorAllCompletedEvent} from './event/incoming/AiTranslatorAllCompletedEvent';
 import {AiTranslatorCompletedEvent} from './event/incoming/AiTranslatorCompletedEvent';
+import {AiTranslatorNoLicenseEvent} from './event/incoming/AiTranslatorNoLicenseEvent';
 import {AiTranslatorStartedEvent} from './event/incoming/AiTranslatorStartedEvent';
 import {AiContentOperatorConfigureEvent} from './event/outgoing/AiContentOperatorConfigureEvent';
 import {AiTranslatorConfigureEvent} from './event/outgoing/AiTranslatorConfigureEvent';
 import {AiUpdateDataEvent} from './event/outgoing/AiUpdateDataEvent';
-import {CompareStatus} from '../content/CompareStatus';
-import {AiTranslatorNoLicenseEvent} from './event/incoming/AiTranslatorNoLicenseEvent';
-import {i18n} from '@enonic/lib-admin-ui/util/Messages';
 
 declare global {
     interface Window {
@@ -244,7 +244,7 @@ export class AI {
         const data = text ? {text} : undefined;
         this.aiToolHelper.setState(this.aiContentDataHelper.transformPathOnDemand(event.path), state, data);
 
-        if (event.success) {
+        if (event.success && event.text != null) {
             this.aiContentDataHelper.setValue(event.path, event.text);
         }
 
