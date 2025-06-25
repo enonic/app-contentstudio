27a8aa95043422e2c4218d7958df4ed60d51640a
Mikita Taukachou <edloidas@gmail.com>
2025-06-26 00:38:22 +0300

    POC: Enonic UI integration

--- modules/lib/src/main/resources/assets/js/app/browse/ContentBrowseToolbar.ts
+++ modules/lib/src/main/resources/assets/js/app/browse/ContentBrowseToolbar.ts
@@ -1,6 +1,7 @@
 import {ResponsiveToolbar} from './ResponsiveToolbar';
 import {Action} from '@enonic/lib-admin-ui/ui/Action';
 import {i18n} from '@enonic/lib-admin-ui/util/Messages';
+import {Button2} from '@enonic/lib-admin-ui/ui2/Button2';
 
 export class ContentBrowseToolbar
     extends ResponsiveToolbar {
@@ -15,6 +16,14 @@ export class ContentBrowseToolbar
         });
 
         this.publishAction = publishAction;
+
+        let counter = 0;
+        const btn = new Button2({label: '(0)', onClick: () => {
+            counter++;
+            btn.setProps({label: `(${counter})`});
+        }});
+
+        this.prependChild(btn);
     }
 
     protected processBeforeMobileModeOn() {
