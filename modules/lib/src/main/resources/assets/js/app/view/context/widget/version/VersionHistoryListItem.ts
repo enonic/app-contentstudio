f293cb5ef5677c4a39135a891aaec6466e21f21a
Aliaksandr Shklianko <asl@enonic.com>
2025-07-15 09:34:37 +0200

    Replace "show changes" icon with checkboxes #8909

--- modules/lib/src/main/resources/assets/js/app/view/context/widget/version/VersionHistoryListItem.ts
+++ modules/lib/src/main/resources/assets/js/app/view/context/widget/version/VersionHistoryListItem.ts
@@ -1,21 +1,20 @@
-import {DateHelper} from '@enonic/lib-admin-ui/util/DateHelper';
-import {LiEl} from '@enonic/lib-admin-ui/dom/LiEl';
-import {ActionButton} from '@enonic/lib-admin-ui/ui/button/ActionButton';
-import {VersionHistoryItemViewer} from './VersionHistoryItemViewer';
 import {DivEl} from '@enonic/lib-admin-ui/dom/DivEl';
-import {ContentSummaryAndCompareStatus} from '../../../../content/ContentSummaryAndCompareStatus';
-import {CompareContentVersionsDialog} from '../../../../dialog/CompareContentVersionsDialog';
-import {RevertVersionRequest} from '../../../../resource/RevertVersionRequest';
-import * as $ from 'jquery';
-import {i18n} from '@enonic/lib-admin-ui/util/Messages';
-import {NotifyManager} from '@enonic/lib-admin-ui/notify/NotifyManager';
-import {DefaultErrorHandler} from '@enonic/lib-admin-ui/DefaultErrorHandler';
+import {LiEl} from '@enonic/lib-admin-ui/dom/LiEl';
 import {Action} from '@enonic/lib-admin-ui/ui/Action';
+import {ActionButton} from '@enonic/lib-admin-ui/ui/button/ActionButton';
+import {Checkbox} from '@enonic/lib-admin-ui/ui/Checkbox';
 import {Tooltip} from '@enonic/lib-admin-ui/ui/Tooltip';
-import {VersionHistoryItem, VersionItemStatus} from './VersionHistoryItem';
+import {DateHelper} from '@enonic/lib-admin-ui/util/DateHelper';
+import {i18n} from '@enonic/lib-admin-ui/util/Messages';
+import * as $ from 'jquery';
+import * as Q from 'q';
+import {ContentSummaryAndCompareStatus} from '../../../../content/ContentSummaryAndCompareStatus';
+import {ContentVersionHelper} from '../../../../ContentVersionHelper';
 import {VersionContext} from './VersionContext';
 import Q from 'q';
 import {VersionHistoryHelper} from './VersionHistoryHelper';
+import {VersionHistoryItem} from './VersionHistoryItem';
+import {VersionHistoryItemViewer} from './VersionHistoryItemViewer';
 
 export class VersionHistoryListItem
     extends LiEl {
@@ -25,7 +24,9 @@ export class VersionHistoryListItem
     private readonly versionViewer: VersionHistoryItemViewer;
     private tooltip: Tooltip;
     private actionButton: ActionButton;
-    private compareButton: ActionButton;
+    private compareCheckbox: Checkbox;
+
+    private selectionChangedListeners: ((isSelected: boolean) => void)[] = [];
 
     constructor(version: VersionHistoryItem, content: ContentSummaryAndCompareStatus) {
         super('version-list-item');
@@ -51,6 +52,22 @@ export class VersionHistoryListItem
         return !!this.compareButton;
     }
 
+    getVersion(): VersionHistoryItem {
+        return this.version;
+    }
+
+    onSelectionChanged(listener: (isSelected: boolean) => void): void {
+        this.selectionChangedListeners.push(listener);
+    }
+
+    isSelected(): boolean {
+        return this.compareCheckbox?.isChecked();
+    }
+
+    setSelected(isSelected: boolean, silent?: boolean): void {
+        this.compareCheckbox?.setChecked(isSelected, silent);
+    }
+
     private createVersionViewer(): VersionHistoryItemViewer {
         const versionViewer: VersionHistoryItemViewer = new VersionHistoryItemViewer();
 
@@ -59,6 +76,21 @@ export class VersionHistoryListItem
         }
 
         versionViewer.setObject(this.version);
+
+        if (this.isCompareButtonRequired()) {
+            this.compareCheckbox = this.createCompareCheckbox();
+            versionViewer.appendToNamesAndIconViewWrapper(this.compareCheckbox);
+
+            this.compareCheckbox.onClicked((event) => {
+                event.stopPropagation(); // Prevent the click from propagating to the version viewer
+            });
+
+            this.compareCheckbox.onValueChanged(() => {
+                const isChecked = this.compareCheckbox.isChecked();
+                versionViewer.toggleClass('selected', isChecked);
+                this.selectionChangedListeners.forEach(listener => listener(isChecked));
+            });
+        }
 
         if (this.version.getMessage()) {
             const messageBlock: DivEl = new DivEl('publish-message');
@@ -70,6 +102,10 @@ export class VersionHistoryListItem
         versionViewer.toggleClass('active', this.isActive());
 
         return versionViewer;
+    }
+
+    private isCompareButtonRequired(): boolean {
+        return this.isComparableItem();
     }
 
     private isInteractableItem(): boolean {
@@ -117,52 +153,11 @@ export class VersionHistoryListItem
         } else {
             revertAction.setTitle(i18n('field.version.makeCurrent'));
             revertAction.onExecuted(() => {
-                this.revert(this.version.getId(), this.version.getDateTime());
+                ContentVersionHelper.revert(this.content.getContentId(), this.version.getId(), this.version.getDateTime());
             });
         }
 
         return new ActionButton(revertAction);
-    }
-
-    private createCompareButton(): ActionButton {
-        const compareButton: ActionButton = new ActionButton(new Action());
-
-        compareButton
-            .setTitle(i18n('text.versions.showChanges'))
-            .addClass('compare icon-compare icon-medium transparent');
-
-        compareButton.getAction().onExecuted(this.openCompareDialog.bind(this));
-
-        return compareButton;
-    }
-
-    private openCompareDialog() {
-        CompareContentVersionsDialog.get()
-            .setContent(this.content)
-            .setReadOnly(this.content.isReadOnly())
-            .setRightVersion(this.version)
-            .resetLeftVersion()
-            .setRevertVersionCallback(this.revert.bind(this))
-            .open();
-    }
-
-    private revert(versionId: string, versionDate: Date) {
-        const contentIdAsString: string = this.content.getContentId().toString();
-
-        new RevertVersionRequest(versionId, this.content.getContentId())
-            .sendAndParse()
-            .then((newVersionId: string) => {
-                if (newVersionId === VersionContext.getActiveVersion(contentIdAsString)) {
-                    NotifyManager.get().showFeedback(i18n('notify.revert.noChanges'));
-                    return;
-                }
-
-                const dateTime = `${DateHelper.formatDateTime(versionDate)}`;
-                NotifyManager.get().showSuccess(i18n('notify.version.changed', dateTime));
-
-                VersionContext.setActiveVersion(contentIdAsString, newVersionId);
-            })
-            .catch(DefaultErrorHandler.handle);
     }
 
     private toggleTooltip() {
@@ -222,5 +217,9 @@ export class VersionHistoryListItem
 
             return rendered;
         });
+    }
+
+    private createCompareCheckbox(): Checkbox {
+        return Checkbox.create().setName('compare-version-checkbox').build();
     }
 }
--- modules/lib/src/main/resources/assets/js/app/view/context/widget/version/VersionHistoryView.ts
+++ modules/lib/src/main/resources/assets/js/app/view/context/widget/version/VersionHistoryView.ts
@@ -1,13 +1,17 @@
 import {DivEl} from '@enonic/lib-admin-ui/dom/DivEl';
-import {ObjectHelper} from '@enonic/lib-admin-ui/ObjectHelper';
+import {Action} from '@enonic/lib-admin-ui/ui/Action';
+import {ActionButton} from '@enonic/lib-admin-ui/ui/button/ActionButton';
 import {DateHelper} from '@enonic/lib-admin-ui/util/DateHelper';
 import {i18n} from '@enonic/lib-admin-ui/util/Messages';
 import Q from 'q';
 import {ContentSummary} from '../../../../content/ContentSummary';
 import {ContentSummaryAndCompareStatus} from '../../../../content/ContentSummaryAndCompareStatus';
 import {ContentVersionHelper} from '../../../../ContentVersionHelper';
+import {CompareContentVersionsDialog} from '../../../../dialog/CompareContentVersionsDialog';
 import {WidgetItemView} from '../../WidgetItemView';
+import {VersionHistoryItem} from './VersionHistoryItem';
 import {VersionHistoryList} from './VersionHistoryList';
+import {VersionHistoryListItem} from './VersionHistoryListItem';
 
 export class VersionHistoryView extends WidgetItemView {
 
@@ -16,6 +20,14 @@ export class VersionHistoryView extends WidgetItemView {
     private statusBlock: DivEl;
 
     private content: ContentSummaryAndCompareStatus;
+
+    private selectedItems: VersionHistoryItem[] = [];
+
+    private buttonsWrapper: DivEl;
+
+    private compareButton: ActionButton;
+
+    private resetButton: ActionButton;
 
     public static debug: boolean = false;
 
@@ -32,8 +44,49 @@ export class VersionHistoryView extends WidgetItemView {
         return super.layout().then(() => {
             this.versionListView = new VersionHistoryList();
             this.statusBlock = new DivEl('status');
-            this.appendChildren(this.statusBlock, this.versionListView);
+            this.compareButton = this.createCompareButton();
+            this.resetButton = this.createResetButton();
+            this.buttonsWrapper = new DivEl('action-buttons-wrapper');
+            this.buttonsWrapper.appendChildren(this.compareButton, this.resetButton);
+            this.buttonsWrapper.hide();
+            this.appendChildren(this.statusBlock, this.versionListView, this.buttonsWrapper);
+            this.listenSelectionChange();
         });
+    }
+
+    private createCompareButton(): ActionButton {
+        const actionButton = new ActionButton(new Action(i18n('widget.versions.compare')));
+
+        actionButton.getAction().onExecuted(() => {
+           this.openCompareDialog();
+        });
+
+        actionButton.setEnabled(false);
+        actionButton.addClass('compare-button');
+
+        return actionButton;
+    }
+
+    private listenSelectionChange(): void {
+        this.versionListView.onItemsAdded((items, itemViews) => {
+            itemViews.forEach((view: VersionHistoryListItem) => {
+                view.onSelectionChanged((isSelected: boolean) => {
+                    this.handleSelectionChange(view.getVersion(), isSelected);
+                });
+            });
+        });
+    }
+
+    private createResetButton(): ActionButton {
+        const actionButton = new ActionButton(new Action('X'));
+
+        actionButton.getAction().onExecuted(() => {
+            this.resetSelection();
+        });
+
+        actionButton.setEnabled(false).addClass('reset-compare-button');
+
+        return actionButton;
     }
 
     private getContentStatus(content: ContentSummaryAndCompareStatus): string {
@@ -78,6 +131,8 @@ export class VersionHistoryView extends WidgetItemView {
         }
 
         if (this.versionListView && this.content) {
+            this.resetSelection();
+
             ContentVersionHelper.fetchAndSetActiveVersion(this.content.getContentId()).then(() => {
                 this.versionListView.setContent(this.content);
             });
@@ -85,4 +140,45 @@ export class VersionHistoryView extends WidgetItemView {
 
         return Q();
     }
+
+    private handleSelectionChange(item: VersionHistoryItem, isSelected: boolean): void {
+        if (isSelected) {
+            this.selectedItems.push(item);
+
+            if (this.selectedItems.length > 1) {
+                this.addClass('selection-limit-reached');
+            }
+        } else {
+            this.selectedItems = this.selectedItems.filter(selectedItem => selectedItem.getId() !== item.getId());
+
+            if (this.selectedItems.length < 2) {
+                this.removeClass('selection-limit-reached');
+            }
+        }
+
+        this.buttonsWrapper.setVisible(this.selectedItems.length > 0);
+        this.compareButton.setEnabled(this.selectedItems.length > 1);
+        this.resetButton.setEnabled(this.selectedItems.length > 0);
+    }
+
+    private openCompareDialog(): void {
+        this.selectedItems.sort(this.sortByDate);
+
+        CompareContentVersionsDialog.get()
+            .setContent(this.content)
+            .setLeftVersion(this.selectedItems[0])
+            .setRightVersion(this.selectedItems[1])
+            .open();
+    }
+
+    private sortByDate(v1: VersionHistoryItem, v2: VersionHistoryItem): number {
+        return Number(v1.getContentVersion().getTimestamp()) - Number(v2.getContentVersion().getTimestamp());
+    }
+
+    private resetSelection(): void {
+        this.versionListView.getItemViews().forEach((view: VersionHistoryListItem) => {
+            view.setSelected(false);
+        });
+    }
+
 }
