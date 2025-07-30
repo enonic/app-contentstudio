8a725e4a8364c13f9898c0c21c9a009dbf351a84
Aliaksandr Shklianko <asl@enonic.com>
2025-07-30 15:10:03 +0200

    Version History widget is sometimes missing "Created" version #8846

---
+++ modules/lib/src/main/resources/assets/js/app/view/context/widget/version/BatchedContentVersionsConverter.ts
@@ -0,0 +1,17 @@
+import {ContentSummaryAndCompareStatus} from '../../../../content/ContentSummaryAndCompareStatus';
+import {ContentVersion} from '../../../../ContentVersion';
+import {ContentVersionsConverter} from './ContentVersionsConverter';
+import {VersionHistoryItem} from './VersionHistoryItem';
+
+export class BatchedContentVersionsConverter extends ContentVersionsConverter {
+
+    constructor(content: ContentSummaryAndCompareStatus) {
+        super(content);
+    }
+
+    public append(versions: ContentVersion[], isMoreVersionsToBeAdded: boolean): VersionHistoryItem[] {
+        this.allVersions.push(...versions.slice());
+        return this.makeVersionHistoryItems(this.filterSameVersions(this.allVersions), isMoreVersionsToBeAdded);
+    }
+
+}
