309dc9a7eb2728a264b41694a801452f49900ad4
Aliaksandr Shklianko <asl@enonic.com>
2025-09-08 13:42:17 +0200

    Version History widget is sometimes missing "Created" version #8846

--- modules/lib/src/main/resources/assets/js/app/view/context/widget/version/VersionHistoryList.ts
+++ modules/lib/src/main/resources/assets/js/app/view/context/widget/version/VersionHistoryList.ts
@@ -1,14 +1,12 @@
 import {DefaultErrorHandler} from '@enonic/lib-admin-ui/DefaultErrorHandler';
 import {LazyListBox} from '@enonic/lib-admin-ui/ui/selector/list/LazyListBox';
 import {DateHelper} from '@enonic/lib-admin-ui/util/DateHelper';
-import * as Q from 'q';
+import Q from 'q';
 import {ContentSummaryAndCompareStatus} from '../../../../content/ContentSummaryAndCompareStatus';
-import {ContentVersion} from '../../../../ContentVersion';
-import {ContentVersionPublishInfo} from '../../../../ContentVersionPublishInfo';
 import {GetContentVersionsRequest} from '../../../../resource/GetContentVersionsRequest';
 import {GetContentVersionsResult} from '../../../../resource/GetContentVersionsResult';
-import {VersionHistoryItem, VersionHistoryItemBuilder} from './VersionHistoryItem';
-import {VersionHistoryListHelper} from './VersionHistoryListHelper';
+import {BatchedContentVersionsConverter} from './BatchedContentVersionsConverter';
+import {VersionHistoryItem} from './VersionHistoryItem';
 import {VersionHistoryListItem} from './VersionHistoryListItem';
 
 export class VersionHistoryList
@@ -20,20 +18,29 @@ export class VersionHistoryList
 
     private totalCount: number = -1;
 
-    private lastDate: string;
+    private loadedCount: number = 0;
 
-    private loadedItems: ContentVersion[] = [];
+    private pendingLoadContent: ContentSummaryAndCompareStatus;
+
+    private loading: boolean = false;
+
+    private versionsConverter: BatchedContentVersionsConverter;
 
     constructor() {
         super('version-list');
     }
 
     setContent(content: ContentSummaryAndCompareStatus): void {
+        if (this.loading) {
+            this.pendingLoadContent = content;
+            return;
+        }
+
         this.content = content;
-        this.loadedItems = [];
+        this.loadedCount = 0;
         this.clearItems();
+        this.versionsConverter = null;
         this.load();
-        this.lastDate = null;
     }
 
     createItemView(version: VersionHistoryItem): VersionHistoryListItem {
@@ -45,134 +52,60 @@ export class VersionHistoryList
     }
 
     protected handleLazyLoad(): void {
+        if (this.pendingLoadContent) {
+            const itemToLoad = this.pendingLoadContent;
+            this.pendingLoadContent = null;
+            this.setContent(itemToLoad);
+            return;
+        }
+
         super.handleLazyLoad();
 
-        if (this.loadedItems.length !== this.totalCount) {
+        if (this.loadedCount !== this.totalCount) {
             this.load();
         }
     }
 
     private load(): void {
+        this.loading = true;
+
         this.doLoad().then((result: GetContentVersionsResult) => {
-            const versions = result.getContentVersions();
+            if (!this.versionsConverter) {
+                this.versionsConverter = new BatchedContentVersionsConverter(this.content);
+                this.totalCount = result.getMetadata().totalHits;
+            }
 
-            this.loadedItems.push(...versions);
-            this.totalCount = result.getMetadata().totalHits;
+            this.loadedCount += result.getMetadata().hits;
 
-            const filteredNoSameVersions = VersionHistoryListHelper.filterSameVersions(this.loadedItems);
-            const versionHistoryItems = this.makeVersionHistoryItems(filteredNoSameVersions);
+            const versionHistoryItems = this.versionsConverter.append(result.getContentVersions(), this.loadedCount < this.totalCount);
 
             this.addItems(versionHistoryItems.slice(this.getItemCount(), versionHistoryItems.length));
-        }).catch(DefaultErrorHandler.handle);
+            this.addMissingCompareButtons();
+        }).catch(DefaultErrorHandler.handle).finally(() => {
+            this.loading = false;
+
+            if (this.pendingLoadContent) {
+                const itemToLoad = this.pendingLoadContent;
+                this.pendingLoadContent = null;
+                this.setContent(itemToLoad);
+            }
+        });
     }
 
     private doLoad(): Q.Promise<GetContentVersionsResult> {
         return new GetContentVersionsRequest(this.content.getContentId())
-            .setFrom(this.loadedItems.length)
+            .setFrom(this.loadedCount)
             .setSize(VersionHistoryList.LOAD_SIZE)
             .sendAndParse();
     }
 
-    private makeVersionHistoryItems(loadedBatch: ContentVersion[]): VersionHistoryItem[] {
-        const result: VersionHistoryItem[] = [];
-
-        loadedBatch.forEach((version: ContentVersion, index: number) => {
-            const previousVersion = loadedBatch[index + 1];
-
-            // can't compare last version in the batch with the next one until the next batch is loaded
-            if (!previousVersion && !this.isFirstVersion(version, loadedBatch)) {
-                return;
-            }
-
-            const item = version.hasPublishInfo() ? this.createHistoryItemFromPublishInfo(version, index)
-                                                  : this.createHistoryItemFromVersion(version, previousVersion);
-            result.push(item);
-
-            if (this.isSameVersionForDraftAndMaster(version, previousVersion)) {
-                result.push(this.createHistoryItemFromVersion(version, previousVersion));
+    private addMissingCompareButtons(): void {
+        this.getItemViews().forEach((view: VersionHistoryListItem, index) => {
+            if (!view.hasCompareButton() && view.isComparableItem()) {
+                if (this.getItemViews().slice(index + 1).some((itemView: VersionHistoryListItem) => itemView.isComparableItem())) {
+                    view.addCompareButton();
+                }
             }
         });
-
-        return result;
     }
-
-    private createHistoryItemFromPublishInfo(version: ContentVersion, index: number): VersionHistoryItem {
-        const publishDateAsString: string = DateHelper.formatDate(version.getDisplayDate());
-        const publishInfo: ContentVersionPublishInfo = version.getPublishInfo();
-
-        const builder: VersionHistoryItemBuilder = new VersionHistoryItemBuilder()
-            .setStatus(VersionHistoryListHelper.getPublishVersionItemStatus(version))
-            .setVersion(version)
-            .setDateTime(publishInfo.getTimestamp())
-            .setUser(publishInfo.getPublisherDisplayName() || publishInfo.getPublisher())
-            .setSkipDate(publishDateAsString === this.lastDate)
-            .setRepublished(this.isRepublished(version, index))
-            .setMessage(publishInfo.getMessage())
-            .setContentId(this.content.getContentId());
-
-        if (publishInfo.isPublished()) {
-            builder.setActiveTo(publishInfo.getPublishedTo());
-
-            if (!publishInfo.isScheduled()) {
-                builder.setActiveFrom(publishInfo.getPublishedFrom());
-            }
-        }
-
-        this.lastDate = publishDateAsString;
-
-        return builder.build();
-    }
-
-    private isRepublished(version: ContentVersion, index: number): boolean {
-        if (!version.isPublished()) {
-            return false;
-        }
-
-        const publishedFrom: string = DateHelper.formatDateTime(version.getPublishInfo().getPublishedFrom());
-        const items = this.getItems().map(item => item.getContentVersion());
-
-        return items.some((v: ContentVersion, i: number) => {
-            if (i <= index || !v.isPublished()) {
-                return false;
-            }
-
-            const vPublishedFrom: string = DateHelper.formatDateTime(v.getPublishInfo().getPublishedFrom());
-            return publishedFrom === vPublishedFrom;
-        });
-    }
-
-    private isSameVersionForDraftAndMaster(publishedVersion: ContentVersion, previousVersion?: ContentVersion): boolean {
-        const publishInfo: ContentVersionPublishInfo = publishedVersion.getPublishInfo();
-
-        if (publishInfo?.isPublished() || publishInfo?.isScheduled()) {
-            return previousVersion?.hasPublishInfo() || publishedVersion.getTimestamp().getTime() -
-                   publishedVersion.getModified().getTime() < VersionHistoryListHelper.FILTER_STEP_MS;
-        }
-
-        return false;
-    }
-
-    private createHistoryItemFromVersion(version: ContentVersion, previousVersion?: ContentVersion): VersionHistoryItem {
-        const isFirstVersion = !previousVersion;
-        const timestamp: Date = isFirstVersion ? this.content.getContentSummary().getCreatedTime() : version.getTimestamp();
-        const timestampAsString: string = DateHelper.formatDate(timestamp);
-
-        const item: VersionHistoryItem = new VersionHistoryItemBuilder()
-            .setStatus(VersionHistoryListHelper.getRegularVersionItemStatus(version, previousVersion))
-            .setDateTime(timestamp)
-            .setVersion(version)
-            .setUser(version.getModifierDisplayName() || version.getModifier())
-            .setSkipDate(timestampAsString === this.lastDate)
-            .setContentId(this.content.getContentId())
-            .build();
-
-        this.lastDate = timestampAsString;
-
-        return item;
-    }
-
-    private isFirstVersion(version: ContentVersion, loadedBatch: ContentVersion[]): boolean {
-        return this.loadedItems.length >= this.totalCount && version.getId() === loadedBatch.slice().pop()?.getId();
-    }
-
 }
