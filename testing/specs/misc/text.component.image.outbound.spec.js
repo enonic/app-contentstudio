3ea4b481f1dfac4247dca30cfbc92c5588a5bba5
sgauruseu <sig@eninic.com>
2025-09-08 18:26:11 +0300

    Update ui-tests for ContextWindow #9096

--- testing/specs/misc/text.component.image.outbound.spec.js
+++ testing/specs/misc/text.component.image.outbound.spec.js
@@ -93,7 +93,7 @@ describe("text.component.image.outbound.spec: Inserts a text component with an i
     beforeEach(() => studioUtils.navigateToContentStudioApp());
     afterEach(() => studioUtils.doCloseAllWindowTabsAndSwitchToHome());
     before(async () => {
-        if (typeof browser !== "undefined") {
+        if (typeof browser !== 'undefined') {
             await studioUtils.getBrowser().setWindowSize(appConst.BROWSER_WIDTH, appConst.BROWSER_HEIGHT);
         }
         return console.log('specification starting: ' + this.title);
