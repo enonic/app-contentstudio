a34d7075550c01f177e6e436eac4a35605912f07
rymsha <sry@enonic.com>
2025-09-18 12:06:05 +0200

    Delete generic-style #9148

--- modules/app/src/main/java/com/enonic/xp/app/contentstudio/style/StyleDescriptorMapper.java
+++ modules/app/src/main/java/com/enonic/xp/app/contentstudio/style/StyleDescriptorMapper.java
@@ -9,7 +9,6 @@ import com.enonic.xp.app.ApplicationKey;
 import com.enonic.xp.i18n.LocaleService;
 import com.enonic.xp.script.serializer.MapGenerator;
 import com.enonic.xp.script.serializer.MapSerializable;
-import com.enonic.xp.style.ElementStyle;
 import com.enonic.xp.style.ImageStyle;
 import com.enonic.xp.style.StyleDescriptor;
 import com.enonic.xp.style.StyleDescriptors;
@@ -34,7 +33,7 @@ public class StyleDescriptorMapper
     @Override
     public void serialize( final MapGenerator gen )
     {
-        final Map<String, ElementStyle> styles = new LinkedHashMap<>();
+        final Map<String, ImageStyle> styles = new LinkedHashMap<>();
         final Map<String, ApplicationKey> stylesApps = new LinkedHashMap<>();
 
         gen.array( "css" );
@@ -42,10 +41,13 @@ public class StyleDescriptorMapper
         {
             gen.value( styleDescriptor.getCssPath() );
 
-            for ( ElementStyle styleElement : styleDescriptor.getElements() )
+            for ( var styleElement : styleDescriptor.getElements() )
             {
-                styles.put( styleElement.getName(), styleElement );
-                stylesApps.put( styleElement.getName(), styleDescriptor.getApplicationKey() );
+                if ( styleElement instanceof ImageStyle )
+                {
+                    styles.put( styleElement.getName(), (ImageStyle) styleElement );
+                    stylesApps.put( styleElement.getName(), styleDescriptor.getApplicationKey() );
+                }
             }
         }
         gen.end();
@@ -65,18 +67,15 @@ public class StyleDescriptorMapper
         gen.end();
     }
 
-    private void serializeElement( final MapGenerator gen, final ElementStyle element, final ApplicationKey applicationKey )
+    private void serializeElement( final MapGenerator gen, final ImageStyle element, final ApplicationKey applicationKey )
     {
         gen.map();
 
-        gen.value( "element", element.getElement() );
+        gen.value( "element", "image" );
         gen.value( "name", element.getName() );
         gen.value( "displayName", localizeDisplayName( element, applicationKey ) );
 
-        if ( element instanceof ImageStyle )
-        {
-            serializeImage( gen, (ImageStyle) element );
-        }
+         serializeImage( gen, element );
 
         gen.end();
     }
@@ -87,7 +86,7 @@ public class StyleDescriptorMapper
         gen.value( "aspectRatio", element.getAspectRatio() );
     }
 
-    private String localizeDisplayName( final ElementStyle styleElement, final ApplicationKey applicationKey )
+    private String localizeDisplayName( final ImageStyle styleElement, final ApplicationKey applicationKey )
     {
         LocaleMessageResolver localeMessageResolver = new LocaleMessageResolver( localeService, applicationKey );
         if ( !isBlank( styleElement.getDisplayNameI18nKey() ) )
