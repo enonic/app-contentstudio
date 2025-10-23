a812bfc2890ca6335312ad88b91b7524d2ecc96e
rymsha <sry@enonic.com>
2025-10-23 16:04:14 +0200

    wip

--- modules/rest/src/main/java/com/enonic/xp/app/contentstudio/rest/resource/project/ApplyProjectReadAccessPermissionsCommand.java
+++ modules/rest/src/main/java/com/enonic/xp/app/contentstudio/rest/resource/project/ApplyProjectReadAccessPermissionsCommand.java
@@ -7,7 +7,7 @@ import com.enonic.xp.content.ApplyContentPermissionsParams;
 import com.enonic.xp.content.Content;
 import com.enonic.xp.content.ContentId;
 import com.enonic.xp.content.ContentPath;
-import com.enonic.xp.node.ApplyPermissionsScope;
+import com.enonic.xp.content.ApplyContentPermissionsScope;
 import com.enonic.xp.security.RoleKeys;
 import com.enonic.xp.security.acl.AccessControlEntry;
 import com.enonic.xp.security.acl.AccessControlList;
@@ -52,7 +52,7 @@ public final class ApplyProjectReadAccessPermissionsCommand
                 params( ApplyContentPermissionsParams.create().
                     permissions( newList ).
                     contentId( contentRoot.getId() ).
-                    applyPermissionsScope( ApplyPermissionsScope.TREE ).
+                    applyPermissionsScope( ApplyContentPermissionsScope.TREE ).
                     build() ).
                 description( "Apply project's content root permissions" ).
                 taskService( taskService ).
