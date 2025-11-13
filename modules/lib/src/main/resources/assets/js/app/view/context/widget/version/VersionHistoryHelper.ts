fcfbf6c4e778176e89a1c69b0294e6f70f431570
alansemenov <ase@enonic.com>
2025-11-13 19:02:23 +0100

    "Mark as ready" versions are no longer selectable in the Version History #9354

--- modules/lib/src/main/resources/assets/js/app/view/context/widget/version/VersionHistoryHelper.ts
+++ modules/lib/src/main/resources/assets/js/app/view/context/widget/version/VersionHistoryHelper.ts
@@ -14,7 +14,6 @@ export class VersionHistoryHelper {
         return !version.isPublishAction() &&
                !version.isRestored() &&
                !version.isArchived() &&
-               !version.isReadonly() &&
-               !version.isMarkedAsReady();
+               !version.isReadonly();
     }
 }
