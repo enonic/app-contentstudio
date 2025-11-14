4b464d7f42c8e4dbb471e0f76f71876c96b5de48
Bruno Reis <reisfmb@gmail.com>
2025-11-14 16:18:43 -0300

    Enonic UI: PreviewToolbar adjustments #9385
    Externalized isWidgetRenderable to store. Used it on PreviewToolbarOpenExternalButton
    
    Improved PreviewToolbarExternalButton tooltip

--- modules/lib/src/main/resources/assets/js/v6/features/views/browse/layout/preview/PreviewToolbarOpenExternalButton.tsx
+++ modules/lib/src/main/resources/assets/js/v6/features/views/browse/layout/preview/PreviewToolbarOpenExternalButton.tsx
@@ -1,39 +1,32 @@
 import {Action} from '@enonic/lib-admin-ui/ui/Action';
 import {IconButton, Tooltip, Toolbar} from '@enonic/ui';
 import {SquareArrowOutUpRight} from 'lucide-react';
-import {ReactElement, useEffect, useState} from 'react';
+import {ReactElement} from 'react';
 import {formatShortcut} from '../../../../utils/action';
+import {$isWidgetRenderable} from '../../../../store/isWidgetRenderable';
+import {useStore} from '@nanostores/preact';
 
 export const PreviewToolbarOpenExternalButton = ({action}: {action: Action}): ReactElement => {
-    const tooltipValue = `${action.getLabel()} (${formatShortcut(action)})`;
-    const [isEnabled, setIsEnabled] = useState(action.isEnabled());
-
-    useEffect(() => {
-        if (!action) return;
-
-        setIsEnabled(action.isEnabled());
-
-        const handlePropertyChanged = () => {
-            setIsEnabled(action.isEnabled());
-        };
-
-        action.onPropertyChanged(handlePropertyChanged);
-
-        return () => {
-            action.unPropertyChanged(handlePropertyChanged);
-        };
-    }, [action]);
+    const label = action.getLabel();
+    const shortcut = formatShortcut(action);
+    const isWidgetRenderable = useStore($isWidgetRenderable);
 
     return (
-        <Tooltip value={tooltipValue}>
-            <Toolbar.Item asChild disabled={!isEnabled}>
+        <Tooltip
+            value={
+                <div className="mr-7.5">
+                    <span className="text-nowrap block">{label}</span>(<kbd>{shortcut}</kbd>)
+                </div>
+            }
+        >
+            <Toolbar.Item asChild disabled={!isWidgetRenderable}>
                 <IconButton
                     size="sm"
                     className="flex-shrink-0"
                     icon={SquareArrowOutUpRight}
                     onClick={() => action?.execute()}
-                    aria-label={tooltipValue}
-                    disabled={!isEnabled}
+                    aria-label={`${label} (${shortcut})`}
+                    disabled={!isWidgetRenderable}
                 />
             </Toolbar.Item>
         </Tooltip>
