44a2854703fbc5dd2dbdcccfdcb16f0f9e01c575
Mikita Taukachou <edloidas@gmail.com>
2025-11-23 20:55:56 +0300

    Enonic UI: Update StatusBadge #9444

--- modules/lib/src/main/resources/assets/js/v6/features/shared/StatusBadge.tsx
+++ modules/lib/src/main/resources/assets/js/v6/features/shared/StatusBadge.tsx
@@ -1,25 +1,46 @@
 import {cn} from '@enonic/ui';
-import {CompareStatus, CompareStatusFormatter} from '../../../app/content/CompareStatus';
+import {CompareStatus, CompareStatusChecker, CompareStatusFormatter} from '../../../app/content/CompareStatus';
+import {useI18n} from '../hooks/useI18n';
+import {i18n} from '.xp/dev/lib-admin-ui/util/Messages';
 
 type Props = {
     status: CompareStatus;
+    wasPublished?: boolean;
     className?: string;
 }
 
-export const StatusBadge = ({status, className}: Props) => {
-    const label = CompareStatusFormatter.formatStatusText(status);
+function buildStatusKeys(status: CompareStatus, wasPublished: boolean): string {
+    switch (status) {
+        case CompareStatus.NEW:
+            return wasPublished ? 'status.unpublished' : 'status.new';
+        case CompareStatus.NEWER:
+            return 'status.modified';
+        case CompareStatus.OLDER:
+            return 'status.outofdate';
+        case CompareStatus.EQUAL:
+            return 'status.published';
+        case CompareStatus.MOVED:
+            return 'status.moved';
+        case CompareStatus.ARCHIVED:
+            return 'status.archived';
+        default:
+            return 'status.unknown';
+    }
+}
 
-    return <span className={cn(
-        'text-sm capitalize text-main',
-        ({
-            'new': '',
-            'modified': 'italic text-subtle',
-            'outofdate': 'text-fbk-error',
-            'published': 'text-fbk-success',
-            'moved': 'italic text-subtle',
-            'archived': '',
-            'unknown': '',
-        } satisfies Record<CompareStatus, string>)[status],
-        className,
-    )}>{label}</span>;
+export const StatusBadge = ({status, wasPublished, className}: Props) => {
+    const isOnline = status !== CompareStatus.NEW && status !== CompareStatus.UNKNOWN;
+    const onlineLabel = useI18n('status.online');
+
+    const isMoved = status === CompareStatus.MOVED;
+    const modifiedLabel = useI18n('status.modified');
+    const statusLabel = useI18n(buildStatusKeys(status, wasPublished));
+    const statusLabels = isMoved ? `${modifiedLabel}, ${statusLabel}` : statusLabel;
+
+    return (
+        <span className={cn('flex items-center gap-x-2 text-sm capitalize', className)}>
+            {isOnline && <span className="text-success border-r-1 border-bdr-subtle pr-2">{onlineLabel}</span>}
+            <span className='text-subtle italic'>{statusLabels}</span>
+        </span>
+    );
 };
