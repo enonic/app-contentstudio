1972c92b07db1db1578177fc1dd12321823b303c
Mikita Taukachou <edloidas@gmail.com>
2025-11-23 20:55:56 +0300

    Enonic UI: Update StatusBadge #9444

--- modules/lib/src/main/resources/assets/js/v6/features/shared/items/ContentItemCheckable.tsx
+++ modules/lib/src/main/resources/assets/js/v6/features/shared/items/ContentItemCheckable.tsx
@@ -1,5 +1,5 @@
-import {Button, Checkbox, CheckboxProps, ListItem} from '@enonic/ui';
-import {useMemo} from 'react';
+import {Button, Checkbox, CheckboxProps, cn, ListItem} from '@enonic/ui';
+import {ComponentPropsWithoutRef, useMemo} from 'react';
 import {ContentSummaryAndCompareStatus} from '../../../../app/content/ContentSummaryAndCompareStatus';
 import {EditContentEvent} from '../../../../app/event/EditContentEvent';
 import {calcWorkflowStateStatus} from '../../utils/cms/content/workflow';
@@ -13,15 +13,17 @@ export type Props = {
     checked?: boolean;
     defaultChecked?: boolean;
     onCheckedChange?: (checked: boolean) => void;
-};
+} & ComponentPropsWithoutRef<'div'>;
 
 
 export const ContentItemCheckable = ({
+    className,
     content,
     checked,
     defaultChecked,
     onCheckedChange,
     readOnly,
+    ...props
 }: Props): React.ReactElement => {
     const label = String(content.getPath());
     const contentType = String(content.getType());
@@ -36,7 +38,9 @@ export const ContentItemCheckable = ({
     return (
         <ListItem
             role='row'
+            className={cn('py-0.75', className)}
             aria-selected={checked}
+            {...props}
         >
             <ListItem.Left>
                 <Checkbox
@@ -49,12 +53,12 @@ export const ContentItemCheckable = ({
             <ListItem.Content>
                 <Button onClick={() => {
                     new EditContentEvent([content]).fire();
-                }} className="block flex-1 w-[calc(100%+10px)] h-8 -mx-1.25 -my-1 px-1.25 py-1">
+                }} className="block flex-1 w-[calc(100%+10px)] h-7.5 -mx-1.25 -my-0.75 px-1.25 py-0.75">
                     <ListItem.DefaultContent label={label} icon={Icon} />
                 </Button>
             </ListItem.Content>
             <ListItem.Right>
-                <StatusBadge status={content.getCompareStatus()} />
+                <StatusBadge status={content.getCompareStatus()} wasPublished={!!content.getContentSummary().getPublishFirstTime()} />
             </ListItem.Right>
         </ListItem>
     );
