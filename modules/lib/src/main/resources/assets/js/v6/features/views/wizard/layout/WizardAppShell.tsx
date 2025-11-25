e55192d4f118e3acd709cd8da95a0d5f843f5484
Mikita Taukachou <edloidas@gmail.com>
2025-11-25 22:22:08 +0300

    Enonic UI: Add support for server events #9473

--- modules/lib/src/main/resources/assets/js/v6/features/views/wizard/layout/WizardAppShell.tsx
+++ modules/lib/src/main/resources/assets/js/v6/features/views/wizard/layout/WizardAppShell.tsx
@@ -1,15 +1,13 @@
 import type {ReactElement} from 'react';
-import {ProjectSelectionDialog} from '../../../shared/dialogs/ProjectSelectionDialog';
 import {PublishDialog} from '../../../shared/dialogs/publish/PublishDialog';
 
-export const BrowseAppShell = (): ReactElement => {
+export const WizardAppShell = (): ReactElement => {
     return (
         <>
             {/* Shared dialogs */}
-            <ProjectSelectionDialog />
             <PublishDialog />
         </>
     );
 };
 
-BrowseAppShell.displayName = 'BrowseAppShell';
+WizardAppShell.displayName = 'WizardAppShell';
