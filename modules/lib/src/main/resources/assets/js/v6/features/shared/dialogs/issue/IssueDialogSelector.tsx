26b3c85263714e6fa9d7e05983e6f2e095a7fbf0
Mathias Skjulestad <msk@enonic.com>
2026-01-07 12:47:57 +0100

    Enonic UI: IssueDialog list #9567 details #9564
    Added functionality:
    Allow user to add comments with the enter-key.
    Fetch individual issues instead of a complete list unless accessible.
    Refactored after PR review.

---
+++ modules/lib/src/main/resources/assets/js/v6/features/shared/dialogs/issue/IssueDialogSelector.tsx
@@ -0,0 +1,65 @@
+import {Selector} from '@enonic/ui';
+import type {ReactElement, ReactNode} from 'react';
+
+type IssueDialogSelectorOption = {
+    value: string;
+    label: string;
+    disabled?: boolean;
+};
+
+export type IssueDialogSelectorProps<Option extends IssueDialogSelectorOption> = {
+    value?: string;
+    disabled?: boolean;
+    options: Option[];
+    placeholder?: string;
+    onValueChange: (next: string) => void;
+    renderValue?: (value: string | undefined) => ReactNode;
+    renderItemText?: (option: Option) => ReactNode;
+};
+
+export function IssueDialogSelector<Option extends IssueDialogSelectorOption>({
+    value,
+    disabled,
+    options,
+    placeholder,
+    onValueChange,
+    renderValue,
+    renderItemText,
+}: IssueDialogSelectorProps<Option>): ReactElement {
+    const defaultValueRenderer = (selectedValue: string | undefined): ReactNode => {
+        return options.find(option => option.value === selectedValue)?.label ?? placeholder;
+    };
+
+    const valueRenderer = (selectedValue: string | undefined): ReactNode => {
+        return renderValue ? renderValue(selectedValue) : defaultValueRenderer(selectedValue);
+    };
+
+    const itemTextRenderer = (option: Option): ReactNode => {
+        return renderItemText ? renderItemText(option) : option.label;
+    };
+
+    return (
+        <Selector.Root value={value} disabled={disabled} onValueChange={onValueChange}>
+            <Selector.Trigger>
+                <Selector.Value placeholder={placeholder}>{valueRenderer}</Selector.Value>
+                <Selector.Icon />
+            </Selector.Trigger>
+            {/* Prevent the dialog portal closing on select */}
+            <Selector.Content onPointerDown={event => event.stopPropagation()}>
+                <Selector.Viewport>
+                    {options.map(option => (
+                        <Selector.Item
+                            key={option.value}
+                            value={option.value}
+                            textValue={option.label}
+                            disabled={option.disabled}
+                        >
+                            <Selector.ItemText>{itemTextRenderer(option)}</Selector.ItemText>
+                            <Selector.ItemIndicator />
+                        </Selector.Item>
+                    ))}
+                </Selector.Viewport>
+            </Selector.Content>
+        </Selector.Root>
+    );
+}
