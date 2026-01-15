604589cf5d430e38ef4d0d9382989b3239aaf7ed
Pavel Milkevich <pmi@enonic.com>
2026-01-15 11:01:10 +0100

    Use IframeEventBus for CS - iframe interaction #9241
    - Split BeforeContentSavedEvent.ts to iframe and CS versions

--- modules/lib/src/main/resources/assets/js/app/event/BeforeContentSavedEvent.ts
+++ modules/lib/src/main/resources/assets/js/app/event/BeforeContentSavedEvent.ts
@@ -1,14 +1,14 @@
 import {ClassHelper} from '@enonic/lib-admin-ui/ClassHelper';
-import {IframeEvent} from '@enonic/lib-admin-ui/event/IframeEvent';
+import {Event} from '@enonic/lib-admin-ui/event/Event';
 
 export class BeforeContentSavedEvent
-    extends IframeEvent {
+    extends Event {
 
     static on(handler: (event: BeforeContentSavedEvent) => void) {
-        IframeEvent.bind(ClassHelper.getFullName(this), handler);
+        Event.bind(ClassHelper.getFullName(this), handler);
     }
 
     static un(handler?: (event: BeforeContentSavedEvent) => void) {
-        IframeEvent.unbind(ClassHelper.getFullName(this), handler);
+        Event.unbind(ClassHelper.getFullName(this), handler);
     }
 }
