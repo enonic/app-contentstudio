d81a4ddf0e0895af124e6343a87ec60391a681c5
Pavel Milkevich <pmi@enonic.com>
2026-01-22 20:18:17 +0100

    Remove component descriptor selectors from placeholers #9632

--- modules/lib/src/main/resources/assets/js/page-editor/DescriptorBasedComponentViewPlaceholder.ts
+++ modules/lib/src/main/resources/assets/js/page-editor/DescriptorBasedComponentViewPlaceholder.ts
@@ -4,7 +4,5 @@ import {ComponentType} from '../app/page/region/ComponentType';
 export abstract class DescriptorBasedComponentViewPlaceholder
     extends ItemViewPlaceholder {
 
-    getType(): ComponentType {
-        throw new Error('Must be implemented by inheritors');
-    }
+    abstract getType(): ComponentType;
 }
