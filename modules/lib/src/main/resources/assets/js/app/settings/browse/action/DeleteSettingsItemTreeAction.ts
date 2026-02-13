3baaf2201eaabb168535a327f7cd90452f87301c
Aliaksandr Shklianko <asl@enonic.com>
2026-02-13 13:54:50 +0100

    Enonic UI: Project delete dialog #9836

--- modules/lib/src/main/resources/assets/js/app/settings/browse/action/DeleteSettingsItemTreeAction.ts
+++ modules/lib/src/main/resources/assets/js/app/settings/browse/action/DeleteSettingsItemTreeAction.ts
@@ -1,19 +1,13 @@
 import {Action} from '@enonic/lib-admin-ui/ui/Action';
 import {i18n} from '@enonic/lib-admin-ui/util/Messages';
 import {ObjectHelper} from '@enonic/lib-admin-ui/ObjectHelper';
-import {ProjectDeleteRequest} from '../../resource/ProjectDeleteRequest';
-import {DefaultErrorHandler} from '@enonic/lib-admin-ui/DefaultErrorHandler';
-import {showFeedback} from '@enonic/lib-admin-ui/notify/MessageBus';
 import {SettingsViewItem} from '../../view/SettingsViewItem';
 import {ProjectViewItem} from '../../view/ProjectViewItem';
-import {ConfirmValueDialog} from '../../../remove/ConfirmValueDialog';
-import {TextInputSize} from '@enonic/lib-admin-ui/ui/text/TextInput';
 import {getCurrentItems} from '../../../../v6/features/store/settingsTreeSelection.store';
+import {openDeleteSettingsDialog} from '../../../../v6/features/store/dialogs/deleteSettingsDialog.store';
 
 export class DeleteSettingsItemTreeAction
     extends Action {
-
-    private deleteConfirmationDialog?: ConfirmValueDialog;
 
     constructor() {
         super(i18n('action.delete'), 'mod+del');
@@ -22,38 +16,14 @@ export class DeleteSettingsItemTreeAction
     }
 
     private handleExecuted() {
-        if (!this.deleteConfirmationDialog) {
-            this.initConfirmationDialog();
+        const selectedItems: SettingsViewItem[] = [...getCurrentItems()];
+        const selectedItem = selectedItems[0];
+
+        if (!selectedItem || !ObjectHelper.iFrameSafeInstanceOf(selectedItem, ProjectViewItem)) {
+            return;
         }
 
-        const selectedItems: SettingsViewItem[] = [...getCurrentItems()];
-
-        this.deleteConfirmationDialog.setValueToCheck(selectedItems[0].getId()).open();
-    }
-
-    private initConfirmationDialog() {
-        this.deleteConfirmationDialog = new ConfirmValueDialog({inputSize: TextInputSize.LARGE});
-        this.deleteConfirmationDialog
-            .setHeaderText(i18n('dialog.confirmDelete'))
-            .setSubheaderText(i18n('dialog.project.delete.confirm.subheader'))
-            .setYesCallback(this.deleteSelectedItems.bind(this));
-    }
-
-    private deleteSelectedItems() {
-        const selectedItems: SettingsViewItem[] = [...getCurrentItems()];
-
-        const projectItems: ProjectViewItem[] = selectedItems.filter((item: SettingsViewItem) => {
-            return ObjectHelper.iFrameSafeInstanceOf(item, ProjectViewItem);
-        }) as ProjectViewItem[];
-
-        this.deleteSelectedProjectItems(projectItems);
-    }
-
-    private deleteSelectedProjectItems(projectItems: ProjectViewItem[]) {
-        projectItems.forEach((item: ProjectViewItem) => {
-            new ProjectDeleteRequest(item.getName()).sendAndParse().then(() => {
-                showFeedback(i18n('notify.settings.project.deleted', item.getName()));
-            }).catch(DefaultErrorHandler.handle);
-        });
+        const projectItem = selectedItem as ProjectViewItem;
+        openDeleteSettingsDialog(projectItem.getId(), projectItem.getName());
     }
 }
