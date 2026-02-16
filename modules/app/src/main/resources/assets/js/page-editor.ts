1559371096a9323b5acbbcd777dd83c7ab3b5e18
Pavel Milkevich <pmi@enonic.com>
2026-02-16 17:38:49 +0100

    Use npm-page-editor in live edit iframe #9840

--- modules/app/src/main/resources/assets/js/page-editor.ts
+++ modules/app/src/main/resources/assets/js/page-editor.ts
@@ -1,7 +1,6 @@
-/*import {PageEditor} from '@enonic/page-editor';
+import {PageEditor} from '@enonic/page-editor';
 
 import '@enonic/page-editor/styles.css';
 
 PageEditor.init();
-*/
 console.info('Page editor started.');
