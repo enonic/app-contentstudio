42822f6036dc0393c54605af3ef66f9c28ee951d
Mikita Taukachou <edloidas@gmail.com>
2026-03-18 21:55:17 +0300

    Port changes for the preview button to the new UI #10051

--- modules/lib/src/main/resources/assets/js/v6/features/views/browse/layout/preview/PreviewToolbarRefreshItem.tsx
+++ modules/lib/src/main/resources/assets/js/v6/features/views/browse/layout/preview/PreviewToolbarRefreshItem.tsx
@@ -1,16 +1,19 @@
 import {IconButton, Tooltip, Toolbar} from '@enonic/ui';
+import {useStore} from '@nanostores/preact';
 import {RefreshCw} from 'lucide-react';
 import {type ReactElement} from 'react';
 import {useI18n} from '../../../../hooks/useI18n';
+import {$isWidgetRenderable} from '../../../../store/contextWidgets.store';
 
 export const PreviewToolbarRefreshItem = ({onRefresh}: {onRefresh?: () => void}): ReactElement => {
     const label = useI18n('action.refresh');
+    const isWidgetRenderable = useStore($isWidgetRenderable);
 
     return (
         <Tooltip value={label}>
-            <Toolbar.Item asChild>
+            <Toolbar.Item asChild disabled={!isWidgetRenderable}>
                 <IconButton
-                    size="md"
+                    size="sm"
                     className="flex-shrink-0"
                     icon={RefreshCw}
                     onClick={() => onRefresh?.()}
