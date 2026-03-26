d5f5132328925e71f1fd999b988eedc02ebde3b3
Mikita Taukachou <edloidas@gmail.com>
2026-03-26 14:10:38 +0100

    Minor build adjustments

--- modules/lib/vitest.config.mjs
+++ modules/lib/vitest.config.mjs
@@ -6,6 +6,7 @@ export default defineConfig({
     globals: true,
     environment: 'happy-dom',
     setupFiles: ['./vitest.setup.ts'],
+    reporters: ['dot'],
     include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     exclude: ['**/node_modules/**', '**/build/**', '**/dist/**'],
     coverage: {
