45df56c7f6ad1bb593559451b825968499b849aa
Anatol Sialitski <asi@enonic.com>
2026-04-22 12:12:40 +0200

    Use lib-router for style extension #10276

--- modules/app/src/main/resources/admin/extensions/style/style.js
+++ modules/app/src/main/resources/admin/extensions/style/style.js
@@ -1,19 +1,16 @@
-exports.GET = function (req) {
-    const match = req.path.match(/\/([^/]+)\/editor\.css/);
-    if (!match) {
-        return {
-            status: 404,
-            contentType: 'text/plain',
-            body: 'Not found'
-        };
-    }
+const router = require('/lib/router')();
 
+exports.all = function (req) {
+    return router.dispatch(req);
+};
+
+router.get('/{appName:[^/]+}/editor.css', (req) => {
     const bean = __.newBean('com.enonic.app.main.GetStyleBean');
-    bean.setApplication(match[1]);
+    bean.setApplication(req.pathParams.appName);
     const css = bean.execute();
 
     return {
         contentType: 'text/css; charset=utf-8',
         body: css,
     };
-};
+});
