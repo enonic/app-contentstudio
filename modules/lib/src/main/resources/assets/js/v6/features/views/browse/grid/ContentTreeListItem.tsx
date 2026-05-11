d98668735047944bff3eeea3b8727415bd1c68c4
Mathias Skjulestad <msk@enonic.com>
2026-05-11 09:57:53 +0200

    The filtered grid should display the content path #10442
    Add content path to ContentTreeListItem.

--- modules/lib/src/main/resources/assets/js/v6/features/views/browse/grid/ContentTreeListItem.tsx
+++ modules/lib/src/main/resources/assets/js/v6/features/views/browse/grid/ContentTreeListItem.tsx
@@ -1,16 +1,17 @@
 import {ListItem} from '@enonic/ui';
 import {ContentLabel} from '../../../shared/content/ContentLabel';
 import {StatusBadge} from '../../../shared/status/StatusBadge';
-import {ContentData} from './ContentData';
+import {type ContentData} from './ContentData';
 
 export type ContentTreeListItemProps = {
     content: ContentData;
+    showPath?: boolean;
 };
 
-export const ContentTreeListItem = ({content}: ContentTreeListItemProps): React.ReactElement => (
+export const ContentTreeListItem = ({content, showPath = false}: ContentTreeListItemProps): React.ReactElement => (
     <ListItem className='p-0'>
         <ListItem.Left className="flex-1">
-            <ContentLabel content={content.item} />
+            <ContentLabel content={content.item} variant={showPath ? 'detailed' : 'normal'} />
         </ListItem.Left>
         <ListItem.Right>
             <StatusBadge status={content.publishStatus} />
