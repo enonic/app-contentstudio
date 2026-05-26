f43290282884935118ec7a86a4ec3c850df0ccf7
Aliaksandr Shklianko <asl@enonic.com>
2026-05-26 09:31:42 +0200

    Layer icon in Content Wizard should open the Layers widget #10561

--- modules/lib/src/main/resources/assets/js/v6/features/store/wizardToolbar.types.ts
+++ modules/lib/src/main/resources/assets/js/v6/features/store/wizardToolbar.types.ts
@@ -19,4 +19,6 @@ export type WizardToolbarStore = {
     isContentOnline: boolean;
     contentState: ContentState | null;
     isLayerProject: boolean;
+    isContentInherited: boolean;
+    isContentDataInherited: boolean;
 };
