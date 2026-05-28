d021929181551c5444b5d98b85441c4e768db10f
Mathias Skjulestad <msk@enonic.com>
2026-05-28 09:23:10 +0200

    Enonic UI: DeleteDialog key navigation #9565
    Implement key navigation for delete and unpublish dialog.

--- modules/lib/src/main/resources/assets/js/v6/features/shared/items/ContentListItemWithReference.tsx
+++ modules/lib/src/main/resources/assets/js/v6/features/shared/items/ContentListItemWithReference.tsx
@@ -1,10 +1,13 @@
-import {Branch} from '../../../../app/versioning/Branch';
-import {ContentReferencesLink} from '../ContentReferencesLink';
-import {ContentItemProps, ContentListItem} from './ContentListItem';
+import {type Ref} from 'react';
+import {type Branch} from '../../../../app/versioning/Branch';
+import {ContentReferencesLink, type ContentReferencesLinkProps} from '../ContentReferencesLink';
+import {type ContentItemProps, ContentListItem} from './ContentListItem';
 
 export type ContentListItemWithReferenceProps = {
     branch: Branch;
     hasInbound: boolean;
+    referenceLinkProps?: Omit<ContentReferencesLinkProps, 'contentId' | 'branch'>;
+    referenceLinkRef?: Ref<HTMLAnchorElement>;
 } & ContentItemProps;
 
 const CONTENT_LIST_ITEM_WITH_REFERENCE_NAME = 'ContentListItemWithReference';
@@ -13,13 +16,22 @@ export const ContentListItemWithReference = ({
     content,
     branch,
     hasInbound,
+    referenceLinkProps,
+    referenceLinkRef,
     'data-component': componentName = CONTENT_LIST_ITEM_WITH_REFERENCE_NAME,
     ...props
 }: ContentListItemWithReferenceProps): React.ReactElement => {
     const contentId = content.getContentId().toString();
     return (
         <ContentListItem content={content} data-component={componentName} {...props}>
-            {hasInbound && <ContentReferencesLink contentId={contentId} branch={branch} />}
+            {hasInbound && (
+                <ContentReferencesLink
+                    ref={referenceLinkRef}
+                    contentId={contentId}
+                    branch={branch}
+                    {...referenceLinkProps}
+                />
+            )}
         </ContentListItem>
     );
 }
