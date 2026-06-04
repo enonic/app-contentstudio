75606ba0d41fb25ed639e8c5c45bfaa5c630f9e9
Alex Shklianko <asl@enonic.com>
2026-06-04 14:16:49 +0200

    Global key shortcuts must toggle on opening/closing modal dialogs #10687 (#10723)
    Added temporary service to manage global key bindings.
    Updated tests.
    
    ---------
    
    Co-authored-by: sgauruseu <sig@eninic.com>

--- testing/specs/content.filter.panel.spec.js
+++ testing/specs/content.filter.panel.spec.js
@@ -64,19 +64,20 @@ describe('content.filter.panel.spec: tests for filter panel', function () {
             // 1. Open Filter Panel:
             await contentBrowsePanel.clickOnSearchButton();
             await filterPanel.waitForOpened();
+            await filterPanel.pause(300);
             // 2. Get the number in Folder aggregation checkbox in Filter Panel
             let number1 = await filterPanel.getNumberInAggregationLabel('Folder');
-
+            await studioUtils.saveScreenshot('before_folder_added');
             // 3. Open new folder wizard and save the folder:
             await studioUtils.openContentWizard(appConst.contentTypes.FOLDER);
             await contentWizard.typeDisplayName(FOLDER_NAME);
             await contentWizard.waitAndClickOnSave();
-            // TODO enonic ui bug
-            //await contentWizard.waitForSavedButtonVisible();
+            await contentWizard.waitForNotificationMessage();
+            await contentWizard.pause(1000);
             // 4. Switch to Browse Panel and verify that number in the aggregation checkbox is updated:
             await studioUtils.doSwitchToContentBrowsePanel();
-            await contentBrowsePanel.clickOnSearchButton();
             await filterPanel.waitForOpened();
+            await studioUtils.saveScreenshot('new_folder_added');
             let number2 = await filterPanel.getNumberInAggregationLabel('Folder');
             assert.equal(number2 - number1, 1, "Number of folders should be increased in the Filter Panel");
         });
