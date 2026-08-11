1154400b3f3bed400073d1277a68eb83493626a5
alansemenov <ase@enonic.com>
2026-08-11 10:24:41 +0200

    Fix the UI tests

--- testing/page_objects/home.page.js
+++ testing/page_objects/home.page.js
@@ -6,21 +6,20 @@ const Page = require('./page');
 const appConst = require('../libs/app_const');
 
 const xpTourDialog = {
-    container: `//div[contains(@id,'ModalDialog') and descendant::h2[contains(.,'Welcome Tour')]]`
+    container: `//div[contains(@id,'ModalDialog') and descendant::h2[contains(.,'Welcome Tour')]]`,
 };
 
 const selectors = {
     container: `div[class*='home-main-container visible']`,
-    contentStudioLink: 'a[id="com.enonic.app.contentstudio"] span.app-tile-name',
-    usersLink: 'a[id="com.enonic.xp.app.users"] span.app-tile-name',
-    applicationsLink: 'a[id="com.enonic.xp.app.applications"] span.app-tile-name',
-    dashboardLink: 'a.home-app span.app-tile-name',
-    avatarButton:'button#avatar-button',
+    contentStudioLink: 'a[data-tool-id="com.enonic.app.contentstudio:main"] span.app-tile-name',
+    usersLink: 'a[data-tool-id="com.enonic.xp.app.users:main"] span.app-tile-name',
+    applicationsLink: 'a[data-tool-id="com.enonic.xp.app.applications:main"] span.app-tile-name',
+    dashboardLink: 'a[data-tool-id="com.enonic.xp.app.main:dashboard"] span.app-tile-name',
+    avatarButton: 'button#avatar-button',
     logoutMenuItem: 'a.avatar-dropdown-item',
 };
 
 class HomePage extends Page {
-
     get closeXpTourButton() {
         return `${xpTourDialog.container}//div[@class='cancel-button-top']`;
     }
@@ -29,34 +28,33 @@ class HomePage extends Page {
         return await this.waitForElementDisplayed(selectors.container);
     }
 
-
-    async waitForUsersLinkNotDisplayed(){
+    async waitForUsersLinkNotDisplayed() {
         const host = await this.getXpMenuShadowHost();
         const span = await host.shadow$(selectors.usersLink);
-        await span.waitForDisplayed({timeout: appConst.mediumTimeout, reverse: true});
+        await span.waitForDisplayed({ timeout: appConst.mediumTimeout, reverse: true });
     }
 
-    async waitForDashboardLinkDisplayed(){
+    async waitForDashboardLinkDisplayed() {
         const host = await this.getXpMenuShadowHost();
         const span = await host.shadow$(selectors.dashboardLink);
-        await span.waitForDisplayed({timeout: appConst.mediumTimeout});
+        await span.waitForDisplayed({ timeout: appConst.mediumTimeout });
     }
 
     isLoaded() {
         return this.isElementDisplayed(selectors.container);
     }
 
-    async waitForContentLinkDisplayed(){
+    async waitForContentLinkDisplayed() {
         const host = await this.getXpMenuShadowHost();
         const span = await host.shadow$(selectors.contentStudioLink);
-        await span.waitForDisplayed({timeout: appConst.mediumTimeout});
+        await span.waitForDisplayed({ timeout: appConst.mediumTimeout });
     }
 
     async clickOnContentStudioLink() {
         try {
             const host = await this.getXpMenuShadowHost();
             const span = await host.shadow$(selectors.contentStudioLink);
-            await span.waitForDisplayed({timeout: appConst.mediumTimeout});
+            await span.waitForDisplayed({ timeout: appConst.mediumTimeout });
             await span.click();
         } catch (err) {
             await this.handleError('Content Studio link was not found', 'err_content_studio_link', err);
@@ -67,7 +65,7 @@ class HomePage extends Page {
         try {
             const host = await this.getXpMenuShadowHost();
             const span = await host.shadow$(selectors.applicationsLink);
-            await span.waitForDisplayed({timeout: appConst.mediumTimeout});
+            await span.waitForDisplayed({ timeout: appConst.mediumTimeout });
             await span.click();
         } catch (err) {
             await this.handleError('Applications link was not found', 'err_applications_link', err);
@@ -78,7 +76,7 @@ class HomePage extends Page {
         try {
             const host = await this.getXpMenuShadowHost();
             const span = await host.shadow$(selectors.dashboardLink);
-            await span.waitForDisplayed({timeout: appConst.mediumTimeout});
+            await span.waitForDisplayed({ timeout: appConst.mediumTimeout });
             await span.click();
         } catch (err) {
             await this.handleError('Dashboard link was not found', 'err_dashboard_link', err);
@@ -89,7 +87,7 @@ class HomePage extends Page {
         try {
             const host = await this.getXpMenuShadowHost();
             const span = await host.shadow$(selectors.usersLink);
-            await span.waitForDisplayed({timeout: appConst.mediumTimeout});
+            await span.waitForDisplayed({ timeout: appConst.mediumTimeout });
             await span.click();
         } catch (err) {
             await this.handleError('Users link was not found', 'err_users_link', err);
@@ -110,18 +108,18 @@ class HomePage extends Page {
         try {
             let host = await this.getXpMenuShadowHost();
             const avatarButton = await host.shadow$(selectors.avatarButton);
-            await avatarButton.waitForDisplayed({timeout: appConst.mediumTimeout});
+            await avatarButton.waitForDisplayed({ timeout: appConst.mediumTimeout });
             await avatarButton.click();
         } catch (err) {
             await this.handleError('Avatar button was not found', 'err_avatar_button', err);
         }
     }
 
-    async clickOnLogoutDropdownMenuItem(){
+    async clickOnLogoutDropdownMenuItem() {
         try {
             let host = await this.getXpMenuShadowHost();
             const logoutLink = await host.shadow$(selectors.logoutMenuItem);
-            await logoutLink.waitForDisplayed({timeout: appConst.mediumTimeout});
+            await logoutLink.waitForDisplayed({ timeout: appConst.mediumTimeout });
             await logoutLink.click();
         } catch (err) {
             await this.handleError('Logout menu item was not found', 'err_logout_menu_item', err);
